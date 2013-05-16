0313560a8257ae646afedfd4354eabe1c85c2442
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-16 16:17:41 +0000

    Deleted the DBConnection files

--- Quadriga/src/main/java/edu/asu/spring/quadriga/db/IDBConnection.java
+++
@@ -1,8 +0,0 @@
-package edu.asu.spring.quadriga.db;
-
-import edu.asu.spring.quadriga.domain.implementation.User;
-
-public interface IDBConnection {
-
-	public abstract User getUserDetails(String sUserId);
-}
