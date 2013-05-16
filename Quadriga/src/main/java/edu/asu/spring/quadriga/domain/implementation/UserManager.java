d9451e67929b0ba0af678eefb24e7982b6363f16
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-16 01:02:53 +0000

    updated with new DB logic

--- Quadriga/src/main/java/edu/asu/spring/quadriga/domain/implementation/UserManager.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/domain/implementation/UserManager.java
@@ -1,21 +1,25 @@
 package edu.asu.spring.quadriga.domain.implementation;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import edu.asu.spring.quadriga.domain.IUserManager;
 
+@Service
 public class UserManager implements IUserManager{
 	
-	DBConnection dbconnect;
+	DBConnectionManager dbConnect;
 	
 	public UserManager()
 	{
-		dbconnect = new DBConnection();
+		dbConnect = new DBConnectionManager();
 	}
 	
 	@Override
-	public User getUserDetails(String sUserId)
+	public User getUserDetails(String sUserId) throws SQLException
 	{
 		User user = new User();
 		
@@ -31,29 +35,26 @@ public class UserManager implements IUserManager{
 		
 		
 		
-		user = dbconnect.getUserDetails(sUserId);
-		
-		if(user!=null)
-		{
-			
-		}
+		user = dbConnect.getUserDetails(sUserId);
+
+		//TODO Remove: Code implemented for test class but changed later :(
 		//The user is active in Quad DB
-		if(listActiveUser.contains(sUserId))
-		{
-			user.setUserName(sUserId);
-			user.setActive(true);
-		}
-		//The user account is deactivated in the Quad DB
-		else if(listInActiveUser.contains(sUserId))
-		{
-			user.setUserName(sUserId);
-			user.setActive(false);
-		}
-		//No such user in the Quad Db
-		else
-		{
-			user = null;
-		}
+//		if(listActiveUser.contains(sUserId))
+//		{
+//			user.setUserName(sUserId);
+//			//user.setActive(true);
+//		}
+//		//The user account is deactivated in the Quad DB
+//		else if(listInActiveUser.contains(sUserId))
+//		{
+//			user.setUserName(null);
+//			//user.setActive(false);
+//		}
+//		//No such user in the Quad Db
+//		else
+//		{
+//			user = null;
+//		}
 		
 		return user;
 	}
