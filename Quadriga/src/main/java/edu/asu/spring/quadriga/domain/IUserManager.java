41b1b5f9567d52fb8ad7bda843b2166d3b5f47ab
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-16 01:01:20 +0000

    updated with DB functions

--- Quadriga/src/main/java/edu/asu/spring/quadriga/domain/IUserManager.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/domain/IUserManager.java
@@ -1,10 +1,12 @@
 package edu.asu.spring.quadriga.domain;
 
+import java.sql.SQLException;
+
 import edu.asu.spring.quadriga.domain.implementation.User;
 
 public interface IUserManager {
 
-	public abstract User getUserDetails(String sUserId);
+	public abstract User getUserDetails(String sUserId) throws SQLException;
 	
 	public abstract String updateUserDetails(User existingUser);
 	
