22596a0c801145a6f36a1ec871d94711f98353a4
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-17 20:53:34 +0000

    created after successful read from the context file

---
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/service/impl/QuadrigaRoleManager.java
@@ -0,0 +1,43 @@
+package edu.asu.spring.quadriga.service.impl;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import edu.asu.spring.quadriga.domain.IQuadrigaRoles;
+import edu.asu.spring.quadriga.service.IQuadrigaRoleManager;
+
+@Service
+public class QuadrigaRoleManager implements IQuadrigaRoleManager{
+
+	@Autowired
+	private List<IQuadrigaRoles> quadrigaRoles;
+	
+	public IQuadrigaRoles getQuadrigaRole(String sQuadrigaRoleId) {
+		for(IQuadrigaRoles role: quadrigaRoles)
+		{
+			if(role.getId().equals(sQuadrigaRoleId))
+				return role;
+		}
+		return null;
+	}
+
+	public void setQuadrigaRoles(List<IQuadrigaRoles> quadrigaRoles) {
+		this.quadrigaRoles = quadrigaRoles;
+	}
+
+
+
+	public void printRoles()
+	{
+		for(IQuadrigaRoles role: quadrigaRoles)
+		{
+			System.out.println(role.getId());
+			System.out.println(role.getName());
+			System.out.println(role.getDescription());
+		}
+	}
+}
