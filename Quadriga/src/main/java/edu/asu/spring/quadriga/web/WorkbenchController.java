40c8ca0efd714e730e65c65e030d3f154d18d165
rpendbha <rpendbha@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-17 17:11:31 +0000

    created WorkbenController.java, ProjectManager.java, MockupProjectManager.java classes.

--- Quadriga/src/main/java/edu/asu/spring/quadriga/web/WorkbenchController.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/WorkbenchController.java
@@ -1,16 +1,53 @@
 package edu.asu.spring.quadriga.web;
 
+import java.security.Principal;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import edu.asu.spring.quadriga.domain.IProject;
+import edu.asu.spring.quadriga.domain.IUser;
+import edu.asu.spring.quadriga.domain.implementation.Project;
+import edu.asu.spring.quadriga.service.IProjectManager;
+import edu.asu.spring.quadriga.service.IUserManager;
+
 @Controller
 public class WorkbenchController {
-
+	
+	@Autowired IProjectManager projectmanager;
+	List<IProject> project;
+	
+	@Autowired IUserManager usermanager;
+	IUser user;
+	
+	
+	
 	@RequestMapping(value="auth/workbench", method = RequestMethod.GET)
-	public String getProjectsOfUser() {
+	public String userWorkbenchHandle(ModelMap model) throws SQLException {
+	
+		UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		
-		// FIXME: to be implemented
-		return "auth/workbench";
+	    String sUserId = principal.getUsername();
+		String sUserStatus = null;
+				
+		user =  usermanager.getUserDetails(sUserId);
+
+		String username = user.getUserName();
+		
+		project = projectmanager.getProjectsOfUser(sUserId);
+		    
+		System.out.println("projectlist:" + project.toString());
+		
+		
+		return "auth/workbench"; 
 	}
 }
+	
