7e68507a1bcb683b09384fdf603e9829d98b05a5
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-17 23:37:20 +0000

    Created Test class

---
+++ trunk/Quadriga/src/test/java/edu/asu/spring/quadriga/service/impl/QuadrigaRoleManagerTest.java
@@ -0,0 +1,75 @@
+package edu.asu.spring.quadriga.service.impl;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.runner.RunWith;
+import org.junit.Test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+
+import edu.asu.spring.quadriga.domain.IQuadrigaRoles;
+import edu.asu.spring.quadriga.domain.implementation.QuadrigaRole;
+
+@ContextConfiguration(locations={"file:///C:/Users/Ram/Documents/EclipseProjects/Quadriga/src/main/webapp/WEB-INF/spring/quadriga-roles.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class QuadrigaRoleManagerTest {
+
+	@Autowired
+	private List<IQuadrigaRoles> quadrigaRoles;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetQuadrigaRole() {
+		
+		//Check if values are loaded
+		assertNotNull(quadrigaRoles);
+		
+		//Check if all roles are loaded
+		assertEquals(quadrigaRoles.size(), 6);
+		
+		//Check if each instance is of type QuadrigaRole
+		for(IQuadrigaRoles role: quadrigaRoles)
+		{
+			assertTrue(role instanceof QuadrigaRole);
+		}
+	}
+
+	@Test
+	public void testSetQuadrigaRoles() {
+		QuadrigaRoleManager qrManager = new QuadrigaRoleManager();
+		
+		//Assign null
+		qrManager.setQuadrigaRoles(null);
+		assertEquals(qrManager.getQuadrigaRoles(),null);
+		
+		//Assign a list of roles
+		qrManager.setQuadrigaRoles(quadrigaRoles);
+		assertEquals(qrManager.getQuadrigaRoles(),quadrigaRoles);		
+	}
+}
