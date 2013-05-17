db5e67aa2c071d5416289affc40f1b9e57c78f65
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-17 00:52:01 +0000

    After successful connection to Quad DB

--- Quadriga/src/main/java/edu/asu/spring/quadriga/db/IDBConnectionManager.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/db/IDBConnectionManager.java
@@ -2,10 +2,14 @@ package edu.asu.spring.quadriga.db;
 
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 import edu.asu.spring.quadriga.domain.implementation.User;
 
 public interface IDBConnectionManager {
 
+	public abstract void setDataSource(DataSource dataSource);
+	
 	public abstract User getUserDetails(String sUserId) throws SQLException;
 
 }
