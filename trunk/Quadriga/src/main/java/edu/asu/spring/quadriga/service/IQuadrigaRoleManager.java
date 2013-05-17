4f7ae756e0e0f098c1f8a687c459ffcc5a8951f0
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-17 20:53:05 +0000

    created after successful read from the context file

---
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/service/IQuadrigaRoleManager.java
@@ -0,0 +1,11 @@
+package edu.asu.spring.quadriga.service;
+
+import java.util.List;
+
+import edu.asu.spring.quadriga.domain.IQuadrigaRoles;
+
+public interface IQuadrigaRoleManager {
+
+	public abstract IQuadrigaRoles getQuadrigaRole(String sUserId);
+	
+}
