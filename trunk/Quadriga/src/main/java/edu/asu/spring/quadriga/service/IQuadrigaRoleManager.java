c15e0442fafe463fd86784546f13cbd035327c7b
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-17 22:08:58 +0000

    added the setter declaration

--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/service/IQuadrigaRoleManager.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/service/IQuadrigaRoleManager.java
@@ -1,9 +1,13 @@
 package edu.asu.spring.quadriga.service;
 
+import java.util.List;
+
 import edu.asu.spring.quadriga.domain.IQuadrigaRoles;
 
 public interface IQuadrigaRoleManager {
 
+	public abstract void setQuadrigaRoles(List<IQuadrigaRoles> quadrigaRoles);
+	
 	public abstract IQuadrigaRoles getQuadrigaRole(String sUserId);
 	
 }
