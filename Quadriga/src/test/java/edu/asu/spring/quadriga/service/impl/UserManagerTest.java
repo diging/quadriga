0416a3bd3cac8df73bf9f70452c9ac0c7dcde48d
kbatna <kbatna@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-21 22:41:46 +0000

    <Kiran> Test cases checked in

---
+++ Quadriga/src/test/java/edu/asu/spring/quadriga/service/impl/UserManagerTest.java
@@ -0,0 +1,69 @@
+package edu.asu.spring.quadriga.service.impl;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import edu.asu.spring.quadriga.domain.IUser;
+import edu.asu.spring.quadriga.service.IUserManager;
+@ContextConfiguration(locations={"file:///E:/LSA/Quadriga/Quadriga/src/main/webapp/WEB-INF/spring/spring-dbconnectionmanager.xml",
+		"file:///E:/LSA/Quadriga/Quadriga/src/main/webapp/WEB-INF/spring/root-context.xml",
+		"file:///E:/LSA/Quadriga/Quadriga/src/main/webapp/WEB-INF/spring/quadriga-roles.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserManagerTest 
+{
+
+	@Autowired
+	IUserManager usermanager;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testNoAccountUserRole() throws SQLException
+	{
+		IUser user = null;
+		user = usermanager.getUserDetails("hello");
+		assertEquals(user.getQuadrigaRoles().get(0).getId(),"ROLE_QUADRIGA_NOACCOUNT");
+	}
+	
+	@Test
+	public void testDeactivatedUserRole() throws SQLException
+	{
+		IUser user = null;
+		user = usermanager.getUserDetails("jdoe");
+		assertEquals(user.getQuadrigaRoles().get(0).getId(),"ROLE_QUADRIGA_DEACTIVATED");
+	}
+	
+	@Test
+	public void testAdminUserRole() throws SQLException
+	{
+		IUser user = null;
+		user = usermanager.getUserDetails("test");
+		assertFalse(user.getQuadrigaRoles().size()==1);
+	}
+
+}
