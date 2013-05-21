5c5c90b7ff1b0707a3d8670b41e772e25e2b85a6
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-21 01:01:13 +0000

    updated the controller to handle authenticated user cases

--- Quadriga/src/main/java/edu/asu/spring/quadriga/web/AccessForbiddenController.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/AccessForbiddenController.java
@@ -1,11 +1,13 @@
 package edu.asu.spring.quadriga.web;
 
+import java.security.Principal;
 import java.util.Collection;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,12 +17,12 @@ import edu.asu.spring.quadriga.web.login.RoleNames;
 public class AccessForbiddenController {
 
 	@RequestMapping(value="forbidden", method = RequestMethod.GET)
-	public String getInactiveUserPage() {
-		
+	public String getInactiveUserPage(ModelMap model, Principal principal) {
+
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		
+
 		Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
-		
+
 		for (GrantedAuthority ga : authorities) {
 			if (ga.getAuthority().equals(RoleNames.ROLE_QUADRIGA_DEACTIVATED)) {
 				return "inactiveuser";
@@ -29,7 +31,10 @@ public class AccessForbiddenController {
 				return "nouser";
 			}
 		}
-		
-		return "inactiveuser";
+
+		//An authenticated user can try to access other resources. So send them back to home page.
+		String sUserId = principal.getName();
+		model.addAttribute("username", sUserId);
+		return "auth/home";
 	}
 }
