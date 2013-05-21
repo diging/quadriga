ef1ea4d766a5f7e5cee09ca56e13f4c30cf48c92
kbatna <kbatna@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-21 00:28:16 +0000

    <Kiran> Created

---
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/domain/factories/impl/QuadrigaRoleFactory.java
@@ -0,0 +1,25 @@
+package edu.asu.spring.quadriga.domain.factories.impl;
+
+import org.apache.commons.lang.NotImplementedException;
+import org.springframework.stereotype.Service;
+
+import edu.asu.spring.quadriga.domain.IQuadrigaRoles;
+import edu.asu.spring.quadriga.domain.factories.IQuadrigaRoleFactory;
+import edu.asu.spring.quadriga.domain.implementation.QuadrigaRole;
+
+@Service
+public class QuadrigaRoleFactory implements IQuadrigaRoleFactory {
+
+	@Override
+	public IQuadrigaRoles createQuadrigaRoleObject() {
+		// TODO Auto-generated method stub
+		return new QuadrigaRole();
+	}
+
+	@Override
+	public IQuadrigaRoles cloneQuadrigaRoleObject(IQuadrigaRoles quadrigaRole) {
+		// TODO Auto-generated method stub
+		throw new NotImplementedException("Clone Quadriga Role is not yet implemented.");
+	}
+
+}
