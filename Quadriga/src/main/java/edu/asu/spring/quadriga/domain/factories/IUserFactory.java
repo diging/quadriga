2944c780a71d6a84b03cca54c3ea56f2c05ef61e
jdamerow <jdamerow@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-22 21:51:00 +0000

    clone method for user factory

--- Quadriga/src/main/java/edu/asu/spring/quadriga/domain/factories/IUserFactory.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/domain/factories/IUserFactory.java
@@ -19,7 +19,8 @@ public interface IUserFactory {
 	public abstract IUser createUserObject();
 
 	/**
-	 * Method for cloning a {@link IUser} object.
+	 * Method for cloning a {@link IUser} object. Note that this will produce a shallow clone, meaning that the QuadrigaRoles
+	 * will simply be put into a new list for the clone, but the QuadrigaRole objects themselves will be the same.
 	 * @param user the user object to be cloned.
 	 * @return a clone of the given user object that contains the exact same information as the original object.
 	 * @throws NotImplementedException
