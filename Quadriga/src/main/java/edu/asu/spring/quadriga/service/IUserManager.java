e577497fe2c456b3cff653ad7f4cefd6dc44a3f2
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-22 00:19:38 +0000

    Updated javadocs

--- Quadriga/src/main/java/edu/asu/spring/quadriga/service/IUserManager.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/service/IUserManager.java
@@ -1,23 +1,46 @@
 package edu.asu.spring.quadriga.service;
 
-import java.sql.SQLException;
-
 import edu.asu.spring.quadriga.domain.IUser;
 import edu.asu.spring.quadriga.domain.implementation.User;
 
 /**
- * @description : interface to implement UserManager class
+ * Interface class that places restraints on the UserManager class to implement
+ * the required behaviors.
  * 
- * @author      : Kiran
+ * @author Ram Kumar Kumaresan
  *
  */
 public interface IUserManager {
 
-	public abstract IUser getUserDetails(String sUserId) throws SQLException;
+	/**
+	 * Method which creates a user object for the given userId.
+	 * 
+	 * @param sUserId			The unique userid of the user
+	 * @return					User object created based on the userId
+	 */
+	public abstract IUser getUserDetails(String sUserId);
 	
+	/**
+	 * Method to update the user details.
+	 * 
+	 * @param existingUser		The user object corresponding to the authenticated user.
+	 * @return					String which indicates the operations status.
+	 */
 	public abstract String updateUserDetails(User existingUser);
 	
+	/**
+	 * Method to deactivate the user account in Quadriga database.
+	 * 
+	 * @param sUserId			The unique userid of the user
+	 * @return					Integer to indicate the operation status. 0 - Success. 1 - Already Inactive user. -1 - Error in performing the operation
+	 */
 	public abstract int deleteUser(String sUserId);
 	
+	/**
+	 * Method to add a new active user to the existing active user list in Quadriga database.
+	 * 
+	 * @param newUser			The user object which contains the details of the user to be added to Quadriga database.
+	 * @return					Integer to indicate the operation status. 0 - Success. 1 - Userid is taken. -1 - Error in performing the operation
+	 */
 	public abstract int addNewUser(User newUser);
 }
