c48d46aae915e7fc0ac5e0cc1fe82ff48d5302ec
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-22 23:56:51 +0000

    Added new db constants

--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/db/sql/DBConstants.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/db/sql/DBConstants.java
@@ -6,10 +6,14 @@ package edu.asu.spring.quadriga.db.sql;
  * @Called By     : DBConnectionManager.java
  * 
  * @author        : Kiran
- *
+ * @author 		  : Ram Kumar Kumaresan
  */
 public interface DBConstants 
 {
-	public final static String USER_DETAILS = "sp_getUserDetails";
 	public final static String SP_CALL = "call";
+	
+	public final static String USER_DETAILS = "sp_getUserDetails";	
+	public final static String ACTIVE_USER_DETAILS="sp_getActiveUsers";
+	public final static String INACTIVE_USER_DETAILS="sp_getInActiveUsers";
+	
 }
