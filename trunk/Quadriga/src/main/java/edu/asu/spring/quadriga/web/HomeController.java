51252d5521e0665d19c7fc17689db9ee06ba961b
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-22 21:05:42 +0000

    added username attribute

--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/HomeController.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/HomeController.java
@@ -1,5 +1,6 @@
 package edu.asu.spring.quadriga.web;
 
+import java.security.Principal;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -16,22 +17,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "auth/home", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model, Principal principal) {
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
+
+		// Get the LDAP-authenticated userid
+		String sUserId = principal.getName();		
+		model.addAttribute("username", sUserId);
 		model.addAttribute("serverTime", formattedDate );
-		
+
 		return "auth/home";
 	}
-	
+
 }
