04f6b379b5de5c14d70802d4b58497cf023a7eaa
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-24 00:57:45 +0000

    Updated with activate/deactivate user account code

--- Quadriga/src/main/java/edu/asu/spring/quadriga/service/impl/UserManager.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/service/impl/UserManager.java
@@ -11,6 +11,7 @@ import edu.asu.spring.quadriga.db.IDBConnectionManager;
 import edu.asu.spring.quadriga.domain.IQuadrigaRole;
 import edu.asu.spring.quadriga.domain.IUser;
 import edu.asu.spring.quadriga.domain.factories.IUserFactory;
+import edu.asu.spring.quadriga.domain.implementation.QuadrigaRole;
 import edu.asu.spring.quadriga.domain.implementation.User;
 import edu.asu.spring.quadriga.service.IUserManager;
 import edu.asu.spring.quadriga.web.login.RoleNames;
@@ -57,18 +58,22 @@ public class UserManager implements IUserManager {
 		List<IQuadrigaRole> userRole = null;
 		IQuadrigaRole quadrigaRole = null;
 		List<IQuadrigaRole> rolesList = new ArrayList<IQuadrigaRole>();
-		
+
 		user = dbConnect.getUserDetails(sUserId);
-		
+
 		try
 		{
 			if(user!=null)
 			{
 				userRole = user.getQuadrigaRoles();
-
+				
 				for(i=0;i<userRole.size();i++)
 				{
 					quadrigaRole = rolemanager.getQuadrigaRole(userRole.get(i).getDBid());
+					if(quadrigaRole.getId().equals(RoleNames.ROLE_QUADRIGA_DEACTIVATED))
+					{
+						rolesList.clear();
+					}
 					rolesList.add(quadrigaRole);
 				}
 				user.setQuadrigaRoles(rolesList);
@@ -88,15 +93,14 @@ public class UserManager implements IUserManager {
 
 		return user;	
 	}
-	
+
 	@Override
 	public List<IUser> getAllActiveUsers()
 	{
 		List<IUser> listUsers = null;
-		
+
 		try
 		{
-			System.out.println("---------------------------------------------------");
 			listUsers = dbConnect.getAllActiveUsers();
 		}
 		catch(Exception e)
@@ -105,12 +109,12 @@ public class UserManager implements IUserManager {
 		}
 		return listUsers;		
 	}
-	
+
 	@Override
 	public List<IUser> getAllInActiveUsers()
 	{
 		List<IUser> listUsers = null;
-		
+
 		try
 		{
 			listUsers = dbConnect.getAllInActiveUsers();
@@ -136,8 +140,50 @@ public class UserManager implements IUserManager {
 	 *                   (Not yet implemented)
 	 */
 	@Override
-	public int deleteUser(String sUserId) {
-		return 0;
+	public int deactivateUser(String sUserId) {
+		//Find the ROLEDBID for Deactivated account
+		String sDeactiveRoleDBId = rolemanager.getQuadrigaRoleDBId(RoleNames.ROLE_QUADRIGA_DEACTIVATED);
+
+		//Add the new role to the user.
+		int iResult = dbConnect.deactivateUser(sUserId, sDeactiveRoleDBId);
+		return iResult;
+	}
+
+	@Override
+	public int activateUser(String sUserId) {
+
+		//Find the deactivated role id and create a QuadrigaRole Object
+		String sDeactiveRoleDBId = rolemanager.getQuadrigaRoleDBId(RoleNames.ROLE_QUADRIGA_DEACTIVATED);
+		System.out.println(sDeactiveRoleDBId);
+
+		//Find all the roles of the user
+		IUser user = null;
+		List<IQuadrigaRole> userRole = null;
+		IQuadrigaRole quadrigaRole = null;
+		List<IQuadrigaRole> rolesList = new ArrayList<IQuadrigaRole>();
+
+		user = dbConnect.getUserDetails(sUserId);
+
+		//Remove the deactivated role from user roles
+		if(user!=null)
+		{
+			userRole = user.getQuadrigaRoles();
+			for(int i=0;i<userRole.size();i++)
+			{				
+				if(!userRole.get(i).getDBid().equals(sDeactiveRoleDBId))
+				{
+					quadrigaRole = rolemanager.getQuadrigaRole(userRole.get(i).getDBid());
+					rolesList.add(quadrigaRole);
+				}
+			}
+			user.setQuadrigaRoles(rolesList);
+		}
+
+		//Convert the user roles to one string with DBROLEIDs
+		//Update the role in the Quadriga Database.
+		int iResult = dbConnect.updateUserRoles(sUserId, user.getQuadrigaRolesDBId());
+
+		return iResult;
 	}
 
 	/**
