594e8377a0426a1850aeb595b001664b4ad21c65
jdamerow <jdamerow@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-05-30 01:05:30 +0000

    error page if no username is given in account request; javadoc

--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/manageusers/RequestAccountController.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/manageusers/RequestAccountController.java
@@ -10,13 +10,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.asu.spring.quadriga.service.IUserManager;
 
+/**
+ * This class handles account requests by logged in users that don't have an account yet.
+ * 
+ * @author Julia Damerow
+ *
+ */
 @Controller
 public class RequestAccountController {
 
 	@Autowired
 	private IUserManager userManager;
 
-	
+	/**
+	 * This simply fills in the username for the request account page.
+	 * @param model Model for jsp page.
+	 * @param principal Information about logged in user.
+	 * @return path to request account page.
+	 */
 	@RequestMapping(value = "requests/requestAccount", method = RequestMethod.GET)
 	public String requestAccount(Model model, Principal principal) {
 		String sUserId = principal.getName();		
@@ -25,12 +36,18 @@ public class RequestAccountController {
 		return "requests/accountRequest";
 	}
 	
+	/**
+	 * This sends a request for a user account to the {@link IUserManager}.
+	 * @param model Model for jsp page.
+	 * @param principal Information about logged in user.
+	 * @return result page 
+	 */
 	@RequestMapping(value = "requests/submitAccountRequest", method = RequestMethod.POST)
 	public String submitAccountRequest(Model model, Principal principal) {
 		String userId = principal.getName();
 		
 		if (userId == null || userId.trim().isEmpty())
-			return "error";
+			return "requests/error";
 		
 		userManager.addAccountRequest(userId);
 
