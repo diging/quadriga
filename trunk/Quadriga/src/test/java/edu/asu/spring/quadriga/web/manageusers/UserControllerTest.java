0f9363e6822464578f206defb87f0fe6cf214b32
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-04 01:23:14 +0000

    Updated the test class and added a method in dbconnection manager to setup test environment

--- trunk/Quadriga/src/test/java/edu/asu/spring/quadriga/web/manageusers/UserControllerTest.java
+++ trunk/Quadriga/src/test/java/edu/asu/spring/quadriga/web/manageusers/UserControllerTest.java
@@ -23,6 +23,12 @@ import edu.asu.spring.quadriga.domain.IUser;
 import edu.asu.spring.quadriga.service.IUserManager;
 import edu.asu.spring.quadriga.service.impl.UserManager;
 
+/**
+ * This class tests the {@link UserController}
+ * 
+ * @author Ram Kumar Kumaresan
+ *
+ */
 @ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/spring-dbconnectionmanager.xml",
 		"file:src/main/webapp/WEB-INF/spring/root-context.xml",
 		"file:src/main/webapp/WEB-INF/spring/spring-security.xml",
@@ -48,6 +54,10 @@ public class UserControllerTest {
 	public static void tearDownAfterClass() throws Exception {
 	}
 
+	/**
+	 * This method setsup the needed usermanager, model, principal and authentication objects
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		userContoller = new UserController();
@@ -67,10 +77,16 @@ public class UserControllerTest {
 	public void tearDown() throws Exception {
 	}
 
+	/**
+	 * This method tests if the appropriate paths are returned. It also checks if the correct
+	 * number of users are returned for cases where the status are active, inactive and open requests.
+	 * 
+	 */
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testManageUsers() {
-		
-		//Check the return type
+
+		//Check the return value
 		assertEquals(userContoller.manageUsers(model, principal),"auth/users/manage");
 
 		//User Requests
@@ -86,34 +102,80 @@ public class UserControllerTest {
 		assertEquals(2,inactiveList.size());
 	}
 
+	/**
+	 * This method tests if the appropriate path is returned and also evaluates the number of 
+	 * open requests and also checks if the correct path is returned.
+	 */
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testUserRequestList() {
-				fail("Not yet implemented");
+		//Check the return value
+		assertEquals(userContoller.userRequestList(model, principal),"auth/users/requests");
+
+		//Open Requests
+		List<IUser> userRequestsList = (List<IUser>) model.get("userRequestsList");
+		assertEquals(2,userRequestsList.size());
+
 	}
 
+	/**
+	 * This method tests if a user is approved/denied access to quadriga and also checks if the correct path is returned.
+	 */
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testUserAccessHandler() {
-				fail("Not yet implemented");
+		//Deny a user
+		assertEquals(userContoller.userAccessHandler("charlie-denied", model, principal),"redirect:/auth/users/manage");
+
+		//Approve a user
+		assertEquals(userContoller.userAccessHandler("charlie-approve-admin", model, principal),"redirect:/auth/users/manage");
+
 	}
 
+	/**
+	 * This method tests if the correct number of active users are retrieved and if the appropriate path is also returned.
+	 */
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testUserActiveList() {
-				fail("Not yet implemented");
+		//Check the return value
+		assertEquals(userContoller.userActiveList(model, principal),"auth/users/active");
+
+		//Active Users
+		List<IUser> activeList = (List<IUser>) model.get("activeUserList");
+		assertEquals(2,activeList.size());
 	}
 
+	/**
+	 * This method tests if the correct number of inactive users are retrieved and if the appropriate path is also returned.
+	 */
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testUserInactiveList() {
-				fail("Not yet implemented");
+		//Check the return value
+		assertEquals(userContoller.userInactiveList(model, principal),"auth/users/inactive");
+
+		//Inactive Users
+		List<IUser> inactiveList = (List<IUser>) model.get("inactiveUserList");
+		assertEquals(2,inactiveList.size());
 	}
 
+	/**
+	 * This method tests if a user is deactivated and returns the appropriate path.
+	 */
 	@Test
 	public void testDeactivateUser() {
-				fail("Not yet implemented");
+		//Check the return value
+		assertEquals(userContoller.deactivateUser("jdoe",model, principal),"redirect:/auth/users/manage");
 	}
-
+	
+	/**
+	 * This method tests if a user is activated and returns the appropriate path. 
+	 */
 	@Test
 	public void testActivateUser() {
-				fail("Not yet implemented");
+		//Check the return value
+		assertEquals(userContoller.activateUser("jdoe",model, principal),"redirect:/auth/users/manage");
 	}
 
 }
