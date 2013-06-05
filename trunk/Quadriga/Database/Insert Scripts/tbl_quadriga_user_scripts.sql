1310289f392d3604c7039d354aae3cd38e94b3e3
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-05 16:25:35 +0000

    moved the insert scripts into the scripts folder

--- trunk/Quadriga/Database/Insert Scripts/tbl_quadriga_user_scripts.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO tbl_quadriga_user_requests
-(fullname, username,passwd,email,createdby,createddate,updatedby,updateddate)
-VALUES('dexter','dexter',NULL,'dexter@lsa.asu.edu',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
-INSERT INTO tbl_quadriga_user_requests
-(fullname, username,passwd,email,createdby,createddate,updatedby,updateddate)
-VALUES('deb','deb',NULL,'deb@lsa.asu.edu',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
-INSERT INTO tbl_quadriga_user_requests
-(fullname, username,passwd,email,createdby,createddate,updatedby,updateddate)
-VALUES('harrison','harrison',NULL,'harrison@lsa.asu.edu',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
