8b11b8f4bbbc3dea91c05ebc014416cdb777c4c1
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-13 17:43:50 +0000

    changed the not null columns to nullable.

--- Quadriga/Database/Tables/tbl_quadriga_user_requests.sql
+++ Quadriga/Database/Tables/tbl_quadriga_user_requests.sql
@@ -12,10 +12,10 @@ Modified Date : 05/24/2013
 ********************************************/
 CREATE TABLE IF NOT EXISTS tbl_quadriga_user_requests
 (
-  fullname      VARCHAR(50)  NOT NULL,
+  fullname      VARCHAR(50)  DEFAULT NULL,
   username      VARCHAR(10)  NOT NULL,
-  passwd        VARCHAR(10)  NULL,
-  email         VARCHAR(50)  NOT NULL,
+  passwd        VARCHAR(10)  DEFAULT NULL,
+  email         VARCHAR(50)  DEFAULT NULL,
   updatedby     VARCHAR(10)  NOT NULL,
   updateddate   TIMESTAMP    NOT NULL,
   createdby     VARCHAR(10)  NOT NULL,
