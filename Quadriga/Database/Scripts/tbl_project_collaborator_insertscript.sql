7cfada7f89b6ec50de08b24fa0cbc4acb850fbe5
rpendbha <rpendbha@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-13 17:42:29 +0000

    commiting project script and project collaborator script and foreign key constraint script according to changes in database

--- Quadriga/Database/Scripts/tbl_project_collaborator_insertscript.sql
+++ Quadriga/Database/Scripts/tbl_project_collaborator_insertscript.sql
@@ -1,34 +1,19 @@
 
 
 INSERT tbl_project_collaborator(projectid,collaboratoruser,collaboratorrole,updatedby,updateddate,createdby,createddate)
-VALUES ('1','test','collaborator_role1,collaborator_role4',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
+VALUES (1,'test','collaborator_role1,collaborator_role4',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
 
 INSERT tbl_project_collaborator(projectid,collaboratoruser,collaboratorrole,updatedby,updateddate,createdby,createddate)
-VALUES ('1','jdoe','collaborator_role3',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
+VALUES (1,'jdoe','collaborator_role3',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
+
 
 INSERT tbl_project_collaborator(projectid,collaboratoruser,collaboratorrole,updatedby,updateddate,createdby,createddate)
-VALUES ('1','leonard','collaborator_role2',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
+VALUES (2,'test','collaborator_role3',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
+
 
 INSERT tbl_project_collaborator(projectid,collaboratoruser,collaboratorrole,updatedby,updateddate,createdby,createddate)
-VALUES ('1','dexter','collaborator_role4',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
+VALUES (3,'jdoe','collaborator_role1,collaborator_role4',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
 
 INSERT tbl_project_collaborator(projectid,collaboratoruser,collaboratorrole,updatedby,updateddate,createdby,createddate)
-VALUES ('1','penny','collaborator_role2',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
+VALUES (3,'test','collaborator_role2',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
 
-INSERT tbl_project_collaborator(projectid,collaboratoruser,collaboratorrole,updatedby,updateddate,createdby,createddate)
-VALUES ('2','dexter','collaborator_role3',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
-
-INSERT tbl_project_collaborator(projectid,collaboratoruser,collaboratorrole,updatedby,updateddate,createdby,createddate)
-VALUES ('2','deb','collaborator_role1',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
-
-INSERT tbl_project_collaborator(projectid,collaboratoruser,collaboratorrole,updatedby,updateddate,createdby,createddate)
-VALUES ('3','leonard','collaborator_role1,collaborator_role4',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
-
-INSERT tbl_project_collaborator(projectid,collaboratoruser,collaboratorrole,updatedby,updateddate,createdby,createddate)
-VALUES ('3','test','collaborator_role3',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
-
-INSERT tbl_project_collaborator(projectid,collaboratoruser,collaboratorrole,updatedby,updateddate,createdby,createddate)
-VALUES ('4','jdoe','collaborator_role2',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
-
-INSERT tbl_project_collaborator(projectid,collaboratoruser,collaboratorrole,updatedby,updateddate,createdby,createddate)
-VALUES ('4','penny','collaborator_role1',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
--- Quadriga/Database/Scripts/tbl_project_scripts_sample.sql
+++ Quadriga/Database/Scripts/tbl_project_scripts_sample.sql
@@ -1,16 +1,15 @@
-	use quadriga;
 	
-	INSERT tbl_project(projectname,description,projectid,projectowner,accessibility,updatedby,updateddate,createdby,createddate)
-	VALUES ('project1','Lifecyle of Reptiles',1,'test',ACCESSIBLE,SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
-	
-	
-	INSERT tbl_project(projectname,description,projectid,projectowner,accessibility,updatedby,updateddate,createdby,createddate)
-	VALUES ('project2','Lifecyle of Mammals',2,'dexter',ACCESSIBLE,SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
+	INSERT tbl_project(projectname,description,unixname,projectid,projectowner,accessibility,updatedby,updateddate,createdby,createddate)
+	VALUES ('project1','Lifecyle of Reptiles','unixname1',1,'test','ACCESSIBLE',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
 	
 	
-	INSERT tbl_project(projectname,description,projectid,projectowner,accessibility,updatedby,updateddate,createdby,createddate)
-	VALUES ('project3','Lifecyle of Aquatic species',3,'deb',NOT_ACCESSIBLE,SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
+	INSERT tbl_project(projectname,description,unixname,projectid,projectowner,accessibility,updatedby,updateddate,createdby,createddate)
+	VALUES ('project2','Lifecyle of Mammals','unixname2',2,'jdoe','ACCESSIBLE',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
 	
-	INSERT tbl_project(projectname,description,projectid,projectowner,accessibility,updatedby,updateddate,createdby,createddate)
-	VALUES ('project4','Lifecycle of Vertebrates',4,'test',NOT_ACCESSIBLE,SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
+	
+	INSERT tbl_project(projectname,description,unixname,projectid,projectowner,accessibility,updatedby,updateddate,createdby,createddate)
+	VALUES ('project3','Lifecyle of Aquatic species','unixname3',3,'test','NOT_ACCESSIBLE',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
+	
+	INSERT tbl_project(projectname,description,unixname,projectid,projectowner,accessibility,updatedby,updateddate,createdby,createddate)
+	VALUES ('project4','Lifecycle of Vertebrates','unixname4',4,'test','NOT_ACCESSIBLE',SUBSTRING_INDEX(USER(),'@',1),CURDATE(),SUBSTRING_INDEX(USER(),'@',1),CURDATE());
 	
