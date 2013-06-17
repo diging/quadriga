58db2e0b31570eb07574c896df7ddb4545b48401
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-17 21:02:50 +0000

    Created separate classes for Quadriga and Dsapce rest service.

--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/dspace/service/ICommunityManager.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/dspace/service/ICommunityManager.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 import org.springframework.web.client.RestTemplate;
 
-import edu.asu.spring.quadriga.domain.ICommunity;
 
 public interface ICommunityManager {
 
-	public abstract List<ICommunity> getAllCommunities(RestTemplate restTemplate, String url, String sUserName, String sPassword);
+	public abstract List<IDspaceCommunity> getAllCommunities(RestTemplate restTemplate, String url, String sUserName, String sPassword);
 }
--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/dspace/service/IDspaceManager.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/dspace/service/IDspaceManager.java
@@ -2,15 +2,13 @@ package edu.asu.spring.quadriga.dspace.service;
 
 import java.util.List;
 
-import edu.asu.spring.quadriga.domain.ICollection;
-import edu.asu.spring.quadriga.domain.ICommunity;
-import edu.asu.spring.quadriga.domain.implementation.Community;
+import edu.asu.spring.quadriga.dspace.service.impl.DspaceCommunity;
 
 public interface IDspaceManager {
 
-	public abstract List<ICommunity> getAllCommunities(String sUserName, String sPassword);
+	public abstract List<IDspaceCommunity> getAllCommunities(String sUserName, String sPassword);
 
-	public abstract List<ICollection> getAllCollections(String sUserName, String sPassword,String sCommunityTitle);
+	public abstract List<IDspaceCollection> getAllCollections(String sUserName, String sPassword,String sCommunityTitle);
 
 	public abstract String getCollectionName(String sUserName, String sPassword,String sCollectionId);
 
