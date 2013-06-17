14e652a0d37f049c296550d8e587edf58ad62799
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-17 21:02:50 +0000

    Created separate classes for Quadriga and Dsapce rest service.

--- Quadriga/src/main/java/edu/asu/spring/quadriga/dspace/service/ICollectionManager.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/dspace/service/ICollectionManager.java
@@ -2,10 +2,9 @@ package edu.asu.spring.quadriga.dspace.service;
 
 import org.springframework.web.client.RestTemplate;
 
-import edu.asu.spring.quadriga.domain.ICollection;
 
 public interface ICollectionManager {
 
-	public abstract ICollection testRestGET(RestTemplate restTemplate, String url, String sUserName, String sPassword);
+	public abstract IDspaceCollection testRestGET(RestTemplate restTemplate, String url, String sUserName, String sPassword);
 
 }
---
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/dspace/service/IDspaceCollectionEntityId.java
@@ -0,0 +1,9 @@
+package edu.asu.spring.quadriga.dspace.service;
+
+public interface IDspaceCollectionEntityId {
+
+	public abstract String getId();
+
+	public abstract void setId(String id);
+
+}
