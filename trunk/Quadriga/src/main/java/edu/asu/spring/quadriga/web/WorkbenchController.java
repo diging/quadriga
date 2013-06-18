54fb1e9a419063f70325da450e71e17e875f9136
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-18 16:27:04 +0000

    Moved the controller methods related to Dspace from Workbench Controller to Dspace Controller

--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/WorkbenchController.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/WorkbenchController.java
@@ -62,8 +62,7 @@ public class WorkbenchController {
 	@Autowired
 	ICollaboratorFactory collaboratorFactory;
 
-	@Autowired
-	private IDspaceManager dspaceManager;
+
 
 	/**
 	 * @description this method acts as a controller for handling all the activities on the workbench
@@ -365,87 +364,6 @@ public class WorkbenchController {
 			}
 		}
 	}
-	
-	/**
-	 * Simply selects the workspace communities view to render by returning its path.
-	 */
-	@RequestMapping(value = "/auth/workbench/workspace", method = RequestMethod.GET)
-	public String workspaceRequest(ModelMap model, Principal principal) {
-		return "redirect:/auth/workbench/workspace/communities";
-	}
-
-	@RequestMapping(value = "/auth/workbench/workspace/communities", method = RequestMethod.GET)
-	public String workspaceCommunityListRequest(ModelMap model, Principal principal) {
-		String sPassword = (String)SecurityContextHolder.getContext().getAuthentication().getCredentials();
-
-		List<ICommunity> communities = dspaceManager.getAllCommunities(principal.getName(),sPassword);
-
-		model.addAttribute("communityList", communities);
-
-		return "auth/workbench/workspace/communities";
-	}
-	
-	@RequestMapping(value = "/auth/workbench/workspace/communities-collections", method = RequestMethod.GET)
-	public @ResponseBody String getCommunitiesAndCollections() {
-		String sUserName = (String)SecurityContextHolder.getContext().getAuthentication().getName();
-		String sPassword = (String)SecurityContextHolder.getContext().getAuthentication().getCredentials();
-		StringBuilder sResult = new StringBuilder();
-		List<ICommunity> communities = dspaceManager.getAllCommunities(sUserName,sPassword);
-		
-		if(communities.size()>0)
-		{
-			sResult.append("<h2>Communities in HPS Repository</h2>");
-			sResult.append("<span class=\"byline\">Select a community to browse its collections.</span>");
-			sResult.append("<span style=\"font-weight: bold\">");
-			for(ICommunity community: communities)
-			{
-				sResult.append("<a href='/quadriga/auth/workspace/community/"+community.getId()+"'>");
-				sResult.append(community.getName());
-				sResult.append("</a>");
-				sResult.append("<br />");
-			}
-			sResult.append("</span>");
-		}
-		else
-		{
-			sResult.append("<h2>No Communities in HPS Repository</h2>");
-		}
-		
-		return sResult.toString();
-	}
-	
-
-	@RequestMapping(value = "/auth/workbench/workspace/community/{communityTitle}", method = RequestMethod.GET)
-	public String workspaceCommunityRequest(@PathVariable("communityTitle") String communityTitle, ModelMap model, Principal principal) {
-
-		String sPassword = (String)SecurityContextHolder.getContext().getAuthentication().getCredentials();
-		List<IDspaceCollection> collections = dspaceManager.getAllCollections(principal.getName(),sPassword, communityTitle);
-
-		model.addAttribute("communityTitle", communityTitle);
-		model.addAttribute("collectionList", collections);
-
-		return "auth/workbench/workspace/community/collection";
-	}
-
-
-	/*@RequestMapping(value = "auth/workbench/{projectid}/addcollaborator", method = RequestMethod.GET)
-	public ModelAndView addCollaboratorForm()
-	{
-		return new ModelAndView("auth/workbench/{projectid}/addcollaborator","command", collaboratorFactory.createCollaborator());
-<<<<<<< .mine
-	}*/
-
-
-
-//	@RequestMapping(value = "/auth/workbench/workspace/ajaxtest", method = RequestMethod.GET)
-//	public @ResponseBody String getTime() {
-//
-//		Random rand = new Random();
-//		float r = rand.nextFloat() * 100;
-//		String result = "<br>The Random # is <b>" + r + "</b>. Generated on <b>" + new Date().toString() + "</b>";
-//		System.out.println("Debug Message from Spring-Ajax-JQuery Controller.." + new Date().toString());
-//		return result;
-//	}
 
 }
 
