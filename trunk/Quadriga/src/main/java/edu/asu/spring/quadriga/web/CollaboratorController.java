e0146f1877f0ac57cd3283704e78bd247d267984
jdamerow <jdamerow@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-18 01:34:00 +0000

    add collaborators

--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/CollaboratorController.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/CollaboratorController.java
@@ -1,22 +1,30 @@
 package edu.asu.spring.quadriga.web;
 
+import java.beans.PropertyEditorSupport;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.catalina.mbeans.RoleMBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.asu.spring.quadriga.domain.ICollaborator;
+import edu.asu.spring.quadriga.domain.ICollaboratorRole;
 import edu.asu.spring.quadriga.domain.IProject;
 import edu.asu.spring.quadriga.domain.IUser;
 import edu.asu.spring.quadriga.domain.factories.ICollaboratorFactory;
 import edu.asu.spring.quadriga.domain.factories.IUserFactory;
-import edu.asu.spring.quadriga.domain.implementation.Collaborator;
+import edu.asu.spring.quadriga.service.ICollaboratorRoleMapper;
 import edu.asu.spring.quadriga.service.IProjectManager;
 import edu.asu.spring.quadriga.service.IUserManager;
 
@@ -35,6 +43,35 @@ public class CollaboratorController {
 	@Autowired
 	ICollaboratorFactory collaboratorFactory;
 	
+	@Autowired
+	private ICollaboratorRoleMapper collaboratorRoleManager;
+	
+	@InitBinder
+	protected void initBinder(HttpServletRequest request, ServletRequestDataBinder binder) throws Exception {
+	    binder.registerCustomEditor(IUser.class, "userObj", new PropertyEditorSupport() {
+	    @Override
+	    public void setAsText(String text) {
+	        IUser user = usermanager.getUserDetails(text);
+	        setValue(user);
+	    }
+	    });
+	    
+	    binder.registerCustomEditor(List.class, "collaboratorRoles", new PropertyEditorSupport() {
+
+			@Override
+			public void setAsText(String text) {
+				String[] roleIds = text.split(",");
+				List<ICollaboratorRole> roles = new ArrayList<ICollaboratorRole>();
+				for (String roleId : roleIds) {
+					ICollaboratorRole role = collaboratorRoleManager.getCollaboratorRoleId(roleId.trim());
+					roles.add(role);
+				}
+				setValue(roles);
+			}
+	    	
+	    });
+	}
+	
 	@RequestMapping(value = "auth/workbench/{projectid}/showCollaborators", method = RequestMethod.GET)
 	public String displayCollaborator(@PathVariable("projectid") int projectid, ModelMap model){
 
@@ -49,7 +86,15 @@ public class CollaboratorController {
 		model.addAttribute("collaborator", collaborator);
 
 		List<IUser> notCollaboratingUsers = projectmanager.getNotCollaboratingUsers(projectid);
+//		IUser user = userFactory.createUserObject();
+//		user.setName("test name");
+//		user.setUserName("testid");
+//		notCollaboratingUsers.add(user);
+		
 		model.addAttribute("notCollaboratingUsers", notCollaboratingUsers);
+		model.addAttribute("possibleCollaboratorRoles", collaboratorRoleManager.getCollaboratorRoles());
+		
+		
 		
 		return "auth/workbench/showCollaborators";
 
@@ -81,6 +126,7 @@ public class CollaboratorController {
 		}
 		model.addAttribute("projectid", id);  */
 		System.out.println("user " + collaborator.getUserObj().getUserName());
+		System.out.println("roles " + collaborator.getCollaboratorRoles());
 		
 		return "redirect:/auth/workbench/{projectid}"; 
 	}
