cfb39ddb29a58548c2f9f78587431bceeb82dd31
dlohith <dlohith@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-20 21:47:06 +0000

    Updated DB Script for unix box

--- Quadriga/Database/DBInstallationScript.pl
+++ Quadriga/Database/DBInstallationScript.pl
@@ -1,3 +1,7 @@
+#
+#	Script to create the DB and schema
+#
+
 use Cwd qw();
 
 my ($line, $userName,$dbName);
@@ -17,24 +21,24 @@ while($line=<MYFILE>){
 close(MYFILE);
 
 unlink("./createDB.txt");
-unlink("./dbSP.txt");
-unlink("./dbTables.txt");
-unlink("./dbViews.txt");
+unlink("./createDBSchema.txt");
 
 my $loc = Cwd::cwd();
 
-#	Creates a file for granting permissions to $userName
-open (OUTFILE, '>grantPerm.txt');
-print OUTFILE "grant all privileges on $dbName.* to '".$userName."'\@'localhost'\n";
+#       Creates a file for creating Database
+open (OUTFILE, '>createDB.txt');
+print OUTFILE "create database if not exists $dbName;\n";
 close(OUTFILE);
 
-#	Creates a file for creating Database
-open (OUTFILE, '>createDB.txt');
-print OUTFILE "create database if not exists $dbName\n";
+
+#	Creates a file for granting permissions to $userName
+open (OUTFILE, '>>createDB.txt');
+print OUTFILE "grant all privileges on $dbName.* to '".$userName."'\@'localhost';\n";
 close(OUTFILE);
+
 
 #	Creates a file for creating tables
-open (OUTFILE, '>dbTables.txt');
+open (OUTFILE, '>>createDBSchema.txt');
 @files = <./Tables/*>;
 
 foreach $file (@files) {
@@ -43,7 +47,7 @@ foreach $file (@files) {
 close(OUTFILE);
 
 #	Creates a file for creating views
-open (OUTFILE, '>dbViews.txt');
+open (OUTFILE, '>>createDBSchema.txt');
 @files = <./Views/*>;
 
 foreach $file (@files) {
@@ -52,7 +56,7 @@ foreach $file (@files) {
 close(OUTFILE);
 
 #	Creates a file for creating stored procedures
-open (OUTFILE, '>dbSP.txt');
+open (OUTFILE, '>>createDBSchema.txt');
 @files = <./StoredProcedures/*>;
 
 foreach $file (@files) {
@@ -62,22 +66,12 @@ close(OUTFILE);
 
 print "Creating Database : $dbName schema\n";
 system("sudo mysql < ./createDB.txt >> dbCreation.log 2>&1");
-print "Assigning permission to user : $userName on Database : $dbName schema\n";
-system("sudo mysql < ./grantPerm.txt >> dbCreation.log 2>&1");
 print "Creating tables on the database : $dbName\n";
 print "Please enter your [$userName] user password when prompted\n";
-system("sudo mysql --user=$userName --password $dbName < ./dbTables.txt  >> dbCreation.log 2>&1" );
-print "Creating views on the database : $dbName\n";
-print "Please enter your [$userName] user password when prompted\n";
-system("sudo mysql --user=$userName --password $dbName < ./dbViews.txt  >> dbCreation.log 2>&1" );
-print "Creating stored procedures on the database : $dbName\n";
-print "Please enter your [$userName] user password when prompted\n";
-system("sudo mysql  --user=$userName --password $dbName < ./dbSP.txt  >> dbCreation.log 2>&1" );
+system("sudo mysql --user=$userName --password $dbName < ./createDBSchema.txt  >> dbCreation.log 2>&1" );
 
 # Cleaning the file created by the script
 
 unlink("./createDB.txt");
-unlink("./grantPerm.txt");
-unlink("./dbSP.txt");
-unlink("./dbTables.txt");
-unlink("./dbViews.txt");
+unlink("./createDBSchema.txt");
+
