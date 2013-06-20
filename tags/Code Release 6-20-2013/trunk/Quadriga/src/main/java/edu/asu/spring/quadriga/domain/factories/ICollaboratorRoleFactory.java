0e91150fe8f1c991858d83486afde13b8d526719
dlohith <dlohith@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-20 22:16:32 +0000

    moving

--- tags/Code Release 6-20-2013/trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/domain/factories/ICollaboratorRoleFactory.java
+++
@@ -1,9 +0,0 @@
-package edu.asu.spring.quadriga.domain.factories;
-
-import edu.asu.spring.quadriga.domain.ICollaboratorRole;
-
-public interface ICollaboratorRoleFactory {
-	
-	public abstract ICollaboratorRole createCollaboratorRoleObject();
-
-}
--- tags/Code Release 6-20-2013/trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/domain/factories/IConceptFactory.java
+++
@@ -1,8 +0,0 @@
-package edu.asu.spring.quadriga.domain.factories;
-
-import edu.asu.spring.quadriga.domain.IConcept;
-
-public interface IConceptFactory {
-
-	public abstract IConcept createConceptObject();
-}
