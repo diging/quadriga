0af9dee6ff474845ac96adcdf88e1e948849bfad
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-20 23:26:24 +0000

    List the available Dspace communities and collections in two separate pages.

--- Quadriga/src/main/java/edu/asu/spring/quadriga/dspace/service/IDspaceManager.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/dspace/service/IDspaceManager.java
@@ -2,6 +2,8 @@ package edu.asu.spring.quadriga.dspace.service;
 
 import java.util.List;
 
+import org.springframework.web.client.RestTemplate;
+
 import edu.asu.spring.quadriga.domain.ICollection;
 import edu.asu.spring.quadriga.domain.ICommunity;
 
@@ -9,8 +11,10 @@ public interface IDspaceManager {
 
 	public abstract List<ICommunity> getAllCommunities(String sUserName, String sPassword);
 
-	public abstract List<IDspaceCollection> getAllCollections(String sUserName, String sPassword,String sCommunityTitle);
+	public abstract List<ICollection> getAllCollections(String sUserName, String sPassword,String sCommunityId);
 
+	public abstract String getCommunityName(String sCommunityId);
+	
 	public abstract ICollection getCollection(String sCollectionId);
 
 }
