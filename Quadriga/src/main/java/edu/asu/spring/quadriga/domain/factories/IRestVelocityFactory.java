7c3a00b7d1c5ac5464443a9e260f4058046f9ab7
dlohith <dlohith@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-24 18:38:13 +0000

    Rest velocity factories and some changes to URL in spring

---
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/domain/factories/IRestVelocityFactory.java
@@ -0,0 +1,10 @@
+package edu.asu.spring.quadriga.domain.factories;
+
+import org.apache.velocity.app.VelocityEngine;
+
+public interface IRestVelocityFactory {
+
+	//@Override
+	public abstract VelocityEngine RestVelocityFactory();
+
+}
--- Quadriga/src/main/java/edu/asu/spring/quadriga/web/RestController.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/RestController.java
@@ -1,27 +1,14 @@
 package edu.asu.spring.quadriga.web;
 
-import java.io.IOException;
 import java.io.StringWriter;
-import java.io.Writer;
-
 import java.util.List;
-
-import javax.xml.bind.JAXBContext;
-
-import javax.xml.bind.JAXBException;
-import javax.xml.transform.stream.StreamResult;
-
-
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.Velocity;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.exception.MethodInvocationException;
 import org.apache.velocity.exception.ParseErrorException;
 import org.apache.velocity.exception.ResourceNotFoundException;
-import org.apache.velocity.runtime.RuntimeConstants;
-import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,12 +18,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import edu.asu.spring.quadriga.domain.IProject;
 import edu.asu.spring.quadriga.domain.factories.IDictionaryFactory;
+import edu.asu.spring.quadriga.domain.factories.IRestVelocityFactory;
 import edu.asu.spring.quadriga.domain.factories.impl.DictionaryItemsFactory;
-import edu.asu.spring.quadriga.domain.implementation.WordpowerReply;
 import edu.asu.spring.quadriga.domain.rest.ProjectList;
 import edu.asu.spring.quadriga.exceptions.QuadrigaStorageException;
 import edu.asu.spring.quadriga.service.IDictionaryManager;
@@ -46,14 +32,13 @@ import edu.asu.spring.quadriga.service.IUserManager;
 @Controller
 public class RestController {
 
-
-
-	@Autowired 
+	@Autowired
 	IDictionaryManager dictonaryManager;
 
-	private static final Logger logger = LoggerFactory.getLogger(RestController.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(RestController.class);
 
-	@Autowired 
+	@Autowired
 	IUserManager usermanager;
 
 	@Autowired
@@ -62,58 +47,55 @@ public class RestController {
 	@Autowired
 	IProjectManager projectManager;
 
-	@Autowired 
+	@Autowired
 	IDictionaryFactory dictionaryFactory;
 
-	@Autowired 
+	@Autowired
 	DictionaryItemsFactory dictionaryItemsFactory;
-
 	
+	@Autowired
+	IRestVelocityFactory restVelocityFactory;
 
-
-	@RequestMapping(value="api/projects/{userID}", method = RequestMethod.GET , produces = "application/xml")
+	@RequestMapping(value = "rest/projects/{userID}", method = RequestMethod.GET, produces = "application/xml")
 	@ResponseBody
-	public String listProjects(@PathVariable("userID") String userId, ModelMap model){	
-		List<IProject> projectList=null;
-		VelocityEngine engine = new VelocityEngine();
-        engine.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
-        engine.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
-        
+	public String listProjects(@PathVariable("userID") String userId,
+			ModelMap model) {
+		List<IProject> projectList = null;
+		VelocityEngine engine = restVelocityFactory.RestVelocityFactory();
+		
 		Template template = null;
-		//add
+		// add
 		ProjectList projectListTest = new ProjectList();
 		StringWriter sw = new StringWriter();
 		try {
 			engine.init();
 			projectList = projectManager.getProjectsOfUser(userId);
 			projectListTest.SetProjectList(projectList);
-			template = engine.getTemplate("mytemplate.vm");
-		        VelocityContext context = new VelocityContext();
-		        context.put("list", projectList);
-		        
-		        StringWriter writer = new StringWriter();
-		        template.merge( context, writer );
-		        return writer.toString(); 
-			} catch (ResourceNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ParseErrorException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (MethodInvocationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-			
-			
-			catch (QuadrigaStorageException e) {
+			template = engine.getTemplate("velocitytemplates/projectlist.vm");
+			VelocityContext context = new VelocityContext();
+			context.put("list", projectList);
+
+			StringWriter writer = new StringWriter();
+			template.merge(context, writer);
+			return writer.toString();
+		} catch (ResourceNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		System.out.println(""+sw);
+		} catch (ParseErrorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MethodInvocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		catch (QuadrigaStorageException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return "";
 	}
 }
