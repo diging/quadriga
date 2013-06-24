db545bb3f973b3b89ab8d6404aa9257f4f073d1a
sboddu1 <sboddu1@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-24 18:36:51 +0000

    Deleting the castor marhalling file and organised project.java

--- Quadriga/src/main/java/edu/asu/spring/quadriga/domain/implementation/Project.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/domain/implementation/Project.java
@@ -2,14 +2,6 @@ package edu.asu.spring.quadriga.domain.implementation;
 
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
-import javax.xml.bind.annotation.XmlType;
-import javax.xml.bind.annotation.adapters.XmlAdapter;
-
 import org.springframework.stereotype.Service;
 
 import edu.asu.spring.quadriga.domain.ICollaborator;
@@ -17,60 +9,48 @@ import edu.asu.spring.quadriga.domain.IProject;
 import edu.asu.spring.quadriga.domain.IUser;
 import edu.asu.spring.quadriga.domain.enums.ENetworkAccessibility;
 import edu.asu.spring.quadriga.domain.enums.EProjectAccessibility;
-import edu.asu.spring.quadriga.dspace.service.IDspaceCollection;
-import edu.asu.spring.quadriga.dspace.service.impl.DspaceCollection;
 
 /**
- * @description : Project class describing the properties 
- *                of a Project object
+ * @description : Project class describing the properties of a Project object
  * 
- * @author      : Kiran Kumar Batna
- *
+ * @author : Kiran Kumar Batna
+ * 
  */
 @Service
-public class Project implements IProject 
-{
-	
+public class Project implements IProject {
 	private String name;
-	
 	private String description;
-	
 	private String id;
-	
 	private int internalid;
-	
 	private List<ICollaborator> collaborators;
-	
-    private EProjectAccessibility projectAccess;
-	
-    private ENetworkAccessibility networksDefaultAccess;
-	
-    private IUser owner;
+	private EProjectAccessibility projectAccess;
+	private ENetworkAccessibility networksDefaultAccess;
+	private IUser owner;
 
-    /**
-     * retrieves the name of the project
-     */
+	/**
+	 * retrieves the name of the project
+	 */
 	@Override
 	public String getName() {
 		return name;
 	}
-    /**
-     * assigns the name of the project to the supplied 
-     * variable.
-     */
+
+	/**
+	 * assigns the name of the project to the supplied variable.
+	 */
 	@Override
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-    /**
-     * retrieves the description of the project
-     */
+
+	/**
+	 * retrieves the description of the project
+	 */
 	@Override
 	public String getDescription() {
 		return description;
 	}
-	
+
 	/**
 	 * assigns the description of the project
 	 */
@@ -79,97 +59,101 @@ public class Project implements IProject
 		this.description = description;
 	}
 
-    /**
-     * retrieves the Unix ID of the project
-     */
+	/**
+	 * retrieves the Unix ID of the project
+	 */
 	@Override
 	public String getId() {
 		return id;
 	}
-	
-    /**
-     * assigns the Unix ID of the project
-     */
+
+	/**
+	 * assigns the Unix ID of the project
+	 */
 	@Override
 	public void setId(String id) {
 		this.id = id;
 	}
 
-    /**
-     * retrieves the internal id of the project
-     */
+	/**
+	 * retrieves the internal id of the project
+	 */
 	@Override
 	public int getInternalid() {
 		return internalid;
 	}
 
-    /**
-     * assigns the internal id of the project
-     */
+	/**
+	 * assigns the internal id of the project
+	 */
 	@Override
 	public void setInternalid(int internalid) {
 		this.internalid = internalid;
 	}
-	 /**
-     * retrieves the owner of the project
-     */
+
+	/**
+	 * retrieves the owner of the project
+	 */
 	@Override
 	public IUser getOwner() {
 		return owner;
 	}
-    
+
 	/**
-     * assigns the owner of the project
-     */
+	 * assigns the owner of the project
+	 */
 	@Override
 	public void setOwner(IUser owner) {
 		this.owner = owner;
 	}
 
-    /**
-     * retrieves the collaborators of the project
-     */
+	/**
+	 * retrieves the collaborators of the project
+	 */
 	@Override
 	public List<ICollaborator> getCollaborators() {
 		return collaborators;
 	}
 
-    /**
-     * assigns the collaborators of the project
-     */
+	/**
+	 * assigns the collaborators of the project
+	 */
 	@Override
 	public void setCollaborators(List<ICollaborator> collaborators) {
 		this.collaborators = collaborators;
 	}
-	
-    /**
-     * retrieves the accessibility of the project
-     */
+
+	/**
+	 * retrieves the accessibility of the project
+	 */
 	@Override
 	public EProjectAccessibility getProjectAccess() {
 		return projectAccess;
 	}
-	
-    /**
-     * assigns the accessibility of the project
-     */
+
+	/**
+	 * assigns the accessibility of the project
+	 */
 	@Override
 	public void setProjectAccess(EProjectAccessibility projectAccess) {
 		this.projectAccess = projectAccess;
 	}
-    /**
-     * retrieves the networks access of the project
-     */
+
+	/**
+	 * retrieves the networks access of the project
+	 */
 	@Override
 	public ENetworkAccessibility getNetworksDefaultAccess() {
 		return networksDefaultAccess;
 	}
-    /**
-     * assigns the network access of the project
-     */
+
+	/**
+	 * assigns the network access of the project
+	 */
 	@Override
-	public void setNetworksDefaultAccess(ENetworkAccessibility networksDefaultAccess) {
+	public void setNetworksDefaultAccess(
+			ENetworkAccessibility networksDefaultAccess) {
 		this.networksDefaultAccess = networksDefaultAccess;
 	}
-	
+
 }
--- Quadriga/src/main/java/edu/asu/spring/quadriga/domain/rest/ProjectList.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/domain/rest/ProjectList.java
@@ -1,24 +1,14 @@
 package edu.asu.spring.quadriga.domain.rest;
 
 import java.util.List;
-
 import edu.asu.spring.quadriga.domain.IProject;
 
-
-
-
 public class ProjectList {
-	 
-	 
 	 protected List<IProject> projectList;
-	 
 	 public List<IProject> getProjectList() {
 	     return this.projectList;
 	 }
 	 public void SetProjectList(List<IProject> projectList) {
 	     this.projectList=projectList;
 	 }
-
-
-	
 }
