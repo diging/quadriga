4e9ebfbc4dfc8be75b111d7b5954c37f33d9c81a
rpendbha <rpendbha@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-25 23:08:49 +0000

    added stored procedure for adding collaborators in conceptcollection components, showing conceptcollection collaborators, showing non collaborators

--- Quadriga/Database/StoredProcedures/sp_getConceptCollectionsCollaborators.sql
+++ Quadriga/Database/StoredProcedures/sp_getConceptCollectionsCollaborators.sql
@@ -17,38 +17,38 @@ DROP PROCEDURE IF EXISTS sp_getConceptCollectionCollaborators;
 DELIMITER $$
 CREATE PROCEDURE sp_getConceptCollectionCollaborators
 (
-  IN incollectionname  VARCHAR(20),
+  IN incollectionid  INT,
   OUT errmsg     VARCHAR(255)
 )
 BEGIN
     -- declare local variables
-     DECLARE varcollectionid   INT DEFAULT 0;
+  --   DECLARE varcollectionname VARCHAR(5) ;
 
 	-- the error handler for any sql exception
     DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
       SET errmsg = "SQL exception has occurred";
 
     -- check input variables
-    IF(incollectionname IS NULL OR incollectionname = "")
-     THEN SET errmsg = "collection name cannot be empty.";
+    IF(incollectionid IS NULL OR incollectionid = "")
+     THEN SET errmsg = "collection id cannot be empty.";
     END IF;
     
-    IF NOT EXISTS (SELECT 1 FROM vw_comceptcollections_collaborator
-                     WHERE collectionname = incollectionname)
-      THEN SET errmsg = "collection name is invalid.";
-    END IF;
+   -- IF NOT EXISTS (SELECT 1 FROM vw_conceptcollections_collaborator
+   --                  WHERE collectionname = incollectionname)
+   --   THEN SET errmsg = "collection name is invalid.";
+   -- END IF;
 
     IF (errmsg IS NULL)
      THEN SET errmsg = "";
       -- retrieve the collection id of the project 
-      SELECT id INTO varcollectionid FROM vw_conceptcollections
-        WHERE collectionname = incollectionname; 
+    -- SELECT collectionname INTO varcollectionname FROM vw_conceptcollections
+     --   WHERE id = incollectionid; 
       
       -- retrieve the collaborator details
       SELECT collectionid,collaboratoruser, 
          GROUP_CONCAT(collaboratorrole SEPARATOR ',')  AS 'Collaboratorrole'
         FROM vw_conceptcollections_collaborator
-	    WHERE collectionid = varcollectionid;
+	    WHERE collectionid = incollectionid;
       
      END IF;
 END$$
