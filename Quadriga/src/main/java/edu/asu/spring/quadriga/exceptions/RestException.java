bc5de63ec23034f54aedf601ff32d8df4108fa57
jdamerow <jdamerow@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-26 08:02:11 +0000

    REST exception and handler

---
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/exceptions/RestException.java
@@ -0,0 +1,22 @@
+package edu.asu.spring.quadriga.exceptions;
+
+/**
+ * Exception to handle exceptions thrown in the REST interface.
+ * @author Julia Damerow
+ *
+ */
+public class RestException extends Exception {
+
+	
+	private static final long serialVersionUID = 8092941011459848011L;
+
+	public RestException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public RestException(Throwable arg0) {
+		super(arg0);
+	}
+
+	
+}
