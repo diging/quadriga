3e81e837895ec5cb635605326dbaf9cc1d2ac9ff
jdamerow <jdamerow@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-26 08:02:11 +0000

    REST exception and handler

---
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/exceptions/QuadrigaRestExceptionHandler.java
@@ -0,0 +1,14 @@
+package edu.asu.spring.quadriga.exceptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class QuadrigaRestExceptionHandler {
+
+	private static final Logger logger = LoggerFactory.getLogger(QuadrigaRestExceptionHandler.class);
+	
+	
+
+}
