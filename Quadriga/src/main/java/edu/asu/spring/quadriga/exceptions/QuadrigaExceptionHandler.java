92c268448c2c75e66d45e3f9b458fec170483f55
sboddu1 <sboddu1@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-06-27 20:52:59 +0000

    Created .vm files and schema file for error xml response and also mapped it in QuadrigaExceptionHandler

--- Quadriga/src/main/java/edu/asu/spring/quadriga/exceptions/QuadrigaExceptionHandler.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/exceptions/QuadrigaExceptionHandler.java
@@ -1,10 +1,25 @@
 package edu.asu.spring.quadriga.exceptions;
 
+import java.io.StringWriter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.MethodInvocationException;
+import org.apache.velocity.exception.ParseErrorException;
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import edu.asu.spring.quadriga.domain.factories.IRestVelocityFactory;
 
 /**
  * This class handles exceptions thrown in Controller classes.
@@ -15,6 +30,8 @@ import org.springframework.web.servlet.ModelAndView;
 @ControllerAdvice
 public class QuadrigaExceptionHandler {
 
+	@Autowired
+	private IRestVelocityFactory restVelocityFactory;
 	private static final Logger logger = LoggerFactory.getLogger(QuadrigaExceptionHandler.class);
 	/**
 	 * For now this method handles all exceptions thrown in Controller classes. Eventually this method can be
@@ -42,6 +59,41 @@ public class QuadrigaExceptionHandler {
 		logger.error(ex.getMessage(), ex);
 		return modelAndView;
 	}
-
 	
+	@RequestMapping(produces="application/xml")
+	@ExceptionHandler(RestException.class)
+	@ResponseBody
+	public String handleRestException(RestException ex, HttpServletRequest req) {
+		logger.error("Exception:", ex);
+		VelocityEngine engine=null;
+		Template template = null;
+		StringWriter sw = new StringWriter();
+		
+		try {
+			engine = restVelocityFactory.getVelocityEngine(req);
+			engine.init();
+			
+			template = engine
+					.getTemplate("velocitytemplates/resterror.vm");
+			VelocityContext context = new VelocityContext(restVelocityFactory.getVelocityContext());
+			context.put("status", "ERROR");
+			context.put("message",ex.getMessage());
+			template.merge(context, sw);
+			return sw.toString();
+		} catch (ResourceNotFoundException e) {
+			logger.error("Exception:", e);
+			
+		} catch (ParseErrorException e) {
+			logger.error("Exception:", e);
+			
+		} catch (MethodInvocationException e) {
+			logger.error("Exception:", e);
+			
+		}  
+		catch (Exception e) {
+			logger.error("Exception:", e);
+			
+		}
+		return sw.toString();
+	}
 }
