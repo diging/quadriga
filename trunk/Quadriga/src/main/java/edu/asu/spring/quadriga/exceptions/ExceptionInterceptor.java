98297e8f74216249604a9af5a278cbba86195dfe
jdamerow <jdamerow@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-07-06 18:49:08 +0000

    aspects are running

--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/exceptions/ExceptionInterceptor.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/exceptions/ExceptionInterceptor.java
@@ -11,32 +11,26 @@ import org.springframework.stereotype.Component;
 
 /**
  * @author satyaswaroop boddu
- *
+ * 
  */
 @Aspect
 @Component
 public class ExceptionInterceptor {
-	
-	private static final Logger logger = LoggerFactory.getLogger(ExceptionInterceptor.class);
-	 @AfterThrowing(pointcut="execution(* *.*(..))", throwing="t")
-	  public void toRuntimeException(Throwable t)  {
-		 logger.info("In Exeception Interpreter");
-	    /*if (t instanceof QuadrigaAcessException) {
-	      throw (QuadrigaAcessException) t;
-		}
-	    else if(t instanceof QuadrigaStorageException)
-	    {
-	    	 throw (QuadrigaStorageException) t;
-	    }
-	    else if(t instanceof RestException)
-	    {
-	    	throw (RestException) t;
-	    }
-	    else {
-	      QuadrigaException se = new QuadrigaException(t.getMessage());
-	      se.setStackTrace(t.getStackTrace());				
-	      throw se;
-	    }*/
-	  }
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(ExceptionInterceptor.class);
+
+	@AfterThrowing(pointcut = "within(edu.asu.spring.quadriga.web.*)", throwing = "t")
+	public void toRuntimeException(Throwable t) {
+		logger.info("In Exeception Interpreter");
+		/*
+		 * if (t instanceof QuadrigaAcessException) { throw
+		 * (QuadrigaAcessException) t; } else if(t instanceof
+		 * QuadrigaStorageException) { throw (QuadrigaStorageException) t; }
+		 * else if(t instanceof RestException) { throw (RestException) t; } else
+		 * { QuadrigaException se = new QuadrigaException(t.getMessage());
+		 * se.setStackTrace(t.getStackTrace()); throw se; }
+		 */
+	}
 
 }
