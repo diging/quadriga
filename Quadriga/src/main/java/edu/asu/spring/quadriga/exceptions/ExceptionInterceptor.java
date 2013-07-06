d81a94c2ddddf270a6564d89b23a139c900d45a7
sboddu1 <sboddu1@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-07-06 02:17:30 +0000

    Changes for aspect but still unable to log the error need to further check

--- Quadriga/src/main/java/edu/asu/spring/quadriga/exceptions/ExceptionInterceptor.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/exceptions/ExceptionInterceptor.java
@@ -7,19 +7,21 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 /**
  * @author satyaswaroop boddu
  *
  */
 @Aspect
+@Component
 public class ExceptionInterceptor {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ExceptionInterceptor.class);
 	 @AfterThrowing(pointcut="execution(* *.*(..))", throwing="t")
-	  public void toRuntimeException(Throwable t) throws QuadrigaAcessException, QuadrigaStorageException, RestException, QuadrigaException {
+	  public void toRuntimeException(Throwable t)  {
 		 logger.info("In Exeception Interpreter");
-	    if (t instanceof QuadrigaAcessException) {
+	    /*if (t instanceof QuadrigaAcessException) {
 	      throw (QuadrigaAcessException) t;
 		}
 	    else if(t instanceof QuadrigaStorageException)
@@ -34,7 +36,7 @@ public class ExceptionInterceptor {
 	      QuadrigaException se = new QuadrigaException(t.getMessage());
 	      se.setStackTrace(t.getStackTrace());				
 	      throw se;
-	    }
+	    }*/
 	  }
 
 }
