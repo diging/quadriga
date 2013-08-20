fccb5574c170ec05416c1bbd8469c4d5353fa443
rpendbha <rpendbha@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-08-20 22:36:03 +0000

    created validator CollaboratorFormDeleteValidator.java class to validate deletion of collaborators from project. Modified code in ProjectCollaboratorDeleteController.java and showdeleteCollaborators.jsp to delete collaborators from projects.

--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/workbench/backing/ModifyCollaborator.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/workbench/backing/ModifyCollaborator.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import edu.asu.spring.quadriga.domain.ICollaboratorRole;
+import edu.asu.spring.quadriga.domain.implementation.Collaborator;
 
 @Service
-public class ModifyCollaborator 
+public class ModifyCollaborator  
 {
 	private String userName;
 	private String name;
