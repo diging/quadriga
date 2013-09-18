ba804e7c79ca291ea2e4ba17f78b6240d06465ab
kbatna <kbatna@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-09-18 20:29:20 +0000

    Update dictionary details

---
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/service/impl/dictionary/ModifyDictionaryManager.java
@@ -0,0 +1,25 @@
+package edu.asu.spring.quadriga.service.impl.dictionary;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import edu.asu.spring.quadriga.db.dictionary.IDBConnectionModifyDictionaryManager;
+import edu.asu.spring.quadriga.domain.IDictionary;
+import edu.asu.spring.quadriga.exceptions.QuadrigaStorageException;
+import edu.asu.spring.quadriga.service.dictionary.IModifyDictionaryManager;
+
+@Service
+public class ModifyDictionaryManager implements IModifyDictionaryManager 
+{
+	@Autowired
+	@Qualifier("DBConnectionModifyDictionaryManagerBean")
+	private IDBConnectionModifyDictionaryManager dbConnect;
+	
+	@Override
+	public void updateDictionaryDetailsRequest(IDictionary dictionary,String userName) throws QuadrigaStorageException
+	{
+		dbConnect.updateDictionaryRequest(dictionary, userName);
+	}
+
+}
