322714e82713fb83b40a01ff7fe609835787fd82
sowjanya2106 <sowjanya2106@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-10-17 00:49:27 +0000

    table added for network annotations

---
+++ Quadriga/Database/Tables/tbl_network_annotations.sql
@@ -0,0 +1,25 @@
+/*******************************************
+Name          : tbl_network_annotations
+
+Description   : Stores the annotaions of each node in a network
+
+Called By     : sp_
+
+Create By     : Sowjanya Ambati
+
+Modified Date : 10/16/2013
+
+********************************************/
+CREATE TABLE IF NOT EXISTS tbl_network_annotations
+(
+  networkid       varchar(50)  NOT NULL,
+  nodename    VARCHAR(50) NOT NULL,
+  annotaiontext    TEXT NOT NULL,
+  annotationid    VARCHAR(50) NOT NULL,
+  userid	VARCHAR(50) NOT NULL,
+  updatedby       VARCHAR(10)   NOT NULL,
+  updateddate     TIMESTAMP     NOT NULL,
+  createdby       VARCHAR(10)   NOT NULL,
+  createddate     DATETIME      NOT NULL,
+  PRIMARY KEY(annotationid)
+)
