4b2f216016784e0e9f47ac382234108bc26c9f46
kbatna <kbatna@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-11-14 20:23:40 +0000

    Implemented Hibernate methods for archive/activate and transfer workspace.

--- Quadriga/src/main/java/edu/asu/spring/quadriga/dao/workspace/IModifyWSManagerDAO.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/dao/workspace/IModifyWSManagerDAO.java
@@ -1,6 +1,5 @@
 package edu.asu.spring.quadriga.dao.workspace;
 
-import edu.asu.spring.quadriga.domain.IProject;
 import edu.asu.spring.quadriga.domain.IWorkSpace;
 import edu.asu.spring.quadriga.dto.QuadrigaUserDTO;
 import edu.asu.spring.quadriga.exceptions.QuadrigaStorageException;
@@ -9,5 +8,11 @@ public interface IModifyWSManagerDAO {
 
 	void addWorkSpaceRequest(IWorkSpace workSpace, String projectId)
 			throws QuadrigaStorageException;
+
+	public abstract QuadrigaUserDTO getUserDTO(String userName)
+			throws QuadrigaStorageException;
+
+	public abstract void transferWSOwnerRequest(String workspaceId, String oldOwner,
+			String newOwner, String collabRole) throws QuadrigaStorageException;
 
 }
