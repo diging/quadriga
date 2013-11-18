4e9b21948a12fb53e876197f8eb831c4c2462846
zambr0 <zambr0@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-11-18 19:53:18 +0000

    Create method signatures for the dbconnectionmanager

--- Quadriga/src/main/java/edu/asu/spring/quadriga/dao/sql/IUserManagerDAO.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/dao/sql/IUserManagerDAO.java
@@ -12,4 +12,20 @@ public interface IUserManagerDAO {
 
 	public abstract List<IUser> getUserRequests() throws QuadrigaStorageException;
 
+	public abstract IUser getUserDetails(String userid) throws QuadrigaStorageException;
+
+	public abstract List<IUser> getUsers(String userRoleId) throws QuadrigaStorageException;
+
+	public abstract List<IUser> getUsersNotInRole(String userRoleId) throws QuadrigaStorageException;
+
+	public abstract void deleteUser(String deleteUser, String adminUser, String adminRole, String deactivatedRole) throws QuadrigaStorageException;
+
+	public abstract int deactivateUser(String sUserId, String sDeactiveRoleDBId, String sAdminId) throws QuadrigaStorageException;
+
+	public abstract int updateUserRoles(String sUserId, String sRoles, String sAdminId)	throws QuadrigaStorageException;
+
+	public abstract int approveUserRequest(String sUserId, String sRoles, String sAdminId) throws QuadrigaStorageException;
+
+	public abstract int denyUserRequest(String sUserId, String sAdminId) throws QuadrigaStorageException;
+
 }
