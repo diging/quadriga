3e79300d3439368c1bbb2cf22a0b37762cdfaf2f
dlohith <dlohith@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-11-22 21:54:51 +0000

    making a network status interface

---
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/network/INetworkStatus.java
@@ -0,0 +1,9 @@
+package edu.asu.spring.quadriga.web.network;
+
+public interface INetworkStatus {
+
+	String PENDING = "PENDING";
+	String ASSIGNED = "ASSIGNED";
+	String APPROVED="APPROVED";
+	String REJECTED = "REJECTED";
+}
