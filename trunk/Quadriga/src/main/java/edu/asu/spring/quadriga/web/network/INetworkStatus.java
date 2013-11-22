b076201b3db47750bf82ecb660f93f2ae433da3b
dlohith <dlohith@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-11-22 22:48:04 +0000

    Adding somemore status

--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/network/INetworkStatus.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/network/INetworkStatus.java
@@ -6,4 +6,7 @@ public interface INetworkStatus {
 	public final static String ASSIGNED = "ASSIGNED";
 	public final static String APPROVED="APPROVED";
 	public final static String REJECTED = "REJECTED";
+	public final static int NOT_ARCHIVED = 0;
+	public final static int ARCHIVE_LEVEL_ONE = 1;
+	public final static int NOT_REACHABLE_ARCHIVE = 2;
 }
