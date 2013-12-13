d10f0825effba2f90c07640dfb80670721a9e5d0
dlohith <dlohith@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-12-13 18:07:51 +0000

    Update the java docs for the data base layer

--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/db/IDBConnectionEditorManager.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/db/IDBConnectionEditorManager.java
@@ -8,37 +8,121 @@ import edu.asu.spring.quadriga.exceptions.QuadrigaStorageException;
 
 public interface IDBConnectionEditorManager {
 
+	/**
+	 * Get networks which editor can assign to him/her 
+	 * @param user
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract List<INetwork> getEditorNetworkList(IUser user)
 			throws QuadrigaStorageException;
 
+	/**
+	 * Assign a network to an editor
+	 * @param networkId
+	 * @param user
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract String assignNetworkToUser(String networkId, IUser user)
 			throws QuadrigaStorageException;
 
+	/**
+	 * Get all the assigned networks of the editor
+	 * @param user
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract List<INetwork> getAssignNetworkOfUser(IUser user)
 			throws QuadrigaStorageException;
 
+	/**
+	 * Update the network status based on REJECT/APPEND/APPROVE
+	 * @param networkId
+	 * @param status
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract String updateNetworkStatus(String networkId, String status)
 			throws QuadrigaStorageException;
 
+	/**
+	 * Get all rejected networks of the user
+	 * @param user
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract List<INetwork> getRejectedNetworkOfUser(IUser user)
 			throws QuadrigaStorageException;
 
+	/**
+	 * Get all approved networks of the user
+	 * @param user
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract List<INetwork> getApprovedNetworkOfUser(IUser user)
 			throws QuadrigaStorageException;
 
+	/**
+	 * Get network list of other editors which has status finalized
+	 * @param user
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract List<INetwork> getfinishedNetworkListOfOtherEditors(IUser user)
 			throws QuadrigaStorageException;
 
+	/**
+	 * Get assigned networks of other editors
+	 * @param user
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract List<INetwork> getAssignedNetworkListOfOtherEditors(IUser user)
 			throws QuadrigaStorageException;
 
+	/**
+	 * update the network status 
+	 * @param networkId
+	 * @param status
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract String updateAssignedNetworkStatus(String networkId, String status)
 			throws QuadrigaStorageException;
 
+	/**
+	 * Add annotation to a network
+	 * @param networkId
+	 * @param nodeName
+	 * @param annotationText
+	 * @param userId
+	 * @param objectType
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract String addAnnotationToNetwork(String networkId, String nodeName,
 			String annotationText, String userId,String objectType)
 			throws QuadrigaStorageException;
+	
+	/**
+	 * Get all annotations of the network 
+	 * @param type
+	 * @param id
+	 * @param userid
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract String[] getAnnotation(String type, String id,String userid) throws QuadrigaStorageException;
+	
+	/**
+	 * Update annotation of networks
+	 * @param annotationId
+	 * @param annotationText
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract String updateAnnotationToNetwork(String annotationId,String annotationText ) throws QuadrigaStorageException;
 
 
--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/db/IDBConnectionNetworkManager.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/db/IDBConnectionNetworkManager.java
@@ -11,39 +11,126 @@ import edu.asu.spring.quadriga.exceptions.QuadrigaStorageException;
 public interface IDBConnectionNetworkManager {
 
 
+	/**
+	 * Add a new network into workspace
+	 * @param networkName
+	 * @param user
+	 * @param workspaceid
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract String addNetworkRequest(String networkName, IUser user, String workspaceid)
 			throws QuadrigaStorageException;
-
+	
+	/**
+	 * Add Network statements (like AE, RE ) associated to networks
+	 * @param networkId
+	 * @param id
+	 * @param type
+	 * @param isTop
+	 * @param user
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract String addNetworkStatement(String networkId, String id, String type,
 			String isTop, IUser user) throws QuadrigaStorageException;
 
+	/**
+	 * Get network status for vogon
+	 * @param networkName
+	 * @param user
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract INetwork getNetworkStatus(String networkName, IUser user)
 			throws QuadrigaStorageException;
 
+	/**
+	 * Get network list for a User
+	 * @param user
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract List<INetwork> getNetworkList(IUser user) throws QuadrigaStorageException;
 
+	/**
+	 * Get the project id for a workspace id
+	 * @param workspaceid
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract String getProjectIdForWorkspaceId(String workspaceid)
 			throws QuadrigaStorageException;
 
+	/**
+	 * Check if the network name is already used
+	 * @param networkName
+	 * @param user
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract boolean hasNetworkName(String networkName,IUser user) throws QuadrigaStorageException;
 
+	/**
+	 * Get the Relation events and appellation event in the outer part
+	 * @param networkId
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract List<INetworkNodeInfo> getNetworkTopNodes(String networkId)
 			throws QuadrigaStorageException;
 
+	/**
+	 * Archive the network statements when rejected and reuploaded networks
+	 * @param networkId
+	 * @param id
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract String archiveNetworkStatement(String networkId, String id)
 			throws QuadrigaStorageException;
 
+	/**
+	 * Get all the network statements for a network
+	 * @param networkId
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract List<INetworkNodeInfo> getAllNetworkNodes(String networkId)
 			throws QuadrigaStorageException;
 
+	/**
+	 * Archive the network
+	 * @param networkId
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract String archiveNetwork(String networkId) throws QuadrigaStorageException;
 
+	/**
+	 * Get the previous version on network
+	 * @param networkId
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract INetworkOldVersion getNetworkOldVersionDetails(String networkId)
 			throws QuadrigaStorageException;
 
+	/**
+	 * Get the top nodes of previous version of networks
+	 * @param networkId
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract List<INetworkNodeInfo> getNetworkOldVersionTopNodes(String networkId)
 			throws QuadrigaStorageException;
 
+	/**
+	 * Get network details based on network id
+	 * @param networkId
+	 * @return
+	 * @throws QuadrigaStorageException
+	 */
 	public abstract INetwork getNetworkDetails(String networkId)
 			throws QuadrigaStorageException;
 }
