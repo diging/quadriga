961a974c7fbeec3c77584b3398a726000dc9a436
dlohith <dlohith@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2013-12-13 20:50:41 +0000

    Added service layer to editor annotation controller

---
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/service/impl/EditingNetworkAnnotationManager.java
@@ -0,0 +1,49 @@
+package edu.asu.spring.quadriga.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.asu.spring.quadriga.db.IDBConnectionEditorManager;
+import edu.asu.spring.quadriga.exceptions.QuadrigaStorageException;
+import edu.asu.spring.quadriga.service.IEditingNetworkAnnotationManager;
+
+@Service
+public class EditingNetworkAnnotationManager implements IEditingNetworkAnnotationManager {
+
+	@Autowired
+	IDBConnectionEditorManager dbConnectionEditManager;
+	
+	/* (non-Javadoc)
+	 * @see edu.asu.spring.quadriga.service.impl.IEditingNetworkAnnotationManager#getAnnotation(java.lang.String, java.lang.String, java.lang.String)
+	 */
+	@Override
+	public String[] getAnnotation(String type, String id,String userid) throws QuadrigaStorageException{
+		String arr[] = null;
+		arr = dbConnectionEditManager.getAnnotation(type,id , userid);
+		return arr;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see edu.asu.spring.quadriga.service.impl.IEditingNetworkAnnotationManager#addAnnotationToNetwork(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String)
+	 */
+	@Override
+	public String addAnnotationToNetwork(String networkId, String nodeName,
+			String annotationText, String userId,String objectType)
+			throws QuadrigaStorageException{
+		String msg = dbConnectionEditManager.addAnnotationToNetwork(networkId, nodeName,
+				annotationText, userId,objectType);
+		return msg;
+	}
+	
+	/* (non-Javadoc)
+	 * @see edu.asu.spring.quadriga.service.impl.IEditingNetworkAnnotationManager#updateAnnotationToNetwork(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public String updateAnnotationToNetwork(String annotationId,String annotationText ) throws QuadrigaStorageException{
+		String msg = dbConnectionEditManager.updateAnnotationToNetwork(annotationId, annotationText);
+		return msg;
+	}
+	
+	
+}
