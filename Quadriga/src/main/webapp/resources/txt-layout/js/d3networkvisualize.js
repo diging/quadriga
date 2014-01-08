86b3733beb64ffce2a7023617ec351531e6222d6
sowjanya2106 <sowjanya2106@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2014-01-08 22:42:45 +0000

    cleaning the code for networks

--- Quadriga/src/main/webapp/resources/txt-layout/js/d3networkvisualize.js
+++ Quadriga/src/main/webapp/resources/txt-layout/js/d3networkvisualize.js
@@ -3,150 +3,119 @@ function d3init(json1, networkId, path) {
 	console.log("init");
 
 	var width = 960,
-	    height = 700;
+	height = 700;
 
 	var color = d3.scale.category20();
 
 	var force = d3.layout.force()
-	    .charge(-120)
-	    .linkDistance(100)
-	    .size([width, height]);
+	.charge(-120)
+	.linkDistance(100)
+	.size([width, height]);
 
-	 var svg = d3.select("#chart").append("svg")
-   .attr("width", width)
-  .attr("height", height);
+
+	var svg = d3.select("#chart").append("svg")
+	.attr("width", width)
+	.attr("height", height);
 
 	d3.json(path+"/resources/txt-layout/data.json", function(error, graph) {
-	  force
-	      .nodes(graph.nodes)
-	      .links(graph.links)
-	      .start();
+		force
+		.nodes(graph.nodes)
+		.links(graph.links)
+		.start();
 
-	  var link = svg.selectAll(".link")
-	      .data(graph.links)
-	    .enter().append("line")
-	      .attr("class", "link")
-	      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
-	  
-	  // Prepare the arrow
-	  svg.append("svg:defs").selectAll("marker")
-	  		.data(["succeeds", "from", "received", "sent", "redeemed", "same_owner", "owns", "transfers", "identifies"])
-	  		.enter().append("svg:marker")
-	  		.attr("id", String)
-	  		.attr("viewBox", "0 -5 10 10")
-	  		.attr("refX", 25)
-	  		.attr("refY", -1.5)
-	  		.attr("markerWidth", 6)
-	  		.attr("markerHeight", 6)
-	  		.attr("orient", "auto")
-	  		.append("svg:path")
-	  		.attr("d", "M0,-5L10,0L0,5");
-	  
-	  
-	  var path = svg.append("svg:g").selectAll("path")
-	    .data(force.links())
-	  .enter().append("path")
-	    .attr("class", "link")
-	    .attr("marker-end", function(d) { return "url(#" + d.value + ")"; });
-//	  var path = svg.append("g").selectAll("path")
-//	    .data(force.links())
-//	  .enter().append("path")
-//	    .attr("class", function(d) {//console.log(d.value); 
-//	    return "link " + d.value; })
-//	    .attr("marker-end", function(d) { return "url(#" + d.value + ")"; });
-	  
-//	  // Draw the arrow
-//	  link.attr("class", function(d)
-//	  {
-//		//  console.log(d.value)
-//	  	return "link " + d.value;
-//	  });
-//	   
-//	  // The end market
-//	  link.attr("marker-end", function(d)
-//	  {
-//	  	return "url(#" + d.value + ")";
-//	  });
-	  var gnodes = svg.selectAll('g.gnode')
-	     .data(graph.nodes)
-	     .enter()
-	     .append('g')
-	     .classed('gnode', true);
-	    
-	  var node = gnodes.append("circle")
-	      .attr("class", "node")
-	      .attr("r", 5)
-	      .style("fill", function(d) { return color(d.group); })
-	     .call(force.drag);
 
-//	  var labels = gnodes.append("text")
-//	  .attr("class", "word")
-//	      .text(function(d) { console.log(d.name);return d.name; });
-	  
-	  var text = svg.append("g").selectAll("text")
-	    .data(force.nodes())
-	  .enter().append("text")
-	  .attr("class", "text")
-	  // .attr("x", 8)
-	   //.attr("y", ".31em")
-	    .text(function(d) { //console.log(d.name);
-	    return d.name; });
-	 
-	  
-	  node.append("title")
-     .text(function(d) { return d.name; });
-	  
-	  
-	  
-	//  console.log(labels);
-//	  var node = svg.selectAll(".node")
-//	      .data(graph.nodes)
-//	    .enter().append("circle")
-//	      .attr("class", "node")
-//	      .attr("r", 5)
-//	      .style("fill", function(d) { return color(d.group); })
-//	      .call(force.drag);
-//	  var labels = node.append("text")
-//	  .text(function(d) { return d.name; });
-//	  node.append("text")
-//      .attr("x", 12)
-//      .attr("dy", ".35em")
-//      .text(function(d) { return  d.name; });
-//	 // .text("node56");
-//
-//	  node.append("title")
-//	      .text(function(d) { return d.name; });
+		// Prepare the arrow
+		svg.append("defs").selectAll("marker")
+		.data(["arrow"])
+		.enter().append("marker")
+		.attr("id", "arrow")
+		.attr("viewBox", "0 -5 10 10")
+		.attr("refX", 10)
+		.attr("refY", -1.5)
+		.append("path")
+		.attr("d", "M0,-5L10,0L0,5");
+		
+		// Draw links between the nodes
+		var link = svg.selectAll(".link")
+		.data(graph.links)
+		.enter().append("line")
+		.attr("class", "link")
+		.style("stroke-width", function(d) { return Math.sqrt(d.value); })
+		.attr("marker-end", "url(#arrow)");
 
-	  force.on("tick", function() {
-	    link.attr("x1", function(d) { return d.source.x; })
-	        .attr("y1", function(d) { return d.source.y; })
-	        .attr("x2", function(d) { return d.target.x; })
-	        .attr("y2", function(d) { return d.target.y; });
-	    text.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-	  //  node.attr("cx", function(d) { return d.x; })
-	   //     .attr("cy", function(d) { return d.y; });
-	    
-	    
-	    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-	  });
-	  
-	// Populate the nodes
-//		for (var node in graph.nodes.node)
-//		{		
-//			// Only add new stuff.  This provides correct behavior on data updates
-//			if (!(graph.nodes.node[node]['@'].id in nodes))
-//			{
-//				nodes[graph.nodes.node[node]['@'].id] = {"name": graph.nodes.node[node]['@'].id, "data": "d3"};
-//			}			
-//		}
-//		// Populate the links
-//		for (var edge in graph.links.link)
-//		{	
-//			links[graph.links.linsk[edge]['@'].id] = {"source": nodes[graph.links.links[edge]['@'].source], "target": nodes[graph.links.link[edge]['@'].target], "data":graph.links.link[edge]};				
-//		}
-	  
+		// Dragging the nodes
+		var node_drag = d3.behavior.drag()
+	    .on("dragstart", dragstart)
+	    .on("drag", dragmove)
+	    .on("dragend", dragend);
+
+	function dragstart(d, i) {
+	    //force.stop() // stops the force auto positioning before you start dragging
+	}
+
+	function dragmove(d, i) {
+	    d.px += d3.event.dx;
+	    d.py += d3.event.dy;
+	    d.x += d3.event.dx;
+	    d.y += d3.event.dy; 
+	    tick(); // this is the key to make it work together with updating both px,py,x,y on d !
+	}
+
+	function dragend(d, i) {
+	    //d.fixed = true; // of course set the node to fixed so the force doesn't include the node in its auto positioning stuff
+	    //tick();
+	    //force.resume();
+	}
+
+		    // Properties for the nodes
+		    
+		var gnodes = svg.selectAll('g.gnode')
+		.data(graph.nodes)
+		.enter()
+		.append('g')
+		.classed('gnode', true);
+		
+		//COnstructing circle nodes
+		var node = gnodes.append("circle")
+		.attr("class", "node")
+		.attr("r", 5)
+		.style("fill", function(d) { return color(d.group); })
+		.call(node_drag);
+		//.call(force.drag);
+		
+		// Appending labels to the nodes
+		var text = gnodes.append("text")
+		.call(node_drag)
+		  .text(function(d) { return d.name; });
+
+		// Appending title to the nodes 
+		//Mouse hover on the node shows the name of the node.
+		node.append("title")
+		.text(function(d) { return d.name; });
+		
+		//Properties for the position of the node
+		function tick() {
+			  link.attr("x1", function(d) { return d.source.x; })
+			      .attr("y1", function(d) { return d.source.y; })
+			      .attr("x2", function(d) { return d.target.x; })
+			      .attr("y2", function(d) { return d.target.y; });
+
+			  node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+			  text.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+			};
+
+		force.on("tick", function() {
+			link.attr("x1", function(d) { return d.source.x; })
+			.attr("y1", function(d) { return d.source.y; })
+			.attr("x2", function(d) { return d.target.x; })
+			.attr("y2", function(d) { return d.target.y; });
+			text.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+			node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+		});
+
+
 	});
-	
 
-    
+
+
 }
