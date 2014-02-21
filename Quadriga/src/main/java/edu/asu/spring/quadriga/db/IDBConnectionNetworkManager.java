47c258470b677c3cf2b8c1c44d2eb5b1c8825d77
dlohith <dlohith@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2014-02-21 17:27:51 +0000

    Java doc for IDBConnectionNetworkManager

--- Quadriga/src/main/java/edu/asu/spring/quadriga/db/IDBConnectionNetworkManager.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/db/IDBConnectionNetworkManager.java
@@ -15,10 +15,10 @@ public interface IDBConnectionNetworkManager {
 	 * Add a new network into workspace
 	 * Creates a new Network ID  and assigns owner to network object, Adds a network into the Workspace. 
 	 * So Vogon can access this network from its Workspace
-	 * @param networkName
-	 * @param user
-	 * @param workspaceid
-	 * @return
+	 * @param networkName 		Name for the newly added network
+	 * @param user				Owner of the network
+	 * @param workspaceid		Workspace the network would be assigned
+	 * @return					return success/error message
 	 * @throws QuadrigaStorageException
 	 */
 	public abstract String addNetworkRequest(String networkName, IUser user, String workspaceid)
@@ -29,12 +29,12 @@ public interface IDBConnectionNetworkManager {
 	 * This method would also add more info of the network statement like top node, type of node.
 	 * Also adds all the statements in the XML for further use cases.
 	 * At present only the statements with isTop = 1 are been used for displaying networks on UI.
-	 * @param networkId
-	 * @param id
-	 * @param type
-	 * @param isTop
-	 * @param user
-	 * @return
+	 * @param networkId			ID of network
+	 * @param id				ID of network statement
+	 * @param type				Type of network statement, AE - Appellation event, RE - Relation event
+	 * @param isTop				{@link Boolean} To whether the network statement is in starting point xml. 
+	 * @param user				Owner of the network
+	 * @return					return success/error message
 	 * @throws QuadrigaStorageException
 	 */
 	public abstract String addNetworkStatement(String rowid,String networkId, String id, String type,
@@ -45,9 +45,9 @@ public interface IDBConnectionNetworkManager {
 	 * Get {@link INetwork} object of the User using the network ID.
 	 * This object would have info of project and workspace the network belongs to.
 	 * Used for showing the network in the UI.
-	 * @param networkid
-	 * @param user
-	 * @return
+	 * @param networkid			ID of network
+	 * @param user				Owner of the network
+	 * @return					return {@link INetwork} object associated to the networkid, user
 	 * @throws QuadrigaStorageException
 	 */
 	public abstract INetwork getNetwork(String networkid, IUser user) throws QuadrigaStorageException;
@@ -55,17 +55,17 @@ public interface IDBConnectionNetworkManager {
 	/**
 	 * This would give the list of {@link INetwork} belonging the {@link IUser}.
 	 * Mainly used to display list of Networks and project and workspace it belong to, in the UI. 
-	 * @param user
-	 * @return
+	 * @param user				Owner of the network
+	 * @return					returns {@link List} of {@link INetwork} belonging to owner
 	 * @throws QuadrigaStorageException
 	 */
 	public abstract List<INetwork> getNetworkList(IUser user) throws QuadrigaStorageException;
 
 	/**
 	 * Check if the network name is already used
-	 * @param networkName
-	 * @param user
-	 * @return
+	 * @param networkName		Name for the network
+	 * @param user				{@link IUser} object for searching network name
+	 * @return					returns {@link Boolean} value of whether name exist or not
 	 * @throws QuadrigaStorageException
 	 */
 	public abstract boolean hasNetworkName(String networkName,IUser user) throws QuadrigaStorageException;
@@ -76,9 +76,9 @@ public interface IDBConnectionNetworkManager {
 	 * isarchived = 0 is network is active
 	 * isarchived = 1 is network is archived as is most recently archived
 	 * isarchived = 2 is network is archived and never used again. kept in DB for future references.
-	 * @param networkId
-	 * @param id
-	 * @return
+	 * @param networkId			ID of network
+	 * @param id				ID of network statement
+	 * @return					return success/error message
 	 * @throws QuadrigaStorageException
 	 */
 	public abstract String archiveNetworkStatement(String networkId, String id)
@@ -91,8 +91,8 @@ public interface IDBConnectionNetworkManager {
 	 * Used to access Relation event or Appellation events from QStore.
 	 * We can use this method for visualize network or edit network purpose.
 	 * 
-	 * @param networkId
-	 * @return
+	 * @param networkId			ID of network
+	 * @return					returns {@link List} of {@link INetworkNodeInfo}	
 	 * @throws QuadrigaStorageException
 	 */
 	public abstract List<INetworkNodeInfo> getNetworkNodes(String networkId)
@@ -103,8 +103,8 @@ public interface IDBConnectionNetworkManager {
 	 * Archive the network, would mark the network statements with isarchived =1 or isarchived=2.
 	 * isarchived = 1 is network is archived as is most recently archived
 	 * isarchived = 2 is network is archived and never used again. kept in DB for future references.
-	 * @param networkId
-	 * @return
+	 * @param networkId			ID of network
+	 * @return					returns success/error messages
 	 * @throws QuadrigaStorageException
 	 */
 	public abstract String archiveNetwork(String networkId) throws QuadrigaStorageException;
@@ -113,9 +113,9 @@ public interface IDBConnectionNetworkManager {
 	/**
 	 * Update network name, when vogon tries to re-upload the network with a different name
 	 * This method could be called if User's network has been rejected and user prefers to store the network with an alternative name (Like version name) 
-	 * @param networkId
-	 * @param networkName
-	 * @return
+	 * @param networkId			ID of network
+	 * @param networkName		New name for the network
+	 * @return					returns success/error messages
 	 * @throws QuadrigaStorageException
 	 */
 	public abstract String updateNetworkName(String networkId,String networkName) throws QuadrigaStorageException;
@@ -134,9 +134,9 @@ public interface IDBConnectionNetworkManager {
 	/**
 	 * Get Networks of old versions.
 	 * Network with isarchive =1 and isarchive =2 are retrived.
-	 * @param networkId
-	 * @param archiveLevel
-	 * @return
+	 * @param networkId					ID of network
+	 * @param archiveLevel				Archive level would be from 0, 1, 2 - levels of old versions
+	 * @return							returns {@link List} of {@link INetworkOldVersion}
 	 * @throws QuadrigaStorageException
 	 */
 	public abstract List<INetworkOldVersion> getNetworkVersions(String networkId, int archiveLevel) throws QuadrigaStorageException;
