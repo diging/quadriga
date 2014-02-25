9dc020e5a73f73b18ae6eb459c2946792da80c40
dlohith <dlohith@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2014-02-25 00:44:43 +0000

    Javadocs

--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/domain/factories/impl/D3LinkFactory.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/domain/factories/impl/D3LinkFactory.java
@@ -1,8 +1,5 @@
 package edu.asu.spring.quadriga.domain.factories.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import edu.asu.spring.quadriga.domain.ID3Link;
@@ -12,13 +9,11 @@ import edu.asu.spring.quadriga.domain.implementation.D3Link;
 @Service
 public class D3LinkFactory implements ID3LinkFactory  {
 
-	/* (non-Javadoc)
-	 * @see edu.asu.spring.quadriga.domain.factories.impl.ID3LinkFactory#createD3LinkObject()
+	/**
+	 * {@inheritDoc}
 	 */
 	@Override
 	public ID3Link createD3LinkObject() {
-		// TODO Auto-generated method stub
 		return new D3Link();
-		
 	}
 }
