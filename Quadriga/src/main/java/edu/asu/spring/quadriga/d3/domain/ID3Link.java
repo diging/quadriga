481ec1949b9f781f0e5a90dd248b80e0596c3d7e
dlohith <dlohith@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2014-03-05 21:24:03 +0000

    Javadoc for D3 related classes

--- Quadriga/src/main/java/edu/asu/spring/quadriga/d3/domain/ID3Link.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/d3/domain/ID3Link.java
@@ -1,17 +1,42 @@
 package edu.asu.spring.quadriga.d3.domain;
 
+import java.util.HashMap;
+import java.util.List;
+
+import edu.asu.spring.quadriga.d3.domain.impl.D3Node;
+
 /**
- * Interface to hold the link variables and methods in the network
+ * Interface to hold the link between source to target in the network.
+ * This is very specific to D3 JQuery, we can use this interface to build the
+ * D3 Json for force directed graph.
  *
+ *	@author Lohith Dwaraka
  */
+
 public interface ID3Link {
 
+	/**
+	 * Getter for the source where the link would start
+	 * @return 		returns the source node number from  {@link HashMap} of {@link D3Node}
+	 */
 	public abstract int getSource();
 
+	/**
+	 * Setter for the source where the link would start. We would set the {@link D3Node} number from the {@link HashMap} of {@link D3Node}
+	 * @param source		{@link Integer} value of D3Node number from {@link HashMap} of {@link D3Node}.
+	 */
 	public abstract void setSource(int source);
 
+	/**
+	 * Getter for the target where the link would end
+	 * @return 		returns the target node number from {@link HashMap} of {@link D3Node}
+	 */
 	public abstract int getTarget();
 
+	/**
+	 * Setter for the target where the link would start. We would set the {@link D3Node} number from the {@link HashMap} of {@link D3Node}
+	 * @param target		{@link Integer} value of D3Node number from {@link HashMap} of {@link D3Node}.
+	 */
 	public abstract void setTarget(int target);
 
 }
--- Quadriga/src/main/java/edu/asu/spring/quadriga/d3/domain/factory/impl/D3LinkFactory.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/d3/domain/factory/impl/D3LinkFactory.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Service;
 import edu.asu.spring.quadriga.d3.domain.ID3Link;
 import edu.asu.spring.quadriga.d3.domain.factory.ID3LinkFactory;
 import edu.asu.spring.quadriga.d3.domain.impl.D3Link;
-
+/**
+ * Implementation of {@link ID3LinkFactory}. Creates the factory object of {@link ID3Link}
+ * @author Dwaraka
+ *
+ */
 @Service
 public class D3LinkFactory implements ID3LinkFactory  {
 
