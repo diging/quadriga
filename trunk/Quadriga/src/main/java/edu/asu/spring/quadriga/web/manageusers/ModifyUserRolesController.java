901e105201d823aa9edc46971de4ff8a7f508940
kbatna <kbatna@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2014-03-08 00:25:58 +0000

    Implemented methods and jsp page to update quadriga roles associated with the user

--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/manageusers/ModifyUserRolesController.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/manageusers/ModifyUserRolesController.java
@@ -1,23 +1,34 @@
 package edu.asu.spring.quadriga.web.manageusers;
 
 import java.beans.PropertyEditorSupport;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.hibernate.HibernateException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import edu.asu.spring.quadriga.domain.IQuadrigaRole;
+import edu.asu.spring.quadriga.domain.IUser;
 import edu.asu.spring.quadriga.exceptions.QuadrigaAccessException;
 import edu.asu.spring.quadriga.exceptions.QuadrigaStorageException;
 import edu.asu.spring.quadriga.service.IQuadrigaRoleManager;
+import edu.asu.spring.quadriga.service.IUserManager;
 import edu.asu.spring.quadriga.validator.CollaboratorFormValidator;
+import edu.asu.spring.quadriga.web.users.backingbean.ModifyQuadrigaUser;
+import edu.asu.spring.quadriga.web.users.backingbean.ModifyQuadrigaUserForm;
+import edu.asu.spring.quadriga.web.users.backingbean.ModifyQuadrigaUserFormManager;
 
 public class ModifyUserRolesController 
 {
@@ -27,6 +38,12 @@ public class ModifyUserRolesController
 	@Autowired
 	private IQuadrigaRoleManager rolemanager;
 	
+	@Autowired 
+	private IUserManager userManager;
+	
+	@Autowired
+	private ModifyQuadrigaUserFormManager quadrigaUserMananger;
+
 	@InitBinder
 	protected void initBinder(HttpServletRequest request, ServletRequestDataBinder binder,WebDataBinder validateBinder) throws Exception {
         validateBinder.setValidator(validator);
@@ -47,14 +64,111 @@ public class ModifyUserRolesController
 		
 	}
 	
-	
-	public ModelAndView updateQuadrigaRolesRequest(@PathVariable("userName") String userName)
+	/**
+	 * This method retrieve all the active users and their quadriga roles to be 
+	 * displayed for updating.
+	 * @return ModelAndView - View to display users and quadriga roles. 
+	 * @throws QuadrigaStorageException
+	 * @throws QuadrigaAccessException
+	 * @author kiran batna
+	 */
+	//TODO : request mapping should be updated
+	@RequestMapping(value = "auth/users/access", method = RequestMethod.GET)
+	public ModelAndView updateQuadrigaRolesRequest()
 			throws QuadrigaStorageException, QuadrigaAccessException
 	{
 		ModelAndView model;
+		ModifyQuadrigaUserForm userForm;
 		model = new ModelAndView();
+		
+		//Get all Active Users
+		List<IUser> activeUserList = userManager.getAllActiveUsers();
+		
+		userForm = quadrigaUserMananger.modifyUserQuadrigaRolesManager(activeUserList);
+		
+		//Get all Quadriga roles
+		List<IQuadrigaRole> quadrigaRoles = rolemanager.getQuadrigaRoles();
+				
+		model.getModelMap().put("userrolesform", userForm);
+		model.getModelMap().put("quadrigaroles",quadrigaRoles);
+		model.getModelMap().put("success", 0);
 		return model;
-		//TO:DO
+	}
+	
+	/**
+	 * This method updates the quadriga roles associated with the user
+	 * @param userForm
+	 * @param result
+	 * @param principal
+	 * @return ModelAndView - On success View showing the success message.
+	 *                        On error View showing the data for updating.
+	 * @throws QuadrigaStorageException
+	 * @throws QuadrigaAccessException
+	 * @author kiran batna
+	 */
+	//TODO : request mapping should be updated
+	@RequestMapping(value = "auth/users/acces", method = RequestMethod.POST)
+	public ModelAndView updateCollaboratorRequest(@Validated @ModelAttribute("userrolesform") ModifyQuadrigaUserForm userForm,
+			BindingResult result,Principal principal) 
+					throws QuadrigaStorageException, QuadrigaAccessException
+	{
+		ModelAndView model;
+		List<ModifyQuadrigaUser> users;
+		List<IQuadrigaRole> quadrigaRoles;
+		String loggedInUser;
+		StringBuilder userRoles;
+		String userName = "";
+		try
+		{
+			loggedInUser = principal.getName();
+			userRoles = new StringBuilder();
+			quadrigaRoles = new ArrayList<IQuadrigaRole>();
+			
+			//create model view
+			model = new ModelAndView("auth/users/acces/");
+			
+			if(result.hasErrors())
+			{
+				//Get all Active Users
+				List<IUser> activeUserList = userManager.getAllActiveUsers();
+				
+				userForm = quadrigaUserMananger.modifyUserQuadrigaRolesManager(activeUserList);
+				
+				//Get all Quadriga roles
+				quadrigaRoles = rolemanager.getQuadrigaRoles();
+						
+				model.getModelMap().put("userrolesform", userForm);
+				model.getModelMap().put("quadrigaroles",quadrigaRoles);
+				model.getModelMap().put("success", 0);
+			}
+			else
+			{
+				users = userForm.getUsers();
+				
+				for(ModifyQuadrigaUser user : users)
+				{
+					userRoles = new StringBuilder();
+					userName = user.getUserName();
+					quadrigaRoles =  user.getQuadrigaRoles();
+				}
+				
+				for(IQuadrigaRole role : quadrigaRoles)
+				{
+					userRoles.append(",");
+					userRoles.append(role.getDBid());
+				}
+				
+				userManager.updateUserQuadrigaRoles(userName, userRoles.toString().substring(1), loggedInUser);
+				
+				model.getModelMap().put("success", 1);
+			}
+		}
+		catch(HibernateException ex)
+		{
+			throw new QuadrigaStorageException();
+		}
+
+		return model;
 	}
 
 }
