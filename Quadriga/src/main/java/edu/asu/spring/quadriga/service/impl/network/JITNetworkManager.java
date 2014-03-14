e7a1aa005cebb4bca1ae1a16505d3c84290a278f
dlohith <dlohith@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2014-03-14 20:19:45 +0000

    Moving network service implementation classes

--- Quadriga/src/main/java/edu/asu/spring/quadriga/service/impl/network/JITNetworkManager.java
+++
@@ -1,5 +0,0 @@
-package edu.asu.spring.quadriga.service.impl.network;
-
-public class JITNetworkManager {
-
-}
---
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/service/network/impl/JITNetworkManager.java
@@ -0,0 +1,5 @@
+package edu.asu.spring.quadriga.service.network.impl;
+
+public class JITNetworkManager {
+
+}
