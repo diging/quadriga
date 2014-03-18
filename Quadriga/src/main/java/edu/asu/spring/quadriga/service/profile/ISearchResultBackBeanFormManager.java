00c2410af15f93253d6f54dfda8b80dce8320447
rpendbha <rpendbha@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2014-03-18 00:05:27 +0000

    added javadoc

--- Quadriga/src/main/java/edu/asu/spring/quadriga/service/profile/ISearchResultBackBeanFormManager.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/service/profile/ISearchResultBackBeanFormManager.java
@@ -4,6 +4,13 @@ import java.util.List;
 
 import edu.asu.spring.quadriga.web.profile.impl.SearchResultBackBean;
 
+/**
+ * the interface used for setting the searchresult to SearchResultBackBean class
+ * retrieved from viaf service 
+ *  
+ * @author rohit pendbhaje
+ *
+ */
 public interface ISearchResultBackBeanFormManager {
 	
 	public abstract List<SearchResultBackBean> getsearchResultBackBeanList(String serviceId, String term);
