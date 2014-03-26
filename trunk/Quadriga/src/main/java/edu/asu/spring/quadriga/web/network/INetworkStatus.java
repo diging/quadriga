ca7b0b96f60b90ebc473478d1ed8a8b46e119c68
dlohith <dlohith@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2014-03-26 00:54:03 +0000

    Changes in the URL and velocity xml content.

--- trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/network/INetworkStatus.java
+++ trunk/Quadriga/src/main/java/edu/asu/spring/quadriga/web/network/INetworkStatus.java
@@ -5,11 +5,19 @@ package edu.asu.spring.quadriga.web.network;
  */
 public interface INetworkStatus {
 
+	// Network status constants
 	public final static String PENDING = "PENDING";
 	public final static String ASSIGNED = "ASSIGNED";
 	public final static String APPROVED="APPROVED";
 	public final static String REJECTED = "REJECTED";
 	public final static String UNKNOWN = "UNKNOWN";
+	
+	// Status code of the network
+	public final static int PENDING_CODE = 0;
+	public final static int ASSIGNED_CODE = 1;
+	public final static int APPROVED_CODE= 2;
+	public final static int REJECTED_CODE = 3;
+	public final static int UNKNOWN_CODE = -1;
 	
 	public final static int NOT_ARCHIVED = 0;
 	public final static int ARCHIVE_LEVEL_ONE = 1;
