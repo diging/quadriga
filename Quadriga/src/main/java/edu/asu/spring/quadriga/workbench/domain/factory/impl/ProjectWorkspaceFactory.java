3144e6aac40a8335e9165d36e36f99e49fa85d26
sowjanya2106 <sowjanya2106@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2014-04-15 22:54:58 +0000

    Added Project workspace shallow mapper class

--- Quadriga/src/main/java/edu/asu/spring/quadriga/workbench/domain/factory/impl/ProjectWorkspaceFactory.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/workbench/domain/factory/impl/ProjectWorkspaceFactory.java
@@ -1,10 +1,10 @@
-package edu.asu.spring.quadriga.domain.factories.impl;
+package edu.asu.spring.quadriga.workbench.domain.factory.impl;
 
 import org.springframework.stereotype.Service;
 
-import edu.asu.spring.quadriga.domain.factories.IProjectWorkspaceFactory;
 import edu.asu.spring.quadriga.domain.impl.workbench.ProjectWorkspace;
 import edu.asu.spring.quadriga.domain.workbench.IProjectWorkspace;
+import edu.asu.spring.quadriga.workbench.domain.factory.IProjectWorkspaceFactory;
 
 @Service
 public class ProjectWorkspaceFactory implements IProjectWorkspaceFactory {
