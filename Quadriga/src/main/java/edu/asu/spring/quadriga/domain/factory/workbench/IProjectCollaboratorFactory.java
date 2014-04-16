82e600b5fa43094c5f31688bc1c7bf360738b233
dlohith <dlohith@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2014-04-16 01:19:27 +0000

    Methods for getting List of IProjectCollaborators from the ProjectDTO

---
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/domain/factory/workbench/IProjectCollaboratorFactory.java
@@ -0,0 +1,14 @@
+package edu.asu.spring.quadriga.domain.factory.workbench;
+
+import edu.asu.spring.quadriga.domain.impl.workbench.ProjectCollaborator;
+import edu.asu.spring.quadriga.domain.workbench.IProjectCollaborator;
+
+public interface IProjectCollaboratorFactory {
+
+	/**
+	 * Creates {@link ProjectCollaborator} object
+	 * @return							Returns the {@link IProjectCollaborator} object
+	 */
+	public abstract IProjectCollaborator createProjectCollaboratorObject();
+
+}
