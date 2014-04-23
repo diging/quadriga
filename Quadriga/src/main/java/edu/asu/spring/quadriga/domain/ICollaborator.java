b40ba112cb9601b2bfa0897535bf59966c473476
kbatna <kbatna@1c955c2f-30c6-413f-95a5-61d63525ad7a>
2014-04-23 00:08:43 +0000

    Added java doc

--- Quadriga/src/main/java/edu/asu/spring/quadriga/domain/ICollaborator.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/domain/ICollaborator.java
@@ -11,15 +11,41 @@ import java.util.List;
 public interface ICollaborator 
 {
 
+	/**
+	 * Method to set the User details
+	 * @param userObj - {@link IUser} object containing Collaborator details
+	 */
 	public abstract void setUserObj(IUser userObj);
 
+	/**
+	 * Method to retrieve the user details
+	 * @return IUser - {@link IUser} object containing Collaborator details
+	 */
 	public abstract IUser getUserObj();
 	
+    /**
+     * Method to set roles associated with the collaborator
+     * @param collaboratorRoles - List of {@link ICollaboratorRole} objects containing
+     * the roles associated to the collaborator
+     */
 	public abstract void setCollaboratorRoles(List<ICollaboratorRole> collaboratorRoles);
 
+    /**
+     * Method to retrieve roles associated with the collaborator
+     * @return collaboratorRoles - List of {@link ICollaboratorRole} objects containing
+     * the roles associated to the collaborator
+     */
 	public abstract List<ICollaboratorRole> getCollaboratorRoles();
 
+    /**
+     * Method to set the collaborator description.
+     * @param description - text describing the collaborator.
+     */
 	public abstract void setDescription(String description);
 
+	/**
+	 * Method to retrieve the collaborator description
+	 * @return description - text describing the collaborator
+	 */
 	public abstract String getDescription();
 }
--- Quadriga/src/main/java/edu/asu/spring/quadriga/domain/IQuadrigaRole.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/domain/IQuadrigaRole.java
@@ -9,24 +9,64 @@ package edu.asu.spring.quadriga.domain;
 public interface IQuadrigaRole 
 {
 
+	/**
+	 * Method to retrieve quadriga role id stored in the database. 
+	 * @return dBid- text containing the role id stored in the database. 
+	 */
 	public abstract String getDBid();
 	
+	/**
+	 * Method to set the quadriga role id stored in the database. 
+	 * @param dBid - text containing the role id stored in the database.  
+	 */
 	public abstract void setDBid(String dBid);
 	
+	/**
+	 * Method to set the quadriga role id displayed in the UI pages.
+	 * @param id - text containing the role id to be displayed in UI pages. 
+	 */
 	public abstract void setId(String id);
 
+	/**
+	 * Method to retrieve the quadriga role id displayed in the UI pages.
+	 * @return id - text containing the role id to be displayed in UI pages. 
+	 */
 	public abstract String getId();
 	
+	/**
+	 * Method to set the name of the quadriga role
+	 * @param name - text contating the name associated with quadriga role.
+	 */
 	public abstract void setName(String name);
 
+	/**
+	 * Method to retrieve the name of the quadriga role
+	 * @return name - text contating the name associated with quadriga role.
+	 */
 	public abstract String getName();
 	
+	/**
+	 * Method to retrieve the display name of the quadriga role
+	 * @return displayName text containing the name to be displayed for the quadriga role.
+	 */
 	public abstract String getDisplayName();
 	
+	/**
+	 * Method to set the display name of the quadriga role
+	 * @return displayName text containing the name to be displayed for the quadriga role.
+	 */
 	public abstract void setDisplayName(String displayName);
 	
+	/**
+	 * Method to set the description associated with quadriga role
+	 * @param description text containing the description of quadriga role.
+	 */
 	public abstract void setDescription(String description);
 
+	/**
+	 * Method to retrieve the description associated with quadriga role
+	 * @return description text containing the description of quadriga role.
+	 */
 	public abstract String getDescription();
 
 }
