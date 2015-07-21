39ce14d3aefd6a70bab07a09750920f5c4921841
Julia Damerow <julia.damerow@googlemail.com>
2015-07-21 12:31:34 -0700

    deleted generated maven files

--- Quadriga/src/test/java/edu/asu/spring/quadriga/exceptions/QuadrigaExceptionHandlerTest.java
+++ Quadriga/src/test/java/edu/asu/spring/quadriga/exceptions/QuadrigaExceptionHandlerTest.java
@@ -8,21 +8,21 @@ import org.springframework.web.servlet.ModelAndView;
 
 public class QuadrigaExceptionHandlerTest {
 
-	private QuadrigaExceptionHandler exceptionHandler;
-	private Exception ex;
-	
-	@Before
-	public void setUp()  {
-		exceptionHandler = new QuadrigaExceptionHandler();
-		ex = new Exception();
-	}
-	
-	@Test
-	public void handleNotImplementedExTest() {
-		ModelAndView view = exceptionHandler.handleNotImplementedEx(ex);
-		
-		assertEquals("auth/notImplemented", view.getViewName());
-		assertEquals(ex.getClass().getName(), view.getModelMap().get("ex_name"));
-		assertEquals(ex.getMessage(), view.getModelMap().get("ex_message"));
-	}
+    private QuadrigaExceptionHandler exceptionHandler;
+    private Exception ex;
+
+    @Before
+    public void setUp() {
+        exceptionHandler = new QuadrigaExceptionHandler();
+        ex = new Exception();
+    }
+
+    @Test
+    public void handleNotImplementedExTest() {
+        ModelAndView view = exceptionHandler.handleNotImplementedEx(ex);
+
+        assertEquals("auth/notImplemented", view.getViewName());
+        assertEquals(ex.getClass().getName(), view.getModelMap().get("ex_name"));
+        assertEquals(ex.getMessage(), view.getModelMap().get("ex_message"));
+    }
 }
