98b327e62d351ef5373d8b96d443a184397e765a
Karthikeyan Mohan <mkarthik90@gmail.com>
2015-09-28 09:02:34 -0700

    Changes made for alignment.

--- Quadriga/src/main/java/edu/asu/spring/quadriga/service/impl/workbench/ModifyProjectManager.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/service/impl/workbench/ModifyProjectManager.java
@@ -33,187 +33,188 @@ import edu.asu.spring.quadriga.service.workbench.IModifyProjectManager;
 
 /**
  * This class add/update/delete a project
+ * 
  * @author kbatna, Julia Damerow
  */
 @Service
-public class ModifyProjectManager extends BaseManager implements IModifyProjectManager 
-{
+public class ModifyProjectManager extends BaseManager implements IModifyProjectManager {
 
 	@Autowired
 	private IProjectDAO projectDao;
-	
+
 	@Autowired
-    private ProjectDTOMapper projectDTOMapper;
-	
+	private ProjectDTOMapper projectDTOMapper;
+
 	@Autowired
 	private IProjectWorkspaceDAO projectWorkspaceDao;
-	
+
 	@Autowired
 	private IProjectDictionaryDAO projectDictionaryDao;
-	
+
 	@Autowired
 	private IProjectConceptCollectionDAO projectCCDao;
-	
+
 	@Autowired
 	private IProjectCollaboratorDAO projectCollabDao;
-	
+
 	@Autowired
 	private IProjectEditorDAO projectEditorDao;
-	
+
 	@Resource(name = "projectconstants")
-    private Properties messages;
-    
-	
+	private Properties messages;
+
 	private static final Logger logger = LoggerFactory.getLogger(ModifyProjectManager.class);
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	@Transactional
 	public void addNewProject(IProject project, String userName) {
-	    String projectId = messages.getProperty("project_internalid.name") + generateUniqueID();
-        project.setProjectId(projectId);
-        ProjectDTO projectDTO = projectDTOMapper.getProjectDTO(project,userName);
-        projectDao.saveNewDTO(projectDTO);
+		String projectId = messages.getProperty("project_internalid.name") + generateUniqueID();
+		project.setProjectId(projectId);
+		ProjectDTO projectDTO = projectDTOMapper.getProjectDTO(project, userName);
+		projectDao.saveNewDTO(projectDTO);
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	@Transactional
-	public void updateProject(String projID, String projName,String projDesc,String userName) throws QuadrigaStorageException
-	{
-	    ProjectDTO projectDTO = projectDao.getProjectDTO(projID);
-        projectDTO.setProjectname(projName);
-        projectDTO.setDescription(projDesc);
-        projectDTO.setUpdatedby(userName);
-        projectDTO.setUpdateddate(new Date());
-	    
-        projectDao.updateDTO(projectDTO);
+	public void updateProject(String projID, String projName, String projDesc, String userName)
+			throws QuadrigaStorageException {
+		ProjectDTO projectDTO = projectDao.getProjectDTO(projID);
+		projectDTO.setProjectname(projName);
+		projectDTO.setDescription(projDesc);
+		projectDTO.setUpdatedby(userName);
+		projectDTO.setUpdateddate(new Date());
+
+		projectDao.updateDTO(projectDTO);
 	}
-	
+
 	/**
 	 * This method deletes a project into the database.
+	 * 
 	 * @param project
-	 * @return String - error message blank on success and contains error on failure.
+	 * @return String - error message blank on success and contains error on
+	 *         failure.
 	 * @throws QuadrigaStorageException
 	 * @author Karthik Jayaraman, Julia Damerow
 	 */
 	@Override
 	@Transactional
-	public void deleteProjectRequest(List<String> projectIdList) throws QuadrigaStorageException
-	{
-	    for(String projectId : projectIdList) {
-            ProjectDTO  project = projectDao.getProjectDTO(projectId);
-            //retrieve all the foreign key tables associated with them
-            //delete the project workspace DTO
-            deleteProjectWorkspaceMapping(project);
-            deleteProjectDictionaryMapping(project);
-            deleteProjectConceptCollectionMapping(project);
-            deleteProjectCollaboratorMapping(project);
-            deleteProjectEditorMapping(project);
-            
-            projectDao.updateDTO(project);    
-            projectDao.deleteDTO(project);
-        }
-	}
-	
-	/**
-	 * This method will provide owner editor role to the project 
-	 * @param projectId
-	 * @param owner
-	 * @throws QuadrigaStorageException
-	 * @author Lohith Dwaraka
-	 */
-	@Override
-	@Transactional
-	public void assignEditorRole(String projectId, String editor) throws QuadrigaStorageException{
-	    ProjectEditorDTOPK projectEditorKey = new ProjectEditorDTOPK(projectId,editor);
-	    ProjectEditorDTO projectEditor = projectEditorDao.getProjectEditorDTO(projectEditorKey);
-        
-	    // if user is not editor yet, make him editor
-        if (projectEditor == null) {
-            ProjectDTO project = projectDao.getProjectDTO(projectId);
-            projectEditor = projectDTOMapper.getProjectEditor(project, editor);
-            
-            projectEditorDao.saveNewDTO(projectEditor);
-        }
-	}
-	
-	/**
-	 * This method will remove owner editor role from the project 
-	 * @param projectId
-	 * @param owner
-	 * @throws QuadrigaStorageException
-	 * @author Lohith Dwaraka
-	 */
-	@Override
-	@Transactional
-	public void removeEditorRole(String projectId, String editor) throws QuadrigaStorageException{
-	    ProjectEditorDTOPK projectEditorKey = new ProjectEditorDTOPK(projectId, editor);
-	    ProjectEditorDTO projectEditor = projectEditorDao.getProjectEditorDTO(projectEditorKey);
+	public void deleteProjectRequest(List<String> projectIdList) throws QuadrigaStorageException {
+		for (String projectId : projectIdList) {
+			ProjectDTO project = projectDao.getProjectDTO(projectId);
+			// retrieve all the foreign key tables associated with them
+			// delete the project workspace DTO
+			deleteProjectWorkspaceMapping(project);
+			deleteProjectDictionaryMapping(project);
+			deleteProjectConceptCollectionMapping(project);
+			deleteProjectCollaboratorMapping(project);
+			deleteProjectEditorMapping(project);
 
-        if(projectEditor != null) {
-            projectEditorDao.deleteDTO(projectEditor);
-        }
+			projectDao.updateDTO(project);
+			projectDao.deleteDTO(project);
+		}
 	}
-	
+
+	/**
+	 * This method will provide owner editor role to the project
+	 * 
+	 * @param projectId
+	 * @param owner
+	 * @throws QuadrigaStorageException
+	 * @author Lohith Dwaraka
+	 */
+	@Override
+	@Transactional
+	public void assignEditorRole(String projectId, String editor) throws QuadrigaStorageException {
+		ProjectEditorDTOPK projectEditorKey = new ProjectEditorDTOPK(projectId, editor);
+		ProjectEditorDTO projectEditor = projectEditorDao.getProjectEditorDTO(projectEditorKey);
+
+		// if user is not editor yet, make him editor
+		if (projectEditor == null) {
+			ProjectDTO project = projectDao.getProjectDTO(projectId);
+			projectEditor = projectDTOMapper.getProjectEditor(project, editor);
+
+			projectEditorDao.saveNewDTO(projectEditor);
+		}
+	}
+
+	/**
+	 * This method will remove owner editor role from the project
+	 * 
+	 * @param projectId
+	 * @param owner
+	 * @throws QuadrigaStorageException
+	 * @author Lohith Dwaraka
+	 */
+	@Override
+	@Transactional
+	public void removeEditorRole(String projectId, String editor) throws QuadrigaStorageException {
+		ProjectEditorDTOPK projectEditorKey = new ProjectEditorDTOPK(projectId, editor);
+		ProjectEditorDTO projectEditor = projectEditorDao.getProjectEditorDTO(projectEditorKey);
+
+		if (projectEditor != null) {
+			projectEditorDao.deleteDTO(projectEditor);
+		}
+	}
+
 	/*
-	 * ==================================================
-	 * Private Methods
+	 * ================================================== Private Methods
 	 * ==================================================
 	 */
 	private void deleteProjectWorkspaceMapping(ProjectDTO project) {
-        List<ProjectWorkspaceDTO> projectWorkspaceList = project.getProjectWorkspaceDTOList();
-        if(projectWorkspaceList != null) {
-            for(ProjectWorkspaceDTO projectWorkspace : projectWorkspaceList) {
-                projectWorkspaceDao.deleteDTO(projectWorkspace);
-            }
-        }
-        project.setProjectWorkspaceDTOList(null);    
-    }
-	
+		List<ProjectWorkspaceDTO> projectWorkspaceList = project.getProjectWorkspaceDTOList();
+		if (projectWorkspaceList != null) {
+			for (ProjectWorkspaceDTO projectWorkspace : projectWorkspaceList) {
+				projectWorkspaceDao.deleteDTO(projectWorkspace);
+			}
+		}
+		project.setProjectWorkspaceDTOList(null);
+	}
+
 	private void deleteProjectDictionaryMapping(ProjectDTO project) {
-        List<ProjectDictionaryDTO> projectDictionaryList = project.getProjectDictionaryDTOList();
-        if(projectDictionaryList !=null) {
-            for(ProjectDictionaryDTO projectDictionary : projectDictionaryList) {
-                projectDictionaryDao.deleteDTO(projectDictionary);;
-            }
-        }
-        project.setProjectDictionaryDTOList(null);        
-    }
-	
+		List<ProjectDictionaryDTO> projectDictionaryList = project.getProjectDictionaryDTOList();
+		if (projectDictionaryList != null) {
+			for (ProjectDictionaryDTO projectDictionary : projectDictionaryList) {
+				projectDictionaryDao.deleteDTO(projectDictionary);
+				;
+			}
+		}
+		project.setProjectDictionaryDTOList(null);
+	}
+
 	private void deleteProjectConceptCollectionMapping(ProjectDTO project) {
-        List<ProjectConceptCollectionDTO> projectConceptCollectionList = project.getProjectConceptCollectionDTOList();
-        if(projectConceptCollectionList != null) {
-            for(ProjectConceptCollectionDTO projectConceptCollection : projectConceptCollectionList) {
-                projectCCDao.deleteDTO(projectConceptCollection);
-            }
-        }
-        project.setProjectConceptCollectionDTOList(null);
-    }
-	
-	private void deleteProjectCollaboratorMapping(ProjectDTO project)
-    {
-        List<ProjectCollaboratorDTO> projectCollaboratorList = project.getProjectCollaboratorDTOList();
-        if(projectCollaboratorList != null) {
-            for(ProjectCollaboratorDTO projectCollaborator : projectCollaboratorList) {
-               projectCollabDao.deleteDTO(projectCollaborator);              
-            }
-        }
-        project.setProjectConceptCollectionDTOList(null);
-    }
-	
+		List<ProjectConceptCollectionDTO> projectConceptCollectionList = project.getProjectConceptCollectionDTOList();
+		if (projectConceptCollectionList != null) {
+			for (ProjectConceptCollectionDTO projectConceptCollection : projectConceptCollectionList) {
+				projectCCDao.deleteDTO(projectConceptCollection);
+			}
+		}
+		project.setProjectConceptCollectionDTOList(null);
+	}
+
+	private void deleteProjectCollaboratorMapping(ProjectDTO project) {
+		List<ProjectCollaboratorDTO> projectCollaboratorList = project.getProjectCollaboratorDTOList();
+		if (projectCollaboratorList != null) {
+			for (ProjectCollaboratorDTO projectCollaborator : projectCollaboratorList) {
+				projectCollabDao.deleteDTO(projectCollaborator);
+			}
+		}
+		project.setProjectConceptCollectionDTOList(null);
+	}
+
 	private void deleteProjectEditorMapping(ProjectDTO project) {
-        List<ProjectEditorDTO> projectEditorList = project.getProjectEditorDTOList();
-        if(projectEditorList != null) {
-            for(ProjectEditorDTO projectEditor : projectEditorList) {
-                projectEditorDao.deleteDTO(projectEditor);
-            }
-        }
-        
-        project.setProjectEditorDTOList(null);
-    }
+		List<ProjectEditorDTO> projectEditorList = project.getProjectEditorDTOList();
+		if (projectEditorList != null) {
+			for (ProjectEditorDTO projectEditor : projectEditorList) {
+				projectEditorDao.deleteDTO(projectEditor);
+			}
+		}
+
+		project.setProjectEditorDTOList(null);
+	}
 }
--- Quadriga/src/main/webapp/WEB-INF/views/workbench/modifyproject.jsp
+++ Quadriga/src/main/webapp/WEB-INF/views/workbench/modifyproject.jsp
@@ -3,101 +3,101 @@
 
 <!-- Content -->
 <script>
-
-$(document).ready(function(){
-	  $("#unixName").keyup(function(event)
-	  {
-		var keyedInput = $("#unixName").val();
-	    $("#UnixURL").text('${unixnameurl}'+keyedInput);
-	  });
+	$(document).ready(function() {
+		$("#unixName").keyup(function(event) {
+			var keyedInput = $("#unixName").val();
+			$("#UnixURL").text('${unixnameurl}' + keyedInput);
+		});
 	});
-	
+
 	$(function() {
 		$("input[type=submit]").button().click(function(event) {
 		});
-		
+
 		$("input[type=button]").button().click(function(event) {
 		});
-		
-		 if ($.trim($("#unixName").val())) {
-				var keyedInput = $("#unixName").val();
-			    $("#UnixURL").text('${unixnameurl}'+keyedInput);
-			}
+
+		if ($.trim($("#unixName").val())) {
+			var keyedInput = $("#unixName").val();
+			$("#UnixURL").text('${unixnameurl}' + keyedInput);
+		}
 	});
-	
+
 	function submitClick(id) {
-		
+
 		location.href = '${pageContext.servletContext.contextPath}/auth/workbench/${project.projectId}';
 	}
-	
-	
 </script>
 
 <style>
 div.projectDiv {
-    position:relative;
-    width:300px;
-    height:30px;
-    border:1px solid;
-    text-align:center;
+	position: relative;
+	width: 300px;
+	height: 30px;
+	border: 1px solid;
+	text-align: center;
 }
 
 input {
-    position:relative;
-    width:125px;
-    height:30px;
+	position: relative;
+	width: 125px;
+	height: 30px;
 }
 </style>
 
 <article class="is-page-content">
-<div id="projectDiv">
-	<form:form commandName="project" method="POST"
-		action="${pageContext.servletContext.contextPath}/auth/workbench/modifyproject/${project.projectId}">
-				<c:choose>
-			<c:when test="${success == '0'}">
-			<header>
-	<h2>Modify Project</h2>
-	<span class="byline">Please fill in the following information:</span>
-</header>
-		<table style="width: 100%">
-			<tr>
-				<td style="width: 170px">Name:</td>
-				<td style="width: 400px"><form:input path="projectName" size="60" id="projectName" /></td>
-				<td><form:errors path="projectName" class="ui-state-error-text"></form:errors></td>
-			</tr>
-			<tr>
-				<td style="vertical-align: top">Description:</td>
-				<td><form:textarea path="description" cols="44" rows="6"
-						id="description" /></td>
-						<td><form:errors path="description" class="ui-state-error-text"></form:errors></td>
-			</tr>
-			<tr>
-				<td>Public URL:
-				    <!--<form:input path="unixName" type="hidden" />-->
-				    <form:input path="projectId" type="hidden" /> 
-				    <form:input path="unixName" id="unixName" type="hidden"/>
-				    <form:input path="projectAccess" id="projectAccess" type="hidden"/>
-				    
-				</td>
-				<td><div id="UnixURL"></div></td>
-			</tr>
-			<tr>
-			<td><input class="command" type="submit" value="Modify Project"> </td>
-			<td><input type="button" value="Cancel" 
-			onclick="location.href='${pageContext.servletContext.contextPath}/auth/workbench/${project.projectId}'"></td>
-			</tr>
-		</table>
-			</c:when>
+	<div id="projectDiv">
+		<form:form commandName="project" method="POST"
+			action="${pageContext.servletContext.contextPath}/auth/workbench/modifyproject/${project.projectId}">
+			<c:choose>
+				<c:when test="${success == '0'}">
+					<header>
+						<h2>Modify Project</h2>
+						<span class="byline">Please fill in the following
+							information:</span>
+					</header>
+					<table style="width: 100%">
+						<tr>
+							<td style="width: 170px">Name:</td>
+							<td style="width: 400px"><form:input path="projectName"
+									size="60" id="projectName" /></td>
+							<td><form:errors path="projectName"
+									class="ui-state-error-text"></form:errors></td>
+						</tr>
+						<tr>
+							<td style="vertical-align: top">Description:</td>
+							<td><form:textarea path="description" cols="44" rows="6"
+									id="description" /></td>
+							<td><form:errors path="description"
+									class="ui-state-error-text"></form:errors></td>
+						</tr>
+						<tr>
+							<td>Public URL: <!--<form:input path="unixName" type="hidden" />-->
+								<form:input path="projectId" type="hidden" /> <form:input
+									path="unixName" id="unixName" type="hidden" /> <form:input
+									path="projectAccess" id="projectAccess" type="hidden" />
+
+							</td>
+							<td><div id="UnixURL"></div></td>
+						</tr>
+						<tr>
+							<td><input class="command" type="submit"
+								value="Modify Project"></td>
+							<td><input type="button" value="Cancel"
+								onclick="location.href='${pageContext.servletContext.contextPath}/auth/workbench/${project.projectId}'"></td>
+						</tr>
+					</table>
+				</c:when>
 				<c:when test="${success == '1'}">
-				<span class="byline">Project modified successfully.</span>
-				<ul>
-					<li><input type="button" onClick="submitClick();"
-						value='Okay'></li>
-				</ul>
-			</c:when>
+					<span class="byline">Project modified successfully.</span>
+					<ul>
+						<li><input type="button" onClick="submitClick();"
+							value='Okay'></li>
+					</ul>
+				</c:when>
 			</c:choose>
-	</form:form>
-</div>
+		</form:form>
+	</div>
 </article>
 
 
