c8cd0e0303c5ddf0c118ab1fa0557d8fa4fe3361
Chandana Kanchanapally <hkanchan@asu.edu>
2015-10-02 11:49:04 -0700

    Indentation fixed

--- Quadriga/src/main/java/edu/asu/spring/quadriga/web/workspace/ActivateWSController.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/workspace/ActivateWSController.java
@@ -66,21 +66,21 @@ public class ActivateWSController {
 	 * @throws QuadrigaAccessException
 	 * @author Hari Chandana Kanchanapally
 	 */
-	@AccessPolicies({
-			@ElementAccessPolicy(type = CheckedElementType.WORKSPACE, paramIndex = 1, userRole = {
-					RoleNames.ROLE_COLLABORATOR_ADMIN,
-					RoleNames.ROLE_PROJ_COLLABORATOR_ADMIN,
-					RoleNames.ROLE_PROJ_COLLABORATOR_CONTRIBUTOR }),
-			@ElementAccessPolicy(type = CheckedElementType.PROJECT, paramIndex = 2, userRole = {
-					RoleNames.ROLE_COLLABORATOR_ADMIN,
-					RoleNames.ROLE_PROJ_COLLABORATOR_ADMIN,
-					RoleNames.ROLE_PROJ_COLLABORATOR_CONTRIBUTOR }) })
-	@RequestMapping(value = "auth/workbench/{workspaceid}/activateWorkspace", method = RequestMethod.GET)
-	public ModelAndView activateWorkspace(
-			@PathVariable("workspaceid") String workspaceid,
-			@RequestParam("projectid") String projectid, Principal principal,
-			RedirectAttributes redirectAttributes)
-			throws QuadrigaStorageException, QuadrigaAccessException {
+    @AccessPolicies({
+    	@ElementAccessPolicy(type = CheckedElementType.WORKSPACE, paramIndex = 1, userRole = {
+    			RoleNames.ROLE_COLLABORATOR_ADMIN,
+    			RoleNames.ROLE_PROJ_COLLABORATOR_ADMIN,
+    			RoleNames.ROLE_PROJ_COLLABORATOR_CONTRIBUTOR }),
+    			@ElementAccessPolicy(type = CheckedElementType.PROJECT, paramIndex = 2, userRole = {
+    					RoleNames.ROLE_COLLABORATOR_ADMIN,
+    					RoleNames.ROLE_PROJ_COLLABORATOR_ADMIN,
+    					RoleNames.ROLE_PROJ_COLLABORATOR_CONTRIBUTOR }) })
+    @RequestMapping(value = "auth/workbench/{workspaceid}/activateWorkspace", method = RequestMethod.GET)
+    public ModelAndView activateWorkspace(
+    		@PathVariable("workspaceid") String workspaceid,
+    		@RequestParam("projectid") String projectid, Principal principal,
+    		RedirectAttributes redirectAttributes)
+    				throws QuadrigaStorageException, QuadrigaAccessException {
 
 		String userName;
 		// fetch the user name
