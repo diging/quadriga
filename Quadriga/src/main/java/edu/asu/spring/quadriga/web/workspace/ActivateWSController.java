554171bf18e7401cb52757af142b272ec7cb6cb7
Chandana Kanchanapally <hkanchan@asu.edu>
2015-10-07 11:44:43 -0700

    Inactive Workspace page lists decativated workspaces like activated workspace list, Added access permission for viewing deactivated list to project roles

--- Quadriga/src/main/java/edu/asu/spring/quadriga/web/workspace/ActivateWSController.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/workspace/ActivateWSController.java
@@ -167,10 +167,16 @@ public class ActivateWSController {
 
     }
 
+    @AccessPolicies({
+        @ElementAccessPolicy(type = CheckedElementType.PROJECT, paramIndex = 1, userRole = {
+                RoleNames.ROLE_COLLABORATOR_ADMIN,
+                RoleNames.ROLE_PROJ_COLLABORATOR_ADMIN,
+                RoleNames.ROLE_PROJ_COLLABORATOR_CONTRIBUTOR }) })
     @RequestMapping(value = "auth/workbench/{projectid}/showinactiveworkspace", method = RequestMethod.GET)
     public String showInactiveWorkspaces(
             @PathVariable("projectid") String projectId, Principal principal,
-            Model model) throws QuadrigaStorageException {
+            Model model) throws QuadrigaStorageException,
+            QuadrigaAccessException {
         List<ModifyWorkspace> deactivatedWSList = workspaceFormManager
                 .getDeactivatedWorkspaceList(projectId, principal.getName());
         model.addAttribute("deactivatedWSList", deactivatedWSList);
