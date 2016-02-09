b8899ebbee59961a7aaab642a21da69f46576fcd
rajat <rajataggarwal91@gmail.com>
2016-02-09 00:29:24 -0700

    QUAD-90 : Added about page to public website

--- Quadriga/src/main/java/edu/asu/spring/quadriga/web/publicwebsite/WebsiteProjectController.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/publicwebsite/WebsiteProjectController.java
@@ -8,7 +8,6 @@ package edu.asu.spring.quadriga.web.publicwebsite;
  *
  */
 
-
 import java.security.Principal;
 import java.util.List;
 
@@ -36,23 +35,22 @@ import edu.asu.spring.quadriga.service.workbench.IRetrieveProjectManager;
 @PropertySource(value = "classpath:/user.properties")
 @Controller
 public class WebsiteProjectController {
-	
-	@Autowired 
+
+	@Autowired
 	private IRetrieveProjectManager projectManager;
-	
+
 	@Autowired
 	private INetworkManager networkmanager;
-	
-	@Autowired
-    private INetworkTransformationManager transformationManager;
-    
-    @Autowired
-    private ID3Creator d3Creator;
-	
-	@Autowired
-    private Environment env;
 
-	
+	@Autowired
+	private INetworkTransformationManager transformationManager;
+
+	@Autowired
+	private ID3Creator d3Creator;
+
+	@Autowired
+	private Environment env;
+
 	public IRetrieveProjectManager getProjectManager() {
 		return projectManager;
 	}
@@ -60,114 +58,131 @@ public class WebsiteProjectController {
 	public void setProjectManager(IRetrieveProjectManager projectManager) {
 		this.projectManager = projectManager;
 	}
-	
-	private IProject getProjectDetails(String name) throws QuadrigaStorageException{
+
+	private IProject getProjectDetails(String name) throws QuadrigaStorageException {
 		return projectManager.getProjectDetailsByUnixName(name);
 	}
 
-	
 	/**
-	 * This method displays the public or external Website for the particular project 
+	 * This method displays the public or external Website for the particular project
 	 * 
-	 * If the project has been set to 'accessible', then the public website page is displayed. If the project does not exist
-	 * then an error page is shown.
+	 * If the project has been set to 'accessible', then the public website page is displayed. If the project does not exist then an error page is shown.
 	 * 
-	 * @param unixName 								unix name that is given to the project at the time of its creation
-	 * @param model									Model object to map values to view
-	 * @return										returns a string to access the external website main page
-	 * @throws QuadrigaStorageException				Database storage exception thrown
+	 * @param unixName
+	 *            unix name that is given to the project at the time of its creation
+	 * @param model
+	 *            Model object to map values to view
+	 * @return returns a string to access the external website main page
+	 * @throws QuadrigaStorageException
+	 *             Database storage exception thrown
 	 */
-	@RequestMapping(value="sites/{ProjectUnixName}", method=RequestMethod.GET)
-	public String showProject(@PathVariable("ProjectUnixName") String unixName,Model model, Principal principal) throws QuadrigaStorageException {
-		
+	@RequestMapping(value = "sites/{ProjectUnixName}", method = RequestMethod.GET)
+	public String showProject(@PathVariable("ProjectUnixName") String unixName, Model model, Principal principal) throws QuadrigaStorageException {
+
 		String user = null;
-		if(principal!=null){
+		if (principal != null) {
 			user = principal.getName();
 		}
-		
+
 		IProject project = getProjectDetails(unixName);
-		
+
 		model.addAttribute("project_baseurl", env.getProperty("project.cite.baseurl"));
-		
+
 		if (project == null)
-		    return "forbidden";
-		
-		if(user == null) {
-			if(projectManager.getPublicProjectWebsiteAccessibility(unixName)){
+			return "forbidden";
+
+		if (user == null) {
+			if (projectManager.getPublicProjectWebsiteAccessibility(unixName)) {
 				model.addAttribute("project", project);
 				return "sites/website";
-			}
-			else {
+			} else {
 				return "forbidden";
 			}
 		}
-	
-		
-		if(projectManager.getPrivateProjectWebsiteAccessibility(unixName, user)
-				|| projectManager.getPublicProjectWebsiteAccessibility(unixName)) {
+
+		if (projectManager.getPrivateProjectWebsiteAccessibility(unixName, user) || projectManager.getPublicProjectWebsiteAccessibility(unixName)) {
 			model.addAttribute("project", project);
 			return "sites/website";
-		}
-		else {
-			return "forbidden";	
+		} else {
+			return "forbidden";
 		}
 	}
-	
+
 	/**
 	 * This method retrieves all the networks associated with the project based on the project unix name
 	 * 
-	 * If the project contains networks, it displays all of the networks along with the names of the workspaces
-	 * that contain the networks. If no networks have been created for that
+	 * If the project contains networks, it displays all of the networks along with the names of the workspaces that contain the networks. If no networks have been created for that
 	 * particular project, then an appropriate error page is displayed.
 	 * 
-	 * @param unixName 							unix name that is given to the project at the time of its creation
-	 * @param model								Model object to map values to view
-	 * @return 									returns a string to access the browse networks page of the project external website
-	 * @throws QuadrigaStorageException			Database storage exception thrown
+	 * @param unixName
+	 *            unix name that is given to the project at the time of its creation
+	 * @param model
+	 *            Model object to map values to view
+	 * @return returns a string to access the browse networks page of the project external website
+	 * @throws QuadrigaStorageException
+	 *             Database storage exception thrown
 	 */
-	@RequestMapping(value="sites/{ProjectUnixName}/browsenetworks", method=RequestMethod.GET)
-	public String browseNetworks(@PathVariable("ProjectUnixName") String unixName,Model model, Principal principal) throws QuadrigaStorageException{
+	@RequestMapping(value = "sites/{ProjectUnixName}/browsenetworks", method = RequestMethod.GET)
+	public String browseNetworks(@PathVariable("ProjectUnixName") String unixName, Model model, Principal principal) throws QuadrigaStorageException {
 		IProject project = getProjectDetails(unixName);
 		String projectid = project.getProjectId();
 		List<INetwork> Networks = networkmanager.getNetworksInProject(projectid);
-		
-		//List<String> networkNames = null;
-		if(!Networks.isEmpty()){
+
+		// List<String> networkNames = null;
+		if (!Networks.isEmpty()) {
 			model.addAttribute("networks", Networks);
 			model.addAttribute("project", project);
 			return "sites/browseNetworks";
 		}
-		
+
 		return "NoNetworks";
 	}
-	
+
 	/**
-	 * This method gives the visualization of the network with the given network id 
-	 * @param networkId 					network id of the network that has to be visualized
-	 * @param model							ModelMap object to map values to view
-	 * @param principal						current session user
-	 * @return								returns a string to access the visualize network page of the project external website
-	 * @throws QuadrigaStorageException		Database storage exception thrown
-	 * @throws JAXBException				JAXB exception while getting the JSON
+	 * This method gives the visualization of the network with the given network id
+	 * 
+	 * @param networkId
+	 *            network id of the network that has to be visualized
+	 * @param model
+	 *            ModelMap object to map values to view
+	 * @param principal
+	 *            current session user
+	 * @return returns a string to access the visualize network page of the project external website
+	 * @throws QuadrigaStorageException
+	 *             Database storage exception thrown
+	 * @throws JAXBException
+	 *             JAXB exception while getting the JSON
 	 */
 	@RequestMapping(value = "sites/{projectUnixName}/networks/{networkId}", method = RequestMethod.GET)
-	public String visualizeNetworks(@PathVariable("projectUnixName") String unixName, @PathVariable("networkId") String networkId, ModelMap model, Principal principal) throws QuadrigaStorageException, JAXBException {
+	public String visualizeNetworks(@PathVariable("projectUnixName") String unixName, @PathVariable("networkId") String networkId, ModelMap model, Principal principal)
+			throws QuadrigaStorageException, JAXBException {
 		INetwork network = networkmanager.getNetwork(networkId);
-		if(network==null){
+		if (network == null) {
 			return "auth/accessissue";
 		}
 		IProject project = getProjectDetails(unixName);
 		model.addAttribute("project", project);
-		
+
 		ITransformedNetwork transformedNetwork = transformationManager.getTransformedNetwork(networkId);
-		
-		String nwId = "\""+networkId+"\"";
-		model.addAttribute("networkid",nwId);
+
+		String nwId = "\"" + networkId + "\"";
+		model.addAttribute("networkid", nwId);
 		String json = null;
-		if(transformedNetwork!=null){
+		if (transformedNetwork != null) {
 			json = d3Creator.getD3JSON(transformedNetwork.getNodes(), transformedNetwork.getLinks());
 		}
-		model.addAttribute("jsonstring",json);
+		model.addAttribute("jsonstring", json);
 		return "sites/networks/visualize";
 	}
+
+	@RequestMapping(value = "sites/{ProjectUnixName}/about", method = RequestMethod.GET)
+	public String showAbout(@PathVariable("ProjectUnixName") String unixName, Model model, Principal principal) throws QuadrigaStorageException {
+		IProject project = getProjectDetails(unixName);
+		String projectid = project.getProjectId();
+		List<INetwork> Networks = networkmanager.getNetworksInProject(projectid);
+		model.addAttribute("project", project);
+
+		return "sites/about";
+	}
+
 }
--- Quadriga/src/main/webapp/WEB-INF/views/public/navbar.jsp
+++ Quadriga/src/main/webapp/WEB-INF/views/public/navbar.jsp
@@ -2,3 +2,4 @@
 <li ${currentPage == "networks" ? "class=\"active\"" : ""}><a href="${pageContext.servletContext.contextPath}/sites/${project.unixName}/browsenetworks">Networks</a></li>
 <li ${currentPage == "exploregraph" ? "class=\"active\"" : ""}><a href="">Explore</a></li>
 <li ${currentPage == "networksearch" ? "class=\"active\"" : ""}><a href="">Search</a></li>
+<li ${currentPage == "about" ? "class=\"active\"" : ""}><a href="${pageContext.servletContext.contextPath}/sites/${project.unixName}/about">About</a></li>
