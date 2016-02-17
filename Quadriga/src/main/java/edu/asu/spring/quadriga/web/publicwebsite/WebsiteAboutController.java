5452c2f8fe5516ba8a17152edd8b8192b155c670
rajat <rajataggarwal91@gmail.com>
2016-02-17 15:25:36 -0700

    QUAD-90: Made corrections to Public website about page and controller

--- Quadriga/src/main/java/edu/asu/spring/quadriga/web/publicwebsite/WebsiteAboutController.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/publicwebsite/WebsiteAboutController.java
@@ -19,21 +19,9 @@ public class WebsiteAboutController {
 	@Autowired
 	private IRetrieveProjectManager projectManager;
 
-	private IProject getProjectDetails(String name) throws QuadrigaStorageException {
-		return projectManager.getProjectDetailsByUnixName(name);
-	}
-
-	public IRetrieveProjectManager getProjectManager() {
-		return projectManager;
-	}
-
-	public void setProjectManager(IRetrieveProjectManager projectManager) {
-		this.projectManager = projectManager;
-	}
-
 	@RequestMapping(value = "sites/{ProjectUnixName}/about", method = RequestMethod.GET)
 	public String showAbout(@PathVariable("ProjectUnixName") String unixName, Model model, Principal principal) throws QuadrigaStorageException {
-		IProject project = getProjectDetails(unixName);
+		IProject project = projectManager.getProjectDetailsByUnixName(unixName);
 		String aboutProject = "<i>This line describes project in italics</i><br> <b>This is bold</b>";
 		model.addAttribute("project", project);
 		model.addAttribute("aboutProject", aboutProject);
--- Quadriga/src/main/webapp/WEB-INF/views/public/navbar.jsp
+++ Quadriga/src/main/webapp/WEB-INF/views/public/navbar.jsp
@@ -1,5 +1,5 @@
 <li ${currentPage == "home" ? "class=\"active\"" : ""}><a href="${pageContext.servletContext.contextPath}/sites/${project.unixName}">Home</a></li>
 <li ${currentPage == "networks" ? "class=\"active\"" : ""}><a href="${pageContext.servletContext.contextPath}/sites/${project.unixName}/browsenetworks">Networks</a></li>
-<li ${currentPage == "exploregraph" ? "class=\"active\"" : ""}><a href="">Explore</a></li>
+<li ${currentPage == "exploregraph" ? "class=\"active\"" : ""}><a href="${pageContext.servletContext.contextPath}/sites/${project.unixName}/networks">Explore</a></li>
 <li ${currentPage == "networksearch" ? "class=\"active\"" : ""}><a href="">Search</a></li>
 <li ${currentPage == "about" ? "class=\"active\"" : ""}><a href="${pageContext.servletContext.contextPath}/sites/${project.unixName}/about">About</a></li>
