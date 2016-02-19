a48639ce8a1f5812a282a951332512cf15771cb6
rajat <rajataggarwal91@gmail.com>
2016-02-19 10:36:46 -0700

    Added Javadoc and updated indentation format

--- Quadriga/src/main/java/edu/asu/spring/quadriga/web/publicwebsite/WebsiteAboutController.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/publicwebsite/WebsiteAboutController.java
@@ -1,3 +1,11 @@
+/**
+ * This controller is to map project/about page for public website. 
+ * A formatted string is sent to jsp which is displayed in the correct HTML format  
+ * 
+ * @author Rajat Aggarwal
+ *
+ */
+
 package edu.asu.spring.quadriga.web.publicwebsite;
 
 import java.security.Principal;
@@ -16,16 +24,16 @@ import edu.asu.spring.quadriga.service.workbench.IRetrieveProjectManager;
 @Controller
 public class WebsiteAboutController {
 
-	@Autowired
-	private IRetrieveProjectManager projectManager;
+    @Autowired
+    private IRetrieveProjectManager projectManager;
 
-	@RequestMapping(value = "sites/{ProjectUnixName}/about", method = RequestMethod.GET)
-	public String showAbout(@PathVariable("ProjectUnixName") String unixName, Model model, Principal principal) throws QuadrigaStorageException {
-		IProject project = projectManager.getProjectDetailsByUnixName(unixName);
-		String aboutProject = "<i>This line describes project in italics</i><br> <b>This is bold</b>";
-		model.addAttribute("project", project);
-		model.addAttribute("aboutProject", aboutProject);
-		return "sites/public/PublicWebsiteAbout";
-	}
+    @RequestMapping(value = "sites/{ProjectUnixName}/about", method = RequestMethod.GET)
+    public String showAbout(@PathVariable("ProjectUnixName") String unixName, Model model, Principal principal) throws QuadrigaStorageException {
+        IProject project = projectManager.getProjectDetailsByUnixName(unixName);
+        String aboutProject = "<i>This line describes project in italics</i><br> <b>This is bold</b>";
+        model.addAttribute("project", project);
+        model.addAttribute("aboutProject", aboutProject);
+        return "sites/public/PublicWebsiteAbout";
+    }
 
 }
