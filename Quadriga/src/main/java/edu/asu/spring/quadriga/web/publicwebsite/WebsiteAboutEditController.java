970065c59f94325134ea2ac886fcd7a440801940
rajat <rajataggarwal91@gmail.com>
2016-03-01 22:55:49 -0700

    QUAD-115: Added javadoc and title on jsp

--- Quadriga/src/main/java/edu/asu/spring/quadriga/web/publicwebsite/WebsiteAboutEditController.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/publicwebsite/WebsiteAboutEditController.java
@@ -13,21 +13,26 @@ import edu.asu.spring.quadriga.domain.workbench.IProject;
 import edu.asu.spring.quadriga.exceptions.QuadrigaStorageException;
 import edu.asu.spring.quadriga.service.workbench.IRetrieveProjectManager;
 
+/**
+ * This controller handles requests for editing project's public website's about page.
+ * 
+ * @author Rajat Aggarwal
+ *
+ */
+
 @Controller
 public class WebsiteAboutEditController {
 
     @Autowired
     private IRetrieveProjectManager projectManager;
 
-    private IProject getProjectDetails(String name) throws QuadrigaStorageException {
-        return projectManager.getProjectDetailsByUnixName(name);
-    }
-
     @RequestMapping(value = "sites/{ProjectUnixName}/EditAbout", method = RequestMethod.GET)
     public String showAbout(@PathVariable("ProjectUnixName") String unixName, Model model, Principal principal) throws QuadrigaStorageException {
         IProject project = projectManager.getProjectDetailsByUnixName(unixName);
+        String title = "Project Title will be here";
         String aboutProject = "<i>This line describes project in italics</i><br> <b>This is bold</b>";
         model.addAttribute("project", project);
+        model.addAttribute("title", title);
         model.addAttribute("aboutProject", aboutProject);
         return "sites/public/publicWebsiteEditAbout";
     }
