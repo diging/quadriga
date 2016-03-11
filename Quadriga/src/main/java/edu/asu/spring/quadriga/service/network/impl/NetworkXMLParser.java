5a45f9c6453b60a5cce1d78e7ebd43663508178a
Supraj Kolluri <supcena@gmail.com>
2016-03-11 09:43:26 -0700

    QUAD-66: Adding exception and logic changes

--- Quadriga/src/main/java/edu/asu/spring/quadriga/service/network/impl/NetworkXMLParser.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/service/network/impl/NetworkXMLParser.java
@@ -7,36 +7,49 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
+import edu.asu.spring.quadriga.exceptions.NetworkXMLParseException;
 import edu.asu.spring.quadriga.service.network.INetworkXMLParser;
 
+/**
+ * 
+ * @author skollur1
+ *
+ */
 @Service
 public class NetworkXMLParser implements INetworkXMLParser {
 
-	@Override
-	public String storeText(String xml) {
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		try {
-			DocumentBuilder builder = factory.newDocumentBuilder();
-			Document document = builder.parse(new InputSource(new StringReader(xml)));
-			Node textNode = document.getElementsByTagName("text").item(0);
-			if (textNode == null) {
-				return "";
-			}
-			System.out.println(textNode.getTextContent());
-			NodeList list = document.getElementsByTagName("refId");
-			for (int temp = 0; temp < list.getLength(); temp++) {
-				Node curNode = list.item(temp);
-				System.out.println(curNode.getTextContent());
-			}
+    @Override
+    public String storeText(String xml) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new InputSource(new StringReader(xml)));
+            NodeList textNodeList = document.getElementsByTagName("text");
+            if (textNodeList.getLength() == 0) {
+                return "Success";
+            }
+            String text = textNodeList.item(0).getTextContent();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return "";
-	}
+            NodeList handle = document.getElementsByTagName("handle");
+            NodeList fileName = document.getElementsByTagName("file_name");
+            if (handle.getLength() == 0 && fileName.getLength() == 0) {
+                throw new NetworkXMLParseException("Please specify the Handle and file name in the input XML ");
+            } else if (handle.getLength() == 0) {
+                throw new NetworkXMLParseException("Please specify the Handle in the input XML ");
+            } else if (fileName.getLength() == 0) {
+                throw new NetworkXMLParseException("Please specify the file name in the input XML ");
+            }
+
+            String handleID = handle.item(0).getTextContent();
+            String filename = fileName.item(0).getTextContent();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 
 }
