b378bcbb6a7cb1864d83316572c7f6ac9343f16e
Kavinya <Kavinya@192.168.43.250>
2016-03-14 19:37:44 -0700

    QUAD 112 : Incorportated review comments. Backing bean object not implemented.

--- Quadriga/src/main/java/edu/asu/spring/quadriga/web/publicwebsite/PublicBlogController.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/publicwebsite/PublicBlogController.java
@@ -1,13 +1,13 @@
 package edu.asu.spring.quadriga.web.publicwebsite;
 
-import java.security.Principal;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,45 +16,52 @@ import edu.asu.spring.quadriga.domain.workbench.IProject;
 import edu.asu.spring.quadriga.exceptions.QuadrigaStorageException;
 import edu.asu.spring.quadriga.service.workbench.IRetrieveProjectManager;
 
+/**
+ * This controller displays the public blog contents and returns json as
+ * response with blog title, date, author and content.
+ *
+ * @author Kavinya Rajendran
+ */
 @PropertySource(value = "classpath:/user.properties")
 @Controller
 public class PublicBlogController {
-	@Autowired 
-	private IRetrieveProjectManager projectManager;
-	
-	private IProject getProjectDetails(String name) throws QuadrigaStorageException{
-		return projectManager.getProjectDetailsByUnixName(name);
-	}
-	
-	/**
-	 * This method gives the the projectblog
-	 * @param projectUnixName	The project unix name
-	 * @param model				Model
-	 * @return view
-	 * @throws QuadrigaStorageException
-	 */
-	@RequestMapping(value = "sites/{projectUnixName}/projectblog", method = RequestMethod.GET)
-	public String projectblog(@PathVariable("projectUnixName") String projectUnixName,
-									   Model model)
-			throws QuadrigaStorageException {
-		IProject project = getProjectDetails(projectUnixName);
+    @Autowired
+    private IRetrieveProjectManager projectManager;
 
-		if (project == null) {
-			return "forbidden";
-		}
+    /**
+     * This method gives the the projectblog
+     * 
+     * @param projectUnixName
+     *            The project unix name
+     * @param model
+     *            Model
+     * @return view
+     * @throws QuadrigaStorageException
+     */
+    @RequestMapping(value = "sites/{projectUnixName}/projectblog", method = RequestMethod.GET)
+    public String projectblog(
+            @PathVariable("projectUnixName") String projectUnixName, Model model)
+            throws QuadrigaStorageException {
+        IProject project = projectManager
+                .getProjectDetailsByUnixName(projectUnixName);
 
-		// Creating Dummy Object
-		List dummyList = new ArrayList ();
-		HashMap <String, String> map = new HashMap<String, String>();
-		map.put("title", "Article: Apple's iPhone Blunder");
-		map.put("text", "<b> Can the United States </b> government compel Apple to help break into the phone of Syed Rizwan Farook, who, along with his wife Tafsheen Malil, gunned down fourteen innocent people last December at the Inland Regional Center in San Bernardino? That question has sparked fireworks in recent days. The dispute arises because Apple has equipped its new iPhones with encryption settings that erase the data contained on the phone whenever ten false password entries have been made. It was agreed on all sides that only Apple has the technology that might overcome the encryption device. [...]");
-		map.put("date", "February 22, 2016");
-		map.put("author", "Daniel T. Richards ");
+        if (project == null) {
+            return "forbidden";
+        }
 
-		dummyList.add(map);
+        // Creating Dummy Object
+        List dummyList = new ArrayList();
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("title", "Article: Apple's iPhone Blunder");
+        map.put("text",
+                "<b> Can the United States </b> government compel Apple to help break into the phone of Syed Rizwan Farook, who, along with his wife Tafsheen Malil, gunned down fourteen innocent people last December at the Inland Regional Center in San Bernardino? That question has sparked fireworks in recent days. The dispute arises because Apple has equipped its new iPhones with encryption settings that erase the data contained on the phone whenever ten false password entries have been made. It was agreed on all sides that only Apple has the technology that might overcome the encryption device. [...]");
+        map.put("date", "February 22, 2016");
+        map.put("author", "Daniel T. Richards ");
 
-	    model.addAttribute("blockentrylist", dummyList);
+        dummyList.add(map);
 
-		return "sites/projectblog";
-	}
+        model.addAttribute("blockentrylist", dummyList);
+
+        return "sites/projectblog";
+    }
 }
