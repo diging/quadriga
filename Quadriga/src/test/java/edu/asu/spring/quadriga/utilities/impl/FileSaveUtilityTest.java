54db2c92712e53003107e2b7a5069b0cbc173052
Nischal Samji <nischalsamji@gmail.com>
2016-04-07 15:43:02 -0700

    Tests for Quad-79

---
+++ Quadriga/src/test/java/edu/asu/spring/quadriga/utilities/impl/FileSaveUtilityTest.java
@@ -0,0 +1,39 @@
+package edu.asu.spring.quadriga.utilities.impl;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import edu.asu.spring.quadriga.exceptions.FileStorageException;
+import edu.asu.spring.quadriga.utilities.IFileSaveUtility;
+import junit.framework.Assert;
+
+public class FileSaveUtilityTest {
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+    
+    @Autowired
+    @Qualifier("txtfileSaveUtil")
+    private IFileSaveUtility fileSaveUtil;
+    
+    @Before
+    public void init() throws Exception{
+        File tempFile = tempFolder.newFile("test.txt");
+        FileUtils.fileWrite(tempFile.getAbsolutePath(), "testdata");
+        
+    }
+    
+    @Test
+    public void successFileWrite() throws FileNotFoundException, FileStorageException, IOException{
+        Assert.assertEquals(true,fileSaveUtil.saveFiletoDir("root","zapak", "chillar".getBytes()));
+    }
+}
