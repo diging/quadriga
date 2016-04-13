03d26a4f5f713996939cf196e8e5949a0e53b397
prasanthpriyanesan <prasanthpriyanesan@gmail.com>
2016-04-13 01:16:37 -0700

    "Implemented AJAX in the public page settings page"

--- Quadriga/src/main/java/edu/asu/spring/quadriga/web/workbench/PublicPageController.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/workbench/PublicPageController.java
@@ -1,23 +1,11 @@
 package edu.asu.spring.quadriga.web.workbench;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 import javax.annotation.Resource;
-
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ValidationUtils;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,11 +16,8 @@ import edu.asu.spring.quadriga.aspects.annotations.AccessPolicies;
 import edu.asu.spring.quadriga.aspects.annotations.CheckedElementType;
 import edu.asu.spring.quadriga.aspects.annotations.ElementAccessPolicy;
 import edu.asu.spring.quadriga.domain.factory.workbench.IPublicPageFactory;
-import edu.asu.spring.quadriga.domain.impl.workbench.PublicPage;
 import edu.asu.spring.quadriga.exceptions.QuadrigaAccessException;
 import edu.asu.spring.quadriga.exceptions.QuadrigaStorageException;
-import edu.asu.spring.quadriga.profile.ISearchResult;
-import edu.asu.spring.quadriga.validator.PublicPageValidator;
 import edu.asu.spring.quadriga.web.login.RoleNames;
 
 @Controller
@@ -44,76 +29,42 @@ public class PublicPageController {
     @Resource(name = "projectconstants")
     private Properties messages;
 
-    @Autowired
-    private PublicPageValidator validator;
-
-    /**
-     * Attach the custom validator to the Spring context
-     *
-     */
-    @InitBinder
-    protected void initBinder(WebDataBinder binder) {
-        binder.setValidator(validator);
-    }
-
     /**
      * This method is called during the load of Public page settings form
      *
      * @return model - model object
      */
     @AccessPolicies({ @ElementAccessPolicy(type = CheckedElementType.PROJECT, paramIndex = 1, userRole = {
-            RoleNames.ROLE_COLLABORATOR_ADMIN, RoleNames.ROLE_PROJ_COLLABORATOR_ADMIN }) })
+            RoleNames.ROLE_COLLABORATOR_ADMIN,
+            RoleNames.ROLE_PROJ_COLLABORATOR_ADMIN }) })
     @RequestMapping(value = "auth/workbench/{projectid}/addpublicpage", method = RequestMethod.GET)
-    public ModelAndView publicPageSettingsForm(@PathVariable("projectid") String projectid)
+    public ModelAndView publicPageSettingsForm(
+            @PathVariable("projectid") String projectid)
             throws QuadrigaStorageException, QuadrigaAccessException {
         ModelAndView model = new ModelAndView("auth/workbench/addpublicpage");
-        model.getModelMap().put("publicpage", publicPageFactory.createPublicPageObject());
+        model.getModelMap().put("publicpage",
+                publicPageFactory.createPublicPageObject());
         model.getModelMap().put("ppprojectid", projectid);
         return model;
     }
 
-    // /**
-    // * This method is called for the validation of the Public page settings
-    // form
-    // *
-    // * @return model - model object
-    // */
-    // @AccessPolicies({ @ElementAccessPolicy(type = CheckedElementType.PROJECT,
-    // paramIndex = 3, userRole = {
-    // RoleNames.ROLE_COLLABORATOR_ADMIN, RoleNames.ROLE_PROJ_COLLABORATOR_ADMIN
-    // }) })
-    // @RequestMapping(value = "auth/workbench/{projectid}/addpublicpage1",
-    // method = RequestMethod.GET)
-    // public ModelAndView publicPageSettingsFormSubmit(@Validated
-    // @ModelAttribute("publicpage") PublicPage publicPage,
-    // BindingResult result, @PathVariable("projectid") String projectid) {
-    // if (result.hasErrors()) {
-    // ModelAndView model = new ModelAndView("auth/workbench/addpublicpage");
-    // return model;
-    // }
-    // ModelAndView model = new ModelAndView("auth/workbench/addpublicpage");
-    // model.getModelMap().put("publicpage",
-    // publicPageFactory.createPublicPageObject());
-    // model.getModelMap().put("ppprojectid", projectid);
-    // return model;
-    // }
- 
     /**
-     * This method returns json data
+     * This method is used update the Text blocks in the Public settings page
      *
      * @return json
      */
-    @RequestMapping(value = "auth/workbench/{projectid}/addpublicpage1", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public PublicPageValidator publicPageSettingsFormSubmit(
-            @Validated @ModelAttribute("publicpage") PublicPage publicPage, BindingResult result) {
-        PublicPageValidator res = new PublicPageValidator();
-        if (!result.hasErrors()) {
-            res.setStatus("SUCCESS");
-        } else {
-            res.setStatus("FAIL");
-        }
 
-        return res;
+    @RequestMapping(method = RequestMethod.GET, value = "auth/workbench/{projectid}/addpublicpage1")
+    public @ResponseBody ResponseEntity<String> getExistingConcepts(
+            @RequestParam("data1") String data1)
+            throws QuadrigaStorageException, QuadrigaAccessException {
+        if (data1.isEmpty())
+            return null;
+        try {
+            return new ResponseEntity<String>("Successfully updated",
+                    HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
