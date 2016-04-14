3d42ecd8c4eb39414c16444ff59e825b53268acd
Nischal Samji <nischalsamji@gmail.com>
2016-04-13 17:04:38 -0700

    Adding Transactional support

--- Quadriga/src/main/java/edu/asu/spring/quadriga/service/textfile/impl/TextFileManager.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/service/textfile/impl/TextFileManager.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import edu.asu.spring.quadriga.dao.textfile.ITextFileDAO;
 import edu.asu.spring.quadriga.domain.workspace.ITextFile;
