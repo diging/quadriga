8205cebdf3572c51eb31a4ce38ea90e96ac64589
rajat <rajataggarwal91@gmail.com>
2016-04-21 22:32:12 -0700

    QUAD-154: Bug fixes as mentioned in comments

--- Quadriga/src/test/java/edu/asu/spring/quadriga/service/publicwebsite/impl/AboutTextManagerTest.java
+++ Quadriga/src/test/java/edu/asu/spring/quadriga/service/publicwebsite/impl/AboutTextManagerTest.java
@@ -1,6 +1,7 @@
 package edu.asu.spring.quadriga.service.publicwebsite.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +12,7 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import edu.asu.spring.quadriga.dao.impl.publicwebsite.AboutTextDAO;
+import edu.asu.spring.quadriga.dao.publicwebsite.IAboutTextDAO;
 import edu.asu.spring.quadriga.dto.AboutTextDTO;
 import edu.asu.spring.quadriga.exceptions.QuadrigaStorageException;
 import edu.asu.spring.quadriga.service.impl.publicwebsite.AboutTextManager;
@@ -21,46 +23,54 @@ public class AboutTextManagerTest {
     private AboutTextManager aboutTextManager;
 
     @Mock
-    private AboutTextDAO mockedAboutTextDAO;
+    private IAboutTextDAO mockedAboutTextDAO = Mockito.mock(AboutTextDAO.class);
 
     private ArgumentCaptor<AboutTextDTO> argument;
-    private AboutTextDTO dto;
 
     @Before
-    public void saveAbout() throws QuadrigaStorageException {
+    public void setUp() throws QuadrigaStorageException {
         MockitoAnnotations.initMocks(this);
-        aboutTextManager.saveAbout("1st", "Title", "Description");
-        aboutTextManager.saveAbout("1st", "NewTitle", "NewDescription");
 
     }
 
     @Test
-    public void testSaveAboutCall() throws QuadrigaStorageException {
-        dto = new AboutTextDTO();
-        dto.setProjectId("1st");
-        dto.setTitle("Title");
-        dto.setDescription("Description");
+    public void testSaveAboutNewDTO() throws QuadrigaStorageException {
+
+        Mockito.when(mockedAboutTextDAO.generateUniqueID()).thenReturn("UniqueId1");
+        aboutTextManager.saveAbout("1st", "Title", "Description");
+        
+        Mockito.verify(mockedAboutTextDAO).generateUniqueID();
 
         argument = ArgumentCaptor.forClass(AboutTextDTO.class);
-        Mockito.verify(mockedAboutTextDAO, Mockito.times(2)).saveOrUpdateDTO(argument.capture());
-        assertEquals(dto.getProjectId(), argument.getAllValues().get(0).getProjectId());
-        assertEquals(dto.getTitle(), argument.getAllValues().get(0).getTitle());
-        assertEquals(dto.getDescription(), argument.getAllValues().get(0).getDescription());
-
+        Mockito.verify(mockedAboutTextDAO).saveOrUpdateDTO(argument.capture());
+        assertEquals("UniqueId1",argument.getValue().getId());
+        assertEquals("1st", argument.getValue().getProjectId());
+        assertEquals("Title", argument.getValue().getTitle());
+        assertEquals("Description", argument.getValue().getDescription());
+        
     }
 
     @Test
-    public void testSaveAboutReCall() {
-        dto = new AboutTextDTO();
+    public void testSaveAboutUpdateDTO() throws QuadrigaStorageException {
+        
+        aboutTextManager.saveAbout("1st", "NewTitle", "NewDescription");
+        
+        AboutTextDTO dto = new AboutTextDTO();
         dto.setProjectId("1st");
         dto.setTitle("NewTitle");
         dto.setDescription("NewDescription");
+        Mockito.when(mockedAboutTextDAO.getDTOByProjectId("1st")).thenReturn(dto);
 
+        
+        aboutTextManager.saveAbout("1st", "NewTitle2", "NewDescription2");
+
+        Mockito.verify(mockedAboutTextDAO, Mockito.times(1)).generateUniqueID();
+        
         argument = ArgumentCaptor.forClass(AboutTextDTO.class);
         Mockito.verify(mockedAboutTextDAO, Mockito.times(2)).saveOrUpdateDTO(argument.capture());
-        assertEquals(dto.getProjectId(), argument.getAllValues().get(1).getProjectId());
-        assertEquals(dto.getTitle(), argument.getAllValues().get(1).getTitle());
-        assertEquals(dto.getDescription(), argument.getAllValues().get(1).getDescription());
+        assertEquals("1st", argument.getValue().getProjectId());
+        assertEquals("NewTitle2", argument.getValue().getTitle());
+        assertEquals("NewDescription2", argument.getValue().getDescription());
 
     }
 
