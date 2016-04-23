3f0fa80d3d89c614b396f031ab6bc2c764ab8a53
jvutukur <jvutukur@asu.edu>
2016-04-23 13:16:10 -0700

    QUAD-138 added javadoc to Validor methods

--- Quadriga/src/main/java/edu/asu/spring/quadriga/validator/TransfomationFilesValidator.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/validator/TransfomationFilesValidator.java
@@ -7,6 +7,12 @@ import org.springframework.validation.Validator;
 
 import edu.asu.spring.quadriga.web.transformation.TransformFilesBackingBean;
 
+/**
+ * 
+ * @author Jaya Venkat
+ * This class has methods which validates transformation files data
+ *
+ */
 @Service
 public class TransfomationFilesValidator implements Validator{
 
@@ -15,6 +21,9 @@ public class TransfomationFilesValidator implements Validator{
 		return arg0.isAssignableFrom(TransformFilesBackingBean.class);
 	}
 
+	/**
+	 * Validates title, patternTitle, mappingTitle fields are empty or just white spaces  
+	 */
 	@Override
 	public void validate(Object obj, Errors error) {
 		ValidationUtils.rejectIfEmptyOrWhitespace(error, "title", "title is required");
