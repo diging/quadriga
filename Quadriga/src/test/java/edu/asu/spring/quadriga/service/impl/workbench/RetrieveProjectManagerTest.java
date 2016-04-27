bf38983361fd5746c65d7986a268a2421a40a483
Charan Thej Aware <charan.1312@gmail.com>
2016-04-27 10:40:27 -0700

    QUAD-147-sprint9: Indentation changes

--- Quadriga/src/test/java/edu/asu/spring/quadriga/service/impl/workbench/RetrieveProjectManagerTest.java
+++ Quadriga/src/test/java/edu/asu/spring/quadriga/service/impl/workbench/RetrieveProjectManagerTest.java
@@ -2,10 +2,8 @@ package edu.asu.spring.quadriga.service.impl.workbench;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -13,7 +11,6 @@ import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-
 import edu.asu.spring.quadriga.dao.workbench.IRetrieveProjectDAO;
 import edu.asu.spring.quadriga.domain.enums.EProjectAccessibility;
 import edu.asu.spring.quadriga.domain.proxy.ProjectProxy;
@@ -24,61 +21,61 @@ import edu.asu.spring.quadriga.service.workbench.mapper.IProjectShallowMapper;
 
 public class RetrieveProjectManagerTest {
 
-	@Mock
-	private IRetrieveProjectDAO mockedDBConnect = Mockito.mock(IRetrieveProjectDAO.class);
+    @Mock
+    private IRetrieveProjectDAO mockedDBConnect = Mockito.mock(IRetrieveProjectDAO.class);
 
-	@Mock
-	private IProjectShallowMapper mockedProjectShallowMapper = Mockito.mock(IProjectShallowMapper.class);
+    @Mock
+    private IProjectShallowMapper mockedProjectShallowMapper = Mockito.mock(IProjectShallowMapper.class);
 
-	@InjectMocks
-	private RetrieveProjectManager retrieveProjectManagerUnderTest;
+    @InjectMocks
+    private RetrieveProjectManager retrieveProjectManagerUnderTest;
 
-	@Before
-	public void setUp() throws QuadrigaStorageException {
-		MockitoAnnotations.initMocks(this);
-	}
+    @Before
+    public void setUp() throws QuadrigaStorageException {
+        MockitoAnnotations.initMocks(this);
+    }
 
-	@Test
-	public void checkOnlyPublicProjectsAreReturned() throws QuadrigaStorageException{
-		Mockito.when(mockedDBConnect.getAllProjectsDTOByAccessibility("PUBLIC")).thenReturn(createPublicProjectDTOList());
-		Mockito.when(mockedProjectShallowMapper.getProjectDetails(Matchers.any(ProjectDTO.class))).thenReturn(createPublicIProject());
-		List<IProject> projectList = retrieveProjectManagerUnderTest.getProjectListByAccessibility("PUBLIC");
+    @Test
+    public void checkOnlyPublicProjectsAreReturned() throws QuadrigaStorageException {
+        Mockito.when(mockedDBConnect.getAllProjectsDTOByAccessibility("PUBLIC")).thenReturn(createPublicProjectDTOList());
+        Mockito.when(mockedProjectShallowMapper.getProjectDetails(Matchers.any(ProjectDTO.class))).thenReturn(createPublicIProject());
+        List<IProject> projectList = retrieveProjectManagerUnderTest.getProjectListByAccessibility("PUBLIC");
 
-		assertNotNull(projectList);
-		assertEquals(1, projectList.size());
-		assertEquals("test Project1", projectList.get(0).getProjectName());
-		assertEquals(EProjectAccessibility.PUBLIC, projectList.get(0).getProjectAccess());
-		
-		Mockito.verify(mockedDBConnect, times(1)).getAllProjectsDTOByAccessibility("PUBLIC");
-		Mockito.verify(mockedProjectShallowMapper, times(1)).getProjectDetails(Matchers.any(ProjectDTO.class));
-	}
+        assertNotNull(projectList);
+        assertEquals(1, projectList.size());
+        assertEquals("test Project1", projectList.get(0).getProjectName());
+        assertEquals(EProjectAccessibility.PUBLIC, projectList.get(0).getProjectAccess());
 
-	@Test
-	public void checkProjectListByAccessibilityReturnsEmptyListIfNoPublicProjects() throws QuadrigaStorageException{
-		Mockito.when(mockedDBConnect.getAllProjectsDTOByAccessibility("PUBLIC")).thenReturn(new ArrayList<ProjectDTO>());
-		List<IProject> projectList = retrieveProjectManagerUnderTest.getProjectListByAccessibility("PUBLIC");
+        Mockito.verify(mockedDBConnect, times(1)).getAllProjectsDTOByAccessibility("PUBLIC");
+        Mockito.verify(mockedProjectShallowMapper, times(1)).getProjectDetails(Matchers.any(ProjectDTO.class));
+    }
 
-		assertNotNull(projectList);
-		assertEquals(0, projectList.size());
-		
-		Mockito.verify(mockedDBConnect, times(1)).getAllProjectsDTOByAccessibility("PUBLIC");
-		Mockito.verify(mockedProjectShallowMapper, times(0)).getProjectDetails(Matchers.any(ProjectDTO.class));
-	}
+    @Test
+    public void checkProjectListByAccessibilityReturnsEmptyListIfNoPublicProjects() throws QuadrigaStorageException {
+        Mockito.when(mockedDBConnect.getAllProjectsDTOByAccessibility("PUBLIC")).thenReturn(new ArrayList<ProjectDTO>());
+        List<IProject> projectList = retrieveProjectManagerUnderTest.getProjectListByAccessibility("PUBLIC");
 
-	private List<ProjectDTO> createPublicProjectDTOList(){
-		ProjectDTO project1 = new ProjectDTO();
-		List<ProjectDTO> projectDTOList = new ArrayList<ProjectDTO>();
-		projectDTOList.add(project1);
+        assertNotNull(projectList);
+        assertEquals(0, projectList.size());
 
-		return projectDTOList;
-	}
+        Mockito.verify(mockedDBConnect, times(1)).getAllProjectsDTOByAccessibility("PUBLIC");
+        Mockito.verify(mockedProjectShallowMapper, times(0)).getProjectDetails(Matchers.any(ProjectDTO.class));
+    }
 
-	private IProject createPublicIProject(){
-		IProject projectProxy = new ProjectProxy(retrieveProjectManagerUnderTest);
-		projectProxy.setCreatedBy("Created By");
-		projectProxy.setProjectName("test Project1");
-		projectProxy.setProjectAccess(EProjectAccessibility.PUBLIC);
+    private List<ProjectDTO> createPublicProjectDTOList() {
+        ProjectDTO project1 = new ProjectDTO();
+        List<ProjectDTO> projectDTOList = new ArrayList<ProjectDTO>();
+        projectDTOList.add(project1);
 
-		return projectProxy;
-	}
+        return projectDTOList;
+    }
+
+    private IProject createPublicIProject() {
+        IProject projectProxy = new ProjectProxy(retrieveProjectManagerUnderTest);
+        projectProxy.setCreatedBy("Created By");
+        projectProxy.setProjectName("test Project1");
+        projectProxy.setProjectAccess(EProjectAccessibility.PUBLIC);
+
+        return projectProxy;
+    }
 }
