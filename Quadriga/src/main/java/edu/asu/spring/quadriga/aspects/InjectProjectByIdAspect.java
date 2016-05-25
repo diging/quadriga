4617c7e45491c54a3468ddf84bb4b0c4a73b2b87
Nischal Samji <nischalsamji@gmail.com>
2016-05-25 16:31:30 -0700

    [QUAD-155]: Update Annotations

--- Quadriga/src/main/java/edu/asu/spring/quadriga/aspects/InjectProjectByIdAspect.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/aspects/InjectProjectByIdAspect.java
@@ -2,23 +2,16 @@ package edu.asu.spring.quadriga.aspects;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
 
-import edu.asu.spring.quadriga.aspects.annotations.InjectProjectByName;
+import edu.asu.spring.quadriga.aspects.annotations.InjectProjectById;
 
-@Aspect
-@Order(value = 10)
-@Component
 public class InjectProjectByIdAspect extends InjectProjectAspect {
 
-    @Around("within(edu.asu.spring.quadriga.web..*) && @annotation(ipName)")
-    public Object injectProject(ProceedingJoinPoint joinPoint, InjectProjectByName ipName) throws Throwable{
-       System.out.println(ipName);
+    @Around("within(edu.asu.spring.quadriga.web..*) && @annotation(ipId)")
+    public Object injectProject(ProceedingJoinPoint joinPoint, InjectProjectById ipId) throws Throwable {
         return super.injectProject(joinPoint);
-        
-    } 
+    }
+    
     @Override
     public String getErrorPage() {
         return null;
---
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/aspects/InjectProjectByNameAspect.java
@@ -0,0 +1,26 @@
+package edu.asu.spring.quadriga.aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import edu.asu.spring.quadriga.aspects.annotations.InjectProjectByName;
+
+@Aspect
+@Order(value = 10)
+@Component
+public class InjectProjectByNameAspect extends InjectProjectAspect {
+
+    @Around("within(edu.asu.spring.quadriga.web..*) && @annotation(ipName)")
+    public Object injectProject(ProceedingJoinPoint joinPoint, InjectProjectByName ipName) throws Throwable {
+        return super.injectProject(joinPoint);
+    }
+
+    @Override
+    public String getErrorPage() {
+        return null;
+    }
+
+}
