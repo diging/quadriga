064417fea1a28c19760e0e47092abbc76c3beebb
Julia Damerow <julia.damerow@googlemail.com>
2016-06-20 13:43:09 +0200

    [QUAD-193] fixed multiple text positions bug?

--- Quadriga/src/main/java/edu/asu/spring/quadriga/service/network/domain/impl/TextOccurance.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/service/network/domain/impl/TextOccurance.java
@@ -54,5 +54,65 @@ public class TextOccurance {
     }
    
     
-    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((access == null) ? 0 : access.hashCode());
+        result = prime * result + ((contents == null) ? 0 : contents.hashCode());
+        if (project != null) {
+            result = prime * result + ((project.getProjectId() == null) ? 0 : project.getProjectId().hashCode());
+        }
+        result = prime * result + ((textId == null) ? 0 : textId.hashCode());
+        int phrasesSum = 0;
+        if (textPhrases != null) {
+            for (TextPhrase phrase : textPhrases) {
+                phrasesSum += phrase == null ? 0 : phrase.hashCode();
+            }
+        }
+        result = prime * result + phrasesSum;
+        result = prime * result + ((textUri == null) ? 0 : textUri.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TextOccurance other = (TextOccurance) obj;
+        if (access != other.access)
+            return false;
+        if (contents == null) {
+            if (other.contents != null)
+                return false;
+        } else if (!contents.equals(other.contents))
+            return false;
+        if (project == null) {
+            if (other.project != null)
+                return false;
+        } else if (project != null) {
+            if (other.project == null)
+                return false;
+        } else if (!project.getProjectId().equals(other.project.getProjectId()))
+            return false;
+        if (textId == null) {
+            if (other.textId != null)
+                return false;
+        } else if (!textId.equals(other.textId))
+            return false;
+        if (textPhrases == null) {
+            if (other.textPhrases != null)
+                return false;
+        } else if (!textPhrases.containsAll(other.textPhrases) && other.textPhrases.containsAll(textPhrases))
+            return false;
+        if (textUri == null) {
+            if (other.textUri != null)
+                return false;
+        } else if (!textUri.equals(other.textUri))
+            return false;
+        return true;
+    }
 }
