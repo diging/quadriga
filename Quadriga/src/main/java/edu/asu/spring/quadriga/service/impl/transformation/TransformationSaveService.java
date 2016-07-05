839fa2b214e02beda3975e6429ec669344b84b0c
yoganandakishore <yyk16293@gmail.com>
2016-07-05 16:07:29 -0700

    [QUAD-138] Making code changes to save metadata about transformation files, for changing directory and file names.

--- Quadriga/src/main/java/edu/asu/spring/quadriga/service/impl/transformation/TransformationSaveService.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/service/impl/transformation/TransformationSaveService.java
@@ -27,9 +27,66 @@ public class TransformationSaveService implements ITransformationSaveService {
     @Override
     public boolean saveFileToLocal(ITransformationFile transformationFile) throws FileStorageException {
 
-        return (transformationFileManager.saveFiletoDir(transformationFile.getPatternTitle(),
-                transformationFile.getPatternFileName(), transformationFile.getPatternFileContent())
-                && transformationFileManager.saveFiletoDir(transformationFile.getMappingTitle(),
-                        transformationFile.getMappingFileName(), transformationFile.getMappingFileContent()));
+        String directoryName = getTransformationDirectoryName(transformationFile);
+        String metaDataContent = getMetaDataContent(transformationFile);
+
+        return (transformationFileManager.saveFiletoDir(directoryName, "metadata.txt", metaDataContent.getBytes())
+                && transformationFileManager.saveFiletoDir(directoryName,
+                        "Pattern_" + transformationFile.getPatternFileName(),
+                        transformationFile.getPatternFileContent())
+                && transformationFileManager.saveFiletoDir(directoryName,
+                        "Mapping_" + transformationFile.getMappingFileName(),
+                        transformationFile.getMappingFileContent()));
+    }
+
+    /**
+     * forms the directory name by appending username followed by
+     * "Transformation" followed by mapping file name and transformation file
+     * names, each separated by underscores.
+     * 
+     * @param transformationFile
+     * @return String which contains the directory name
+     */
+    private String getTransformationDirectoryName(ITransformationFile transformationFile) {
+
+        StringBuilder directoryName = new StringBuilder();
+
+        directoryName.append(transformationFile.getUserName());
+        directoryName.append("_Transformation_");
+        directoryName.append(transformationFile.getMappingTitle());
+        directoryName.append("_");
+        directoryName.append(transformationFile.getPatternTitle());
+
+        return directoryName.toString();
+    }
+
+    /**
+     * takes properties namely nameOfUser, title, description, patternfilename,
+     * mappingfilename and their values each in a separate line.
+     * 
+     * @param transformationFile
+     * @return String which contains nameofUser, title, description,
+     *         patternfilename, mappingfilename each in a separate line
+     * 
+     */
+    private String getMetaDataContent(ITransformationFile transformationFile) {
+
+        StringBuilder content = new StringBuilder();
+        content.append("Name of the User - ");
+        content.append(transformationFile.getUserName());
+        content.append(System.lineSeparator());
+        content.append("Title - ");
+        content.append(transformationFile.getTitle());
+        content.append(System.lineSeparator());
+        content.append("Description - ");
+        content.append(transformationFile.getDescription());
+        content.append(System.lineSeparator());
+        content.append("PatternFileName - ");
+        content.append(transformationFile.getPatternFileName());
+        content.append(System.lineSeparator());
+        content.append("MappingFileName - ");
+        content.append(transformationFile.getMappingFileName());
+
+        return content.toString();
     }
 }
