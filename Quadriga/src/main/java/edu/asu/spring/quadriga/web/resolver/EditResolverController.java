4e5ff230ee90a62d31697a262f4c22478acdea56
yoganandakishore <yyk16293@gmail.com>
2016-07-18 10:09:18 -0700

    [QUAD-209] adding code changes to save the updated resolver

--- Quadriga/src/main/java/edu/asu/spring/quadriga/web/resolver/EditResolverController.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/resolver/EditResolverController.java
@@ -3,51 +3,51 @@ package edu.asu.spring.quadriga.web.resolver;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import edu.asu.spring.quadriga.domain.resolver.IProjectHandleResolver;
 import edu.asu.spring.quadriga.domain.resolver.impl.ProjectHandleResolver;
 import edu.asu.spring.quadriga.service.resolver.IProjectHandleResolverManager;
-import edu.asu.spring.quadriga.validator.ProjectHandleResolverValidator;
 
 @Controller
-public class AddResolverController {
+@Scope("session")
+public class EditResolverController {
 
     @Autowired
     private IProjectHandleResolverManager resolverManager;
 
-    @Autowired
-    private ProjectHandleResolverValidator validator;
+    private String resolverId;
 
-    @InitBinder
-    public void initBinder(WebDataBinder binder) {
-        binder.setValidator(validator);
+    @RequestMapping(value = "/auth/resolvers/edit", method = RequestMethod.POST)
+    public String preparePage(Principal principal, Model model, @RequestParam("resolverId") String resolverId) {
+        // model.addAttribute("resolver", new ProjectHandleResolver());
+
+        IProjectHandleResolver projectHandleResolver = resolverManager.getProjectHandleResolver(resolverId);
+        resolverId = projectHandleResolver.getId();
+        model.addAttribute("resolver", projectHandleResolver);
+        return "auth/resolvers/edit";
     }
 
-    @RequestMapping(value = "/auth/resolvers/add", method = RequestMethod.GET)
-    public String preparePage(Principal principal, Model model) {
-        model.addAttribute("resolver", new ProjectHandleResolver());
-        return "auth/resolvers/add";
-    }
-
-    @RequestMapping(value = "/auth/resolvers/add", method = RequestMethod.POST)
+    @RequestMapping(value = "/auth/resolvers/update", method = RequestMethod.POST)
     public String addResolver(Principal principal,
             @Validated @ModelAttribute("resolver") ProjectHandleResolver resolver, BindingResult results, Model model) {
 
         if (results.hasErrors()) {
             model.addAttribute("resolver", resolver);
-            return "auth/resolvers/add";
+            return "auth/resolvers/edit";
         }
 
+        resolver.setId(resolverId);
         resolverManager.saveProjectHandleResolver(resolver, principal.getName());
-
         return "redirect:/auth/resolvers";
     }
+
 }
