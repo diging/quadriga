86085bd268e37699c1d6b8b25f42f365569f1cce
yoganandakishore <yyk16293@gmail.com>
2016-07-26 12:35:05 -0700

    [QUAD-212] Using spaces instead of tabs for fixing alignment

--- Quadriga/src/main/webapp/resources/js/handleResult.js
+++ Quadriga/src/main/webapp/resources/js/handleResult.js
@@ -1,39 +1,37 @@
 function handleResult(status) {
-
-	if (status == "SUCCESS") {
-	$(".input-group-addon").html('<i class="fa fa-check-circle"></i>');
-	} else if (status == "FAILURE") {
-	$(".input-group-addon").html('<i class="fa fa-times"></i>');
-	}
+    if (status == "SUCCESS") {
+    $(".input-group-addon").html('<i class="fa fa-check-circle"></i>');
+    } else if (status == "FAILURE") {
+    $(".input-group-addon").html('<i class="fa fa-times"></i>');
+    }
 }
 
 function performAction(obj) {
+    var data = {};
+    data['projectName'] = $("#projectName").val();
+    data['projectDescription'] = $('#projectDescription').val();
+    data['projectUrl'] = $('#projectUrl').val();
+    data['handlePattern'] = $('#handlePattern').val();
+    data['resolvedHandlePattern'] = $('#resolvedHandlePattern').val();
+    data['handleExample'] = $('#handleExample').val();
+    data['resolvedHandleExample'] = $('#resolvedHandleExample').val();
 
-	var data = {};
-	data['projectName'] = $("#projectName").val();
-	data['projectDescription'] = $('#projectDescription').val();
-	data['projectUrl'] = $('#projectUrl').val();
-	data['handlePattern'] = $('#handlePattern').val();
-	data['resolvedHandlePattern'] = $('#resolvedHandlePattern').val();
-	data['handleExample'] = $('#handleExample').val();
-	data['resolvedHandleExample'] = $('#resolvedHandleExample').val();
-
-	$
-	.ajax({
-	type : "POST",
-	url : "/quadriga/auth/resolvers/check",
-	data : {
-	data : JSON.stringify(data)
-	},
-	beforeSend : function() {
-	$(".input-group-addon")
-	.html('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>');
-	},
-	success : function(data, status) {
-	handleResult(data);
-	},
-	error : function(e) {
-	console.log("ERROR: ", e.responseText);
-	}
-	});
+    $
+    .ajax({
+    type : "POST",
+    url : "/quadriga/auth/resolvers/check",
+    data : {
+    data : JSON.stringify(data)
+    },
+    beforeSend : function() {
+    $(".input-group-addon")
+    .html('<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>');
+    },
+    success : function(data, status) {
+    handleResult(data);
+    },
+    error : function(e) {
+    console.log("ERROR: ", e.responseText);
+    }
+    });
 }
