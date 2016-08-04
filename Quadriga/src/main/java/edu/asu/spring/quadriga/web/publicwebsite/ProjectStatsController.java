10d3f2a5f8e9eb5ae2d9cc3b15c2ff85dcf7637c
Julia Damerow <jdamerow@users.noreply.github.com>
2016-08-04 15:05:57 -0700

    Update ProjectStatsController.java

--- Quadriga/src/main/java/edu/asu/spring/quadriga/web/publicwebsite/ProjectStatsController.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/publicwebsite/ProjectStatsController.java
@@ -74,7 +74,7 @@ public class ProjectStatsController {
             IConceptStats conceptStats = conceptsList.get(i);
             jsonObject.put("conceptId", conceptStats.getConceptId());
             jsonObject.put("description", StringEscapeUtils.escapeJavaScript(conceptStats.getDescription()));
-            jsonObject.put("label", StringEscapeUtils.escapeJavaScript(conceptStats.getLemma().replace("\"", "")));
+            jsonObject.put("label", StringEscapeUtils.escapeJavaScript(conceptStats.getLemma()));
             jsonObject.put("count", conceptStats.getCount());
             jsonArray.put(jsonObject);
         }
