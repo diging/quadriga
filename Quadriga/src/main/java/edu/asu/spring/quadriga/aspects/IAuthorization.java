2a6ba2db4d3e382cdf9b05b2c64b0086685c72b8
Nischal Samji <nischalsamji@gmail.com>
2016-11-03 15:48:03 -0700

    [QUAD-136]: Update javadoc in IAuthorization interface

--- Quadriga/src/main/java/edu/asu/spring/quadriga/aspects/IAuthorization.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/aspects/IAuthorization.java
@@ -2,6 +2,7 @@ package edu.asu.spring.quadriga.aspects;
 
 import java.util.List;
 
+import edu.asu.spring.quadriga.aspects.annotations.CheckedElementType;
 import edu.asu.spring.quadriga.domain.IUser;
 import edu.asu.spring.quadriga.domain.impl.QuadrigaRole;
 import edu.asu.spring.quadriga.exceptions.QuadrigaAccessException;
@@ -17,12 +18,16 @@ public interface IAuthorization {
 
     /**
      * Method checks authorization based on access object, object id and array
-     * of {@link QuadrigaRole} of {@link IUser}.
+     * of {@link QuadrigaRole} of {@link IUser}. If the object passed is a
+     * string, the Object based on the {@link CheckedElementType} is retrieved.
+     * Otherwise, object is cast into a Project/ Workspace/ Dictionary/
+     * Collection based on the {@link CheckedElementType}
      * 
      * @param userName
      *            User name or id of the {@link IUser}
-     * @param accessObjectId
-     *            Id of any object type we are dealing with
+     * @param accessObject
+     *            Object of the type String or Workspace/ Collection/ Project
+     *            etc.,
      * @param userRoles
      *            Array of {@link IUser}'s {@link QuadrigaRole}
      * @return returns true or false based on authorization of user
