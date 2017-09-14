734d35fc571c67953e832211ec07bf1f28b30e2d
Chiraag Subramanian <chiraag.subramanian@asu.edu>
2017-09-14 12:41:43 -0700

    [QUAD-249] Separate the SecurityConfigurations for Stateful and Stateless endpoints.

--- Quadriga/src/main/java/edu/asu/spring/quadriga/web/config/SecurityContext.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/config/SecurityContext.java
@@ -1,96 +1,139 @@
 package edu.asu.spring.quadriga.web.config;
 
-
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.social.security.SocialUserDetailsService;
 import org.springframework.social.security.SpringSocialConfigurer;
-import edu.asu.spring.quadriga.authentication.UserService;
 import edu.asu.spring.quadriga.web.config.social.SimpleSocialUserDetailsService;
 
-
-@Configuration
 @EnableWebSecurity
-public class SecurityContext extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityContext {
     
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-        // Spring Security ignores request to static resources such as CSS or JS files.
-        .ignoring().antMatchers("/static/**");
+    @Configuration
+    @Order(2)
+    public static class StatefulSecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Autowired
+        @Qualifier("userService")
+        private UserDetailsService userService;
+
+        @Autowired
+        @Qualifier("authSuccessHandler")
+        private SavedRequestAwareAuthenticationSuccessHandler authSuccessHandler;
+
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web
+                    // Spring Security ignores request to static resources such
+                    // as CSS or JS files.
+                    .ignoring().antMatchers("/static/**");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            HeadersConfigurer<HttpSecurity> config = http.antMatcher("**").headers().frameOptions().sameOrigin();
+            config.and().exceptionHandling().accessDeniedPage("/403")
+                    // Configures url based authorization
+                    .and().formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/auth/welcome")
+                    .successHandler(authSuccessHandler).failureUrl("/loginfailed").and().authorizeRequests()
+                    // Anyone can access the urls
+                    .antMatchers("/auth/**", "/signin/**", "/", "/connect/**", "/signup/**", "/user/register/**",
+                            "/resources/**", "/register*", "/login*", "/logout*", "/createaccount*", "/sites/**",
+                            "/public/**", "/search/**", "/home/**")
+                    .permitAll()
+                    // The rest of the our application is protected.
+                    .antMatchers("/users/**", "/admin/**").hasRole("QUADRIGA_USER_ADMIN")
+                    .antMatchers("/", "/auth/welcome**", "/auth/home**", "/auth/about**", "/auth/profile/**",
+                            "/rest/**")
+                    .hasAnyRole("QUADRIGA_USER_ADMIN", "QUADRIGA_USER_STANDARD", "QUADRIGA_USER_COLLABORATOR")
+                    .antMatchers("/auth/workbench/**", "/auth/rest/**", "/auth/conceptcollections/**",
+                            "/auth/conceptdetails/**", "/auth/transformation/**", "/auth/searchitems/**",
+                            "/auth/dictionaries/**", "/auth/editing/**", "/auth/networks/**")
+                    .hasAnyRole("QUADRIGA_USER_STANDARD", "QUADRIGA_USER_COLLABORATOR")
+                    .antMatchers("/users/**", "/admin/**", "/auth/users/**", "/checks/**")
+                    .hasRole("QUADRIGA_USER_ADMIN").anyRequest()
+                    .hasAnyRole("QUADRIGA_USER_ADMIN", "QUADRIGA_USER_STANDARD", "QUADRIGA_USER_COLLABORATOR")
+                    .antMatchers("/**").denyAll()
+                    // Adds the SocialAuthenticationFilter to Spring Security's
+                    // filter chain.
+                    .and().apply(new SpringSocialConfigurer());
+
+        }
+
+        @Autowired
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.authenticationProvider(daoAuthProvider());
+            auth.eraseCredentials(false);
+            auth.inMemoryAuthentication().withUser("admin").password("admin").roles("QUADRIGA_USER_ADMIN");
+            super.configure(auth);
+
+        }
+
+        @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+        @Override
+        public AuthenticationManager authenticationManagerBean() throws Exception {
+            return super.authenticationManager();
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder(10);
+        }
+
+        @Bean
+        public SocialUserDetailsService socialUserDetailsService() {
+            return new SimpleSocialUserDetailsService(userDetailsService());
+        }
+
+        @Bean
+        public UserDetailsService userDetailsService() {
+            return userService;
+        }
+
+        @Bean
+        public DaoAuthenticationProvider daoAuthProvider() throws Exception {
+            DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+            provider.setPasswordEncoder(passwordEncoder());
+            provider.setUserDetailsService(userDetailsService());
+            return provider;
+        }
+
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        
-        http.csrf().disable();
-        HeadersConfigurer<HttpSecurity> config = http.antMatcher("**").headers().frameOptions().sameOrigin();
-        // Configures form login
-        config.and().exceptionHandling().accessDeniedPage("/403")
-                // Configures url based authorization
-                .and()
-                .authorizeRequests()
-                // Anyone can access the urls
-                .antMatchers("/auth/**", "/signin/**", "/", "/connect/**",
-                        "/signup/**", "/user/register/**", "/resources/**",
-                        "/rest/**","/register*","/login*","/logout*","/createaccount*","/sites/**", "/public/**","/search/**","/home/**").permitAll()
-                // The rest of the our application is protected.
-                .antMatchers("/users/**", "/admin/**").hasRole("QUADRIGA_USER_ADMIN")
-                .antMatchers("/auth/welcome**", "/auth/home**","/auth/about**","/auth/profile/**").hasAnyRole("QUADRIGA_USER_ADMIN","QUADRIGA_USER_STANDARD","QUADRIGA_USER_COLLABORATOR")
-                .antMatchers("/auth/workbench/**", "/auth/rest/**","/auth/conceptcollections/**","/auth/conceptdetails/**","/auth/transformation/**","/auth/searchitems/**","/auth/dictionaries/**","/auth/editing/**","/auth/networks/**").hasAnyRole("QUADRIGA_USER_STANDARD","QUADRIGA_USER_COLLABORATOR")
-                .antMatchers("/users/**", "/admin/**","/auth/users/**","/checks/**").hasRole("QUADRIGA_USER_ADMIN")
-                .anyRequest().hasAnyRole("QUADRIGA_USER_ADMIN","QUADRIGA_USER_STANDARD","QUADRIGA_USER_COLLABORATOR")
-                // Adds the SocialAuthenticationFilter to Spring Security's filter chain.
-                .and().apply(new SpringSocialConfigurer());
-    }
-  
-    
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthProvider());
-    }
-  
-    @Bean
-    public SavedRequestAwareAuthenticationSuccessHandler successHandler() {
-        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-        successHandler.setTargetUrlParameter("authSuccessHandler");
-        return successHandler;
-    }
+    // Security Configuration for REST end-points
+    @Configuration
+    @Order(1)
+    public static class StatelessSecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            HeadersConfigurer<HttpSecurity> restConfig = http.antMatcher("/rest/**").headers().frameOptions()
+                    .sameOrigin();
+            restConfig.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            restConfig.and().authorizeRequests().antMatchers("/rest/**").hasAnyRole("QUADRIGA_USER_ADMIN",
+                    "QUADRIGA_USER_STANDARD", "QUADRIGA_USER_COLLABORATOR");
+            restConfig.and().httpBasic();
+            restConfig.and().csrf().disable();
+        }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
-    }
-
-    @Bean
-    public SocialUserDetailsService socialUserDetailsService() {
-        return new SimpleSocialUserDetailsService(userDetailsService());
-    }
-    
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new UserService();
-    }
-    
-    @Bean
-    public DaoAuthenticationProvider daoAuthProvider() throws Exception {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder());
-        provider.setUserDetailsService(userDetailsService());
-        return provider;
     }
 
 }
