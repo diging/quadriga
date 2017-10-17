18f8d1b8785b1c1086e28afc4677cb88d44c6bef
Chiraag Subramanian <chiraag.subramanian@asu.edu>
2017-10-17 12:33:46 -0700

    [QUAD-205] Retain common texts and handles among the searched concepts.

--- Quadriga/src/main/java/edu/asu/spring/quadriga/web/search/TextConceptSearchController.java
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/search/TextConceptSearchController.java
@@ -2,6 +2,7 @@ package edu.asu.spring.quadriga.web.search;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.google.common.collect.Sets;
+
 import edu.asu.spring.quadriga.conceptpower.IConcept;
 import edu.asu.spring.quadriga.conceptpower.IConceptpowerCache;
 import edu.asu.spring.quadriga.domain.enums.EProjectAccessibility;
@@ -56,20 +60,20 @@ public class TextConceptSearchController {
             @RequestParam(value = "conceptid2", defaultValue = "") String conceptId2, Model model) throws Exception {
 
         ArrayList<String> searchedConcepts = new ArrayList<>();
-        List<List<ITextFile>> texts = new ArrayList<List<ITextFile>>();
-        List<List<String>> handles = new ArrayList<List<String>>();
         List<String> conceptUriSearchList = new ArrayList<String>();
         List<IConcept> concepts = new ArrayList<IConcept>();
+        List<Set<String>> referencesList = new ArrayList<Set<String>>();
+        
+        List<ITextFile> texts = new ArrayList<ITextFile>();
+        List<String> handles = new ArrayList<String>();
+        
         searchedConcepts.add(conceptId1);
         searchedConcepts.add(conceptId2);
         for (String conceptUri : searchedConcepts) {
+            
             if (!conceptUri.isEmpty()) {
 
                 IConcept concept = cpCache.getConceptByUri(conceptUri);
-                Set<String> references = new HashSet<String>();
-                List<ITextFile> conceptTexts = new ArrayList<ITextFile>();
-                List<String> conceptHandles = new ArrayList<String>();
-
                 if (concept != null) {
                     concept.setId(conceptUri);
                 }
@@ -81,61 +85,62 @@ public class TextConceptSearchController {
                 }
 
                 String results = qStoreConnector.searchNodesByConcept(conceptId);
-
                 if (results != null && !results.isEmpty()) {
                     ElementEventsType events = marshallingService.unMarshalXmlToElementEventsType(results);
                     List<CreationEvent> eventList = events.getRelationEventOrAppellationEvent();
-
-                    if (concept.getWordnetIds() != null && !concept.getWordnetIds().isEmpty()) {
+                    if (concept.getWordnetIds() != null && !concept.getWordnetIds().isEmpty()) {  
                         results = qStoreConnector.searchNodesByConcept(concept.getWordnetIds().get(0));
                         events = marshallingService.unMarshalXmlToElementEventsType(results);
                         eventList.addAll(events.getRelationEventOrAppellationEvent());
                     }
-
+  
+                    Set<String> references = new HashSet<String>();
                     for (CreationEvent event : eventList) {
                         String sourceRef = event.getSourceReference();
-
-                        // if we haven't seen the reference yet
-                        if (references.add(sourceRef)) {
-                            ITextFile txtFile = textFileManager.getTextFileByUri(sourceRef);
-                            if (txtFile == null || txtFile.getAccessibility() == ETextAccessibility.PRIVATE) {
-                                conceptHandles.add(sourceRef);
-                            } else {
-                                if (txtFile.getAccessibility() == ETextAccessibility.PUBLIC) {
-                                    conceptTexts.add(txtFile);
-                                    textFileManager.loadFile(txtFile);
-                                    txtFile.setSnippetLength(40);
-                                }
-                            }
-                        }
+                        references.add(sourceRef);
                     }
-
+                    referencesList.add(references);
                 }
-
                 concepts.add(concept);
-                handles.add(conceptHandles);
-                texts.add(conceptTexts);
                 conceptUriSearchList.add(conceptUri);
 
             }
         }
-        List<IProject> projects = projectManager.getProjectListByAccessibility(EProjectAccessibility.PUBLIC);
+        
+        if(referencesList.size() >= 1){
+            Set<String> resultSet  = referencesList.get(0);
+            for(int i = 1; i < referencesList.size() ; i++){
+                resultSet = Sets.intersection(resultSet, referencesList.get(i));
+            }
+            Iterator<String> it = resultSet.iterator();
+            while(it.hasNext()){
+               String sourceRef = it.next(); 
+               ITextFile txtFile = textFileManager.getTextFileByUri(sourceRef);
+               if (txtFile == null || txtFile.getAccessibility() == ETextAccessibility.PRIVATE) {
+                   handles.add(sourceRef);
+               } else {
+                   if (txtFile.getAccessibility() == ETextAccessibility.PUBLIC) {
+                       texts.add(txtFile);
+                       textFileManager.loadFile(txtFile);
+                       txtFile.setSnippetLength(40);
+                   }
+               }
+            }
+        }
 
+        List<IProject> projects = projectManager.getProjectListByAccessibility(EProjectAccessibility.PUBLIC);
         List<String> projectIds = new ArrayList<String>();
         projects.forEach(p -> projectIds.add(p.getProjectId()));
 
         ITransformedNetwork transformedNetwork = transformationManager
                 .getSearchTransformedNetworkMultipleProjects(projectIds, conceptUriSearchList, INetworkStatus.APPROVED);
-
         String json = null;
         if (transformedNetwork != null) {
             json = jsonCreator.getJson(transformedNetwork.getNodes(), transformedNetwork.getLinks());
         }
-
         if (transformedNetwork == null || transformedNetwork.getNodes().size() == 0) {
             model.addAttribute("isNetworkEmpty", true);
         }
-
         model.addAttribute("jsonstring", json);
         model.addAttribute("references", handles);
         model.addAttribute("texts", texts);
