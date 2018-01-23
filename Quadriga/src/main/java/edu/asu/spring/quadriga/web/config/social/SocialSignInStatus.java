f49a16aadbfd3354a4054012985115c902a13811
Chiraag Subramanian <chiraag.subramanian@asu.edu>
2018-01-23 12:52:13 -0700

    [QUAD-249] Adding SocialSignInStatus enum.

---
+++ Quadriga/src/main/java/edu/asu/spring/quadriga/web/config/social/SocialSignInStatus.java
@@ -0,0 +1,31 @@
+package edu.asu.spring.quadriga.web.config.social;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum SocialSignInStatus {
+    REGISTRATION_SUCCESS("1"), REGISTRATION_FAILED("2"), ACCOUNT_APPROVAL_PENDING("3");
+    
+    private String socialSignInStatusType;
+    private static final Map<String, SocialSignInStatus> map = new HashMap<>(values().length, 1);
+
+    static {
+      for (SocialSignInStatus s : values()) map.put(s.socialSignInStatusType, s);
+    }
+    
+    private SocialSignInStatus(String socialSignInStatusType){
+        this.socialSignInStatusType = socialSignInStatusType;
+    }
+    
+    public String getSocialSignInStatusType(){
+         return socialSignInStatusType;
+    }
+    
+    public static SocialSignInStatus getSocialSignInStatus(String type) {
+        SocialSignInStatus result = map.get(type);
+        if (result == null) {
+          throw new IllegalArgumentException("Invalid status: " + type);
+        }
+        return result;
+    }
+}
